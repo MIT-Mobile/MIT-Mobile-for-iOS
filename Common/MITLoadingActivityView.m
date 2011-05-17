862971d241e13c4e8d34e0f9b4838903f0164f67
Blake Skinner <bskinner@mit.edu>
2011-05-17 12:07:10 -0400

    Moved the view setup to the -(void)layoutSubviews method. Also removed the drawRect and added a UIImageView for drawing the background

--- Common/MITLoadingActivityView.m
+++ Common/MITLoadingActivityView.m
@@ -7,9 +7,26 @@
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-		// Initialization code
-		
-		UIActivityIndicatorView *spinny = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray] autorelease];
+		self.opaque = YES;
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    {
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:MITImageNameBackground]];
+        CGRect frame = self.frame;
+        frame.origin = CGPointZero;
+        
+        imageView.frame = frame;
+        _backgroundView = imageView;
+        [self addSubview:imageView];
+    }
+    
+    {
+        CGFloat labelLeftMargin = 5.0;
+        CGRect frame = self.frame;
+        UIActivityIndicatorView *spinny = [[[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray] autorelease];
 		[spinny startAnimating];
 		
 		UILabel *loadingLabel = [[[UILabel alloc] initWithFrame:CGRectZero] autorelease];
@@ -17,27 +34,24 @@
 		loadingLabel.backgroundColor = [UIColor clearColor];
 		loadingLabel.text = loadingText;
 		CGSize labelSize = [loadingText sizeWithFont:loadingLabel.font];
-		loadingLabel.frame = CGRectMake(spinny.frame.size.width + 2.0, 0.0, labelSize.width, labelSize.height);
+		loadingLabel.frame = CGRectMake(spinny.frame.size.width + labelLeftMargin, 0.0, labelSize.width, labelSize.height);
 		
-		UIView *centeredView = [[[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, labelSize.width + spinny.frame.size.width + 2.0, labelSize.height)] autorelease];
+        CGRect viewFrame = CGRectMake(0.0,
+                                      0.0,
+                                      labelSize.width + spinny.frame.size.width + labelLeftMargin,
+                                      labelSize.height);
+		UIView *centeredView = [[[UIView alloc] initWithFrame:viewFrame] autorelease];
 		[centeredView addSubview:spinny];
 		[centeredView addSubview:loadingLabel];
 		
 		centeredView.center = CGPointMake(frame.size.width / 2, frame.size.height / 2);
 		[self addSubview:centeredView];
-		
     }
-    return self;
 }
-
-
-- (void)drawRect:(CGRect)rect {
-	UIImage *image = [UIImage imageNamed:MITImageNameBackground];
-	[image drawInRect:rect];
-}
-
 
 - (void)dealloc {
+    [_backgroundView release], _backgroundView = nil;
+    
     [super dealloc];
 }
 
