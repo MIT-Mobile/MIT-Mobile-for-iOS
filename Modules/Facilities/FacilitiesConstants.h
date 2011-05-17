1fbc3784ac6df1154a60b5e406c0d98fa824c351
Blake Skinner <bskinner@mit.edu>
2011-05-17 15:19:05 -0400

    Added several keys used to pass information between view controllers

---
+++ Modules/Facilities/FacilitiesConstants.h
@@ -0,0 +1,6 @@
+#import <Foundation/Foundation.h>
+
+extern NSString * const FacilitiesRequestRepairTypeKey;
+extern NSString * const FacilitiesRequestLocationBuildingKey;
+extern NSString * const FacilitiesRequestLocationRoomKey;
+extern NSString * const FacilitiesRequestLocationCustomKey;
---
+++ Modules/Facilities/FacilitiesConstants.m
@@ -0,0 +1,6 @@
+#import "FacilitiesConstants.h"
+
+NSString * const FacilitiesRequestRepairTypeKey = @"FacilitiesRequestRepairType";
+NSString * const FacilitiesRequestLocationBuildingKey = @"FacilitiesRequestLocationBuilding";
+NSString * const FacilitiesRequestLocationRoomKey = @"FacilitiesRequestLocationRoom";
+NSString * const FacilitiesRequestLocationCustomKey = @"FacilitiesRequestLocationCustom";
