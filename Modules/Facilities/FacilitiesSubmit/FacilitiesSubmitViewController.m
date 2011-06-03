90c5e6e573037be645c05057966c07dd6703f755
Blake Skinner <bskinner@mit.edu>
2011-06-03 14:20:52 -0400

    Undo commit f607b0

---
+++ Modules/Facilities/FacilitiesSubmit/FacilitiesSubmitViewController.m
@@ -0,0 +1,159 @@
+#import "FacilitiesSubmitViewController.h"
+
+#import "FacilitiesRootViewController.h"
+
+@implementation FacilitiesSubmitViewController
+@synthesize statusLabel = _statusLabel;
+@synthesize progressView = _progressView;
+@synthesize completeButton = _completeButton;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    self.statusLabel = nil;
+    self.progressView = nil;
+    self.completeButton = nil;
+    [super dealloc];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    
+}
+
+#pragma mark - View lifecycle
+- (void)loadView {
+    CGRect frame = [[UIScreen mainScreen] applicationFrame];
+    
+    if (self.navigationController.navigationBarHidden == NO) {
+        frame.size.height -= self.navigationController.navigationBar.frame.size.height;
+    }
+    
+    UIView *mainView = [[[UIView alloc] initWithFrame:frame] autorelease];
+    
+    {
+        self.progressView = [[[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault] autorelease];
+        CGRect progressFrame = CGRectMake(15,
+                                          (frame.size.height - self.progressView.frame.size.height) / 2.0,
+                                          frame.size.width - 30,
+                                          self.progressView.frame.size.height);
+        
+        self.progressView.frame = progressFrame;
+        [mainView addSubview:self.progressView];
+    }
+    
+    {
+        CGFloat height = 32;
+        CGRect labelFrame = CGRectMake(15,
+                                       self.progressView.frame.origin.y - height,
+                                       frame.size.width - 30,
+                                       height);
+
+        self.statusLabel = [[[UILabel alloc] initWithFrame:labelFrame] autorelease];
+        self.statusLabel.textAlignment = UITextAlignmentCenter;
+        self.statusLabel.backgroundColor = [UIColor clearColor];
+        [mainView addSubview:self.statusLabel];
+    }
+    
+    {
+        CGRect buttonFrame = CGRectZero;
+        buttonFrame.size = CGSizeMake(128, 32);
+        buttonFrame.origin = CGPointMake((frame.size.width - buttonFrame.size.width) / 2.0,
+                                         self.progressView.frame.origin.y);
+        self.completeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+        self.completeButton.frame = buttonFrame;
+        self.completeButton.hidden = YES;
+        [self.completeButton setTitle:@"Return to start"
+                             forState:UIControlStateNormal];
+        [self.completeButton addTarget:self
+                                action:@selector(reportCompleted:)
+                      forControlEvents:UIControlEventTouchUpInside];
+        [mainView addSubview:self.completeButton];
+    }
+    
+    
+    [self setView:mainView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    self.navigationItem.hidesBackButton = YES;
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    _demoQueue = dispatch_queue_create("edu.mit.mobile.ProgressDemo", 0);
+    NSUInteger imageSize = 768000; // Bytes
+    NSUInteger uploadSpeed = 50000; // Bps
+    
+    [self.progressView setProgress:0.0];
+    self.progressView.hidden = NO;
+    self.completeButton.hidden = YES;
+    [self.statusLabel setText:@"Uploading report to the server"];
+    
+    int blkCount = 0;
+    for (NSUInteger chunk = 0; chunk < imageSize; chunk += uploadSpeed) {
+        dispatch_async(_demoQueue, ^(void) {
+            dispatch_async(dispatch_get_main_queue(), ^(void) {
+                NSMutableString *string = [NSMutableString string];
+                for (int i = 0; i < ((blkCount % 3) + 1); i++) {
+                    [string appendString:@"."];
+                }
+                
+                for (int i = 0; i < (2 - (blkCount % 3)); i++) {
+                    [string appendString:@" "];
+                }
+                
+                [self.statusLabel setText:[NSString stringWithFormat:@"Uploading picture%@",string]];
+                [self.progressView setProgress:((float)chunk / (float)imageSize)];
+            });
+            
+            [NSThread sleepForTimeInterval:1.0f];
+        });
+        blkCount++;
+    }
+
+    dispatch_async(_demoQueue, ^(void) {
+        dispatch_async(dispatch_get_main_queue(), ^(void) {
+            [self.statusLabel setText:@"Successfully submitted your report"];
+            self.progressView.hidden = YES;
+            self.completeButton.hidden = NO;
+        });
+    });
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    dispatch_release(_demoQueue);
+}
+
+- (void)viewDidUnload
+{
+    self.statusLabel = nil;
+    self.progressView = nil;
+    self.completeButton = nil;
+    [super viewDidUnload];    
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)reportCompleted:(id)sender {
+    for (UIViewController *controller in self.navigationController.viewControllers) {
+        if ([controller isKindOfClass:[FacilitiesRootViewController class]]) {
+            [self.navigationController popToViewController:controller
+                                                  animated:YES];
+            break;
+        }
+    }
+}
+
+@end
--- Modules/Facilities/FacilitiesSummary/FacilitiesSummaryViewController.m
+++ Modules/Facilities/FacilitiesSummary/FacilitiesSummaryViewController.m
@@ -1,4 +1,5 @@
 #import <QuartzCore/QuartzCore.h>
+#import <sys/ucred.h>
 
 #import "FacilitiesSummaryViewController.h"
 #import "FacilitiesCategory.h"
@@ -6,35 +7,27 @@
 #import "FacilitiesRoom.h"
 #import "FacilitiesConstants.h"
 #import "FacilitiesRootViewController.h"
-
-enum {
-    FacilitiesFocusDescription = 1,
-    FacilitiesFocusEmail
-};
+#import "FacilitiesSubmitViewController.h"
 
 @interface FacilitiesSummaryViewController ()
 - (UIView*)firstResponderInView:(UIView*)view;
-- (void)layoutOverlayView;
 @end
 
 @implementation FacilitiesSummaryViewController
 @synthesize scrollView = _scrollView;
 @synthesize imageView = _imageView;
+@synthesize pictureButton = _pictureButton;
 @synthesize problemLabel = _problemLabel;
 @synthesize descriptionView = _descriptionView;
 @synthesize emailField = _emailField;
+@synthesize characterCount = _characterCount;
 @synthesize reportData = _reportData;
-@synthesize overlayView = _overlayView;
-@synthesize uploadProgress = _uploadProgress;
-@synthesize uploadStatus = _uploadStatus;
-@synthesize returnButton = _returnButton;
-
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        _keyboardIsVisible = NO;
+        // Custom initialization
     }
     return self;
 }
@@ -42,18 +35,16 @@ enum {
 - (void)dealloc
 {
     self.imageView = nil;
+    self.pictureButton = nil;
     self.problemLabel = nil;
     self.descriptionView = nil;
     self.emailField = nil;
+    self.characterCount = nil;
     self.reportData = nil;
     self.scrollView = nil;
-    self.overlayView = nil;
     
     [[NSNotificationCenter defaultCenter] removeObserver:self];
-
-    [_uploadProgress release];
-    [_uploadStatus release];
-    [_returnButton release];
+    
     [super dealloc];
 }
 
@@ -63,15 +54,19 @@ enum {
 }
 
 #pragma mark - View lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.view.backgroundColor = [UIColor clearColor];
     
+    self.scrollView.backgroundColor = [UIColor clearColor];
+    self.scrollView.autoresizesSubviews = NO;
     self.scrollView.scrollsToTop = NO;
+    self.scrollView.scrollEnabled = NO;
     self.scrollView.contentSize = self.scrollView.bounds.size;
     
     self.imageView.image = [UIImage imageNamed:@"tours/button_photoopp"];
-    self.imageView.userInteractionEnabled = NO;
     
     self.descriptionView.layer.cornerRadius = 5.0f;
     self.descriptionView.layer.borderWidth = 2.0f;
@@ -84,12 +79,9 @@ enum {
                                                              action:@selector(submitReport:)] autorelease];
     item.title = @"Submit";
     self.navigationItem.rightBarButtonItem = item;
-    [self layoutOverlayView];
 }
 
 - (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-    
     FacilitiesLocation *location = [self.reportData objectForKey:FacilitiesRequestLocationBuildingKey];
     FacilitiesRoom *room = [self.reportData objectForKey:FacilitiesRequestLocationRoomKey];
     NSString *customLocation = [self.reportData objectForKey:FacilitiesRequestLocationCustomKey];
@@ -123,7 +115,6 @@ enum {
 }
 
 - (void)viewWillDisappear:(BOOL)animated {
-    [super viewWillDisappear:animated];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
@@ -132,13 +123,11 @@ enum {
     [super viewDidUnload];
     
     self.imageView = nil;
+    self.pictureButton = nil;
     self.problemLabel = nil;
     self.descriptionView = nil;
     self.emailField = nil;
-    self.overlayView = nil;
-    self.uploadProgress = nil;
-    self.uploadStatus = nil;
-    self.returnButton = nil;
+    self.characterCount = nil;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -146,94 +135,13 @@ enum {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
 
-- (void)layoutOverlayView {
-    CGRect viewFrame = self.scrollView.frame;
-    viewFrame.origin = CGPointZero;
-    
-    UIView *overlay = [[[UIView alloc] initWithFrame:viewFrame] autorelease];
-    overlay.hidden = YES;
-    overlay.backgroundColor = [UIColor colorWithWhite:0.00
-                                                alpha:0.25];
-    UIView *highlightView = nil;
-    {
-        CGFloat height = 125;
-        CGRect frame = CGRectMake(25,
-                (viewFrame.size.height - height) / 2,
-                viewFrame.size.width - 50,
-                height);
-        highlightView = [[[UIView alloc] initWithFrame:frame] autorelease];
-        highlightView.layer.cornerRadius = 5.0;
-        highlightView.layer.borderColor = [[UIColor grayColor] CGColor];
-        highlightView.layer.borderWidth = 2.0;
-        highlightView.backgroundColor = [UIColor colorWithWhite:0.95
-                                                          alpha:0.95];
-        [overlay addSubview:highlightView];
-    }
-
-    {
-        CGFloat height = 48;
-        CGRect labelFrame = CGRectMake(15,
-                                       15,
-                                       highlightView.frame.size.width - 30,
-                                       height);
-
-        UILabel *statusLabel = [[[UILabel alloc] initWithFrame:labelFrame] autorelease];
-        statusLabel.textAlignment = UITextAlignmentCenter;
-        statusLabel.backgroundColor = [UIColor clearColor];
-        statusLabel.numberOfLines = 2;
-        statusLabel.lineBreakMode = UILineBreakModeWordWrap;
-        [highlightView addSubview:statusLabel];
-        self.uploadStatus = statusLabel;
-    }
-
-
-
-    {
-        UIProgressView *progressView = progressView = [[[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault] autorelease];
-        CGRect progressFrame = CGRectMake(15,
-                                          ((highlightView.frame.size.height - progressView.frame.size.height) / 2.0) + 5,
-                                          highlightView.frame.size.width - 30,
-                                          progressView.frame.size.height);
-
-        progressView.frame = progressFrame;
-        [highlightView addSubview:progressView];
-        self.uploadProgress = progressView;
-    }
-
-    {
-        CGRect buttonFrame = CGRectZero;
-        buttonFrame.size = CGSizeMake(128, 32);
-        buttonFrame.origin = CGPointMake((highlightView.frame.size.width - buttonFrame.size.width) / 2.0,
-                                         self.uploadProgress.frame.origin.y);
-        UIButton *completeButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
-        completeButton.frame = buttonFrame;
-        completeButton.hidden = YES;
-        [completeButton setTitle:@"Return to start"
-                        forState:UIControlStateNormal];
-        [completeButton addTarget:self
-                           action:@selector(reportUploaded:)
-                 forControlEvents:UIControlEventTouchUpInside];
-        [highlightView addSubview:completeButton];
-        self.returnButton = completeButton;
-    }
-
-    self.overlayView = overlay;
-    [self.scrollView addSubview:overlay];
-}
-
-#pragma mark - IBAction Methods
-- (IBAction)selectPicture:(id)sender {
-    if (_keyboardIsVisible) {
-        [self dismissKeyboard:sender];
-        return;
-    }
-
+- (IBAction)selectPicture:(id)sender { 
     if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
         UIActionSheet *sheet = [[[UIActionSheet alloc] initWithTitle:nil
                                                             delegate:self
                                                    cancelButtonTitle:@"Cancel"
                                               destructiveButtonTitle:nil
-                otherButtonTitles:@"Take Photo",@"Choose Existing", nil] autorelease];
+                                                   otherButtonTitles:@"Take Photo",@"Choose Existing", nil] autorelease];
         sheet.actionSheetStyle = UIActionSheetStyleBlackTranslucent;
         [sheet showInView:self.view];
     } else if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary]) {
@@ -246,105 +154,10 @@ enum {
 }
 
 - (IBAction)submitReport:(id)sender {
-    if (_keyboardIsVisible) {
-        return;
-    }
-    
-    if ([self.descriptionView.text length] == 0) {
-        UIAlertView *alert = [[[UIAlertView alloc] initWithTitle:@"Information Missing"
-                                                         message:@"Please enter a description before continuing"
-                                                        delegate:self
-                                               cancelButtonTitle:@"Ok"
-                                               otherButtonTitles:nil] autorelease];
-        alert.tag = FacilitiesFocusDescription;
-        [alert show];
-        return;
-    }
-
-    [UIView animateWithDuration:0.3
-                          delay:0.0
-                        options:UIViewAnimationOptionBeginFromCurrentState
-                     animations:^{
-                         self.navigationItem.rightBarButtonItem.enabled = NO;
-                         self.overlayView.hidden = NO;
-                     }
-                     completion:^ (BOOL finished) {
-                         if (finished == NO) {
-                             return;
-                         }
-                         
-                         dispatch_queue_t demoQueue = dispatch_queue_create("edu.mit.mobile.ProgressDemo", 0);
-                         NSUInteger imageSize = 768000; // Bytes
-                         NSUInteger uploadSpeed = 50000; // Bps
-
-                         [self.uploadProgress setProgress:0.0];
-                         self.uploadProgress.hidden = NO;
-                         self.returnButton.hidden = YES;
-                         [self.uploadStatus setText:@"Uploading report to the server"];
-
-                         int blkCount = 0;
-                         for (NSUInteger chunk = 0; chunk < imageSize; chunk += uploadSpeed) {
-                             dispatch_async(demoQueue, ^(void) {
-                                 dispatch_async(dispatch_get_main_queue(), ^ {
-                                     NSMutableString *string = [NSMutableString string];
-                                     for (int i = 0; i < ((blkCount % 3) + 1); i++) {
-                                         [string appendString:@"."];
-                                     }
-
-                                     for (int i = 0; i < (2 - (blkCount % 3)); i++) {
-                                         [string appendString:@" "];
-                                     }
-
-                                     [self.uploadStatus setText:[NSString stringWithFormat:@"Uploading picture%@",string]];
-                                     [self.uploadProgress setProgress:((float)chunk / (float)imageSize)];
-                                 });
-
-                                 [NSThread sleepForTimeInterval:1.0f];
-                             });
-                             blkCount++;
-                         }
-
-                         dispatch_async(demoQueue, ^(void) {
-                             dispatch_async(dispatch_get_main_queue(), ^ {
-                                 [self.uploadStatus setText:@"Successfully submitted your report"];
-                                 self.uploadProgress.hidden = YES;
-                                 self.returnButton.hidden = NO;
-                             });
-                         });
-
-                         dispatch_release(demoQueue);
-                     }];
+    [self.navigationController pushViewController:[[[FacilitiesSubmitViewController alloc] init] autorelease]
+                                         animated:YES];
 }
 
-- (IBAction)dismissKeyboard:(id)sender {
-    if (_keyboardIsVisible) {
-        UIView *firstResponder = [self firstResponderInView:self.view];
-
-        if (firstResponder) {
-            [firstResponder resignFirstResponder];
-        }
-    }
-}
-
-- (IBAction)reportUploaded:(id)sender {
-    [UIView animateWithDuration:1.0
-                          delay:0.0
-                        options:0
-                     animations:^{
-                         self.overlayView.hidden = YES;
-                     }
-                     completion: ^ (BOOL finished) {
-                         if (finished) {
-                             for (UIViewController *controller in self.navigationController.viewControllers) {
-                                 if ([controller isKindOfClass:[FacilitiesRootViewController class]]) {
-                                     [self.navigationController popToViewController:controller
-                                                                           animated:YES];
-                                     break;
-                                 }
-                             }
-                         }
-                     }];
-}
 
 #pragma mark - UITextViewDelegate
 static NSUInteger kMaxCharacters = 150;
@@ -403,21 +216,6 @@ static NSUInteger kMaxCharacters = 150;
     [self dismissModalViewControllerAnimated:YES];
 }
 
-#pragma mark - UIAlertViewDelegate
-- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
-    switch (alertView.tag) {
-        case FacilitiesFocusDescription:
-            [self.descriptionView becomeFirstResponder];
-            break;
-        case FacilitiesFocusEmail:
-            [self.emailField becomeFirstResponder];
-            break;
-        default:
-            break;
-    }
-}
-
-
 #pragma mark - Notification Methods
 - (UIView*)firstResponderInView:(UIView*)view {
     if ([view isFirstResponder]) {
@@ -472,9 +270,6 @@ static NSUInteger kMaxCharacters = 150;
                                                      animated:NO];
                      }
                      completion:nil];
-
-    _keyboardIsVisible = YES;
-    self.navigationItem.rightBarButtonItem.enabled = NO;
 }
 
 - (void)keyboardWillHide:(NSNotification*)notification {
@@ -505,7 +300,5 @@ static NSUInteger kMaxCharacters = 150;
                                                          animated:YES];
                          }
                      }];
-    _keyboardIsVisible = NO;
-    self.navigationItem.rightBarButtonItem.enabled = YES;
 }
 @end
