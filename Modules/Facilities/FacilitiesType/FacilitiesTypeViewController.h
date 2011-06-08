7628101dc042d29449e2b687e1b225c10c391b94
Blake Skinner <bskinner@mit.edu>
2011-06-08 13:07:44 -0400

    Removed the JSONDelegate interface

--- Modules/Facilities/FacilitiesType/FacilitiesTypeViewController.h
+++ Modules/Facilities/FacilitiesType/FacilitiesTypeViewController.h
@@ -1,8 +1,7 @@
 #import <UIKit/UIKit.h>
 #import "MITLoadingActivityView.h"
-#import "MITMobileWebAPI.h"
 
-@interface FacilitiesTypeViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, JSONLoadedDelegate>
+@interface FacilitiesTypeViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 {
     NSDictionary *_userData;
     UITableView *_tableView;
@@ -12,7 +11,4 @@
 @property (nonatomic,copy) NSDictionary *userData;
 @property (nonatomic,retain) UITableView* tableView;
 @property (nonatomic, retain) MITLoadingActivityView *loadingView;
-
-- (void)request:(MITMobileWebAPI *)request jsonLoaded:(id)JSONObject;
-- (BOOL)request:(MITMobileWebAPI *)request shouldDisplayStandardAlertForError: (NSError *)error;
 @end
