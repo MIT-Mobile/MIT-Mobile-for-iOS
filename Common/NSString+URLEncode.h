5a290454a28572227e36f13261cf0fa7180ca030
Blake Skinner <bskinner@mit.edu>
2011-07-28 09:42:51 -0400

    Merge the NSString+URLEncode files into the Foundation+MITAdditions

--- Common/NSString+URLEncode.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-
-
-@interface NSString (MITURLEncoding)
-- (NSString*)urlEncodeUsingEncoding:(NSStringEncoding)encoding;
-- (NSString*)urlEncodeUsingEncoding:(NSStringEncoding)encoding useFormURLEncoded:(BOOL)formUrlEncoded;
-- (NSString*)urlDecodeUsingEncoding:(NSStringEncoding)encoding;
-@end
