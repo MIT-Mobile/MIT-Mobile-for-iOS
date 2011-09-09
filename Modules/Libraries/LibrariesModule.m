df54f5f740cc4f8b96b4c7a2b5ac1c8ea770b1de
Justin Anderson <jander@mit.edu>
2011-09-09 17:23:46 -0400

    Use the new icon for Libraries.

--- Modules/Libraries/LibrariesModule.m
+++ Modules/Libraries/LibrariesModule.m
@@ -10,7 +10,7 @@
         self.tag = LibrariesTag;
         self.shortName = @"Libraries";
         self.longName = @"Libraries";
-        self.iconName = @"about";
+        self.iconName = @"libraries";
         self.isMovableTab = TRUE;
         
     }
