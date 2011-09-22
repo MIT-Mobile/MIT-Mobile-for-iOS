be4f96f4fb2d5b232a4e48bd3847831b27ffe6b6
Blake Skinner <bskinner@mit.edu>
2011-09-22 09:35:03 -0400

    Convert any HTML named character references to numeric references and then convert the entities using libxml2 instead of a custom parser

--- Common/Foundation+MITAdditions.m
+++ Common/Foundation+MITAdditions.m
@@ -1,3 +1,5 @@
+#include <libxml/parserInternals.h>
+#include <libxml/HTMLparser.h>
 #import "Foundation+MITAdditions.h"
 
 @implementation NSURL (MITAdditions)
@@ -92,48 +94,104 @@ static NSString *rfc1738_Unsafe = @"<>\"#%{}|\\^~`";
 
 
 @implementation NSString (MITAdditions_HTMLEntity)
-typedef struct HTML4ENTITIES {
+typedef struct {
     char *entityName;
     char *entityCode;
 } htmlEntity_t;
 
-- (NSString*)convertHTML4CharacterReferences:(NSString*)string
+- (NSString*)stringByConvertingHTMLNamedEntities
 {
-    static htmlEntity_t html4Entities[] = {
-        {"&Dagger;","&#8225;"},
-        {"&OElig;","&#338;"},
-        {"&Scaron;","&#352;"},
-        {"&Yuml;","&#376;"},
-        {"&bdquo;","&#8222;"},
-        {"&circ;","&#710;"},
-        {"&dagger;","&#8224;"},
-        {"&emsp;","&#8195;"},
-        {"&ensp;","&#8194;"},
-        {"&euro;","&#8364;"},
-        {"&ldquo;","&#8220;"},
-        {"&lrm;","&#8206;"},
-        {"&lsaquo;","&#8249;"},
-        {"&lsquo;","&#8216;"},
-        {"&mdash;","&#8212;"},
-        {"&ndash;","&#8211;"},
-        {"&oelig;","&#339;"},
-        {"&permil;","&#8240;"},
-        {"&rdquo;","&#8221;"},
-        {"&rlm;","&#8207;"},
-        {"&rsaquo;","&#8250;"},
-        {"&rsquo;","&#8217;"},
-        {"&sbquo;","&#8218;"},
-        {"&scaron;","&#353;"},
-        {"&thinsp;","&#8201;"},
-        {"&tilde;","&#732;"},
-        {"&zwj;","&#8205;"},
-        {"&zwnj;","&#8204;"},
-        {NULL,NULL}};
+    static htmlEntity_t htmlEntities[] = {
+        {"&Aacute;","&#x00C1;"}, {"&aacute;","&#x00E1;"},   {"&Acirc;","&#x00C2;"},
+        {"&acirc;","&#x00E2;"},  {"&acute;","&#x00B4;"},    {"&AElig;","&#x00C6;"},
+        {"&aelig;","&#x00E6;"},  {"&Agrave;","&#x00C0;"},   {"&agrave;","&#x00E0;"},
+        {"&alefsym;","&#x2135;"},{"&Alpha;","&#x0391;"},    {"&alpha;","&#x03B1;"},
+        {"&amp;","&#x0026;"},    {"&and;","&#x2227;"},      {"&ang;","&#x2220;"},
+        {"&apos;","&#x0027;"},   {"&Aring;","&#x00C5;"},    {"&aring;","&#x00E5;"},
+        {"&asymp;","&#x2248;"},  {"&Atilde;","&#x00C3;"},   {"&atilde;","&#x00E3;"},
+        {"&Auml;","&#x00C4;"},   {"&auml;","&#x00E4;"},     {"&bdquo;","&#x201E;"},
+        {"&Beta;","&#x0392;"},   {"&beta;","&#x03B2;"},     {"&brvbar;","&#x00A6;"},
+        {"&bull;","&#x2022;"},   {"&cap;","&#x2229;"},      {"&Ccedil;","&#x00C7;"},
+        {"&ccedil;","&#x00E7;"}, {"&cedil;","&#x00B8;"},    {"&cent;","&#x00A2;"},
+        {"&Chi;","&#x03A7;"},    {"&chi;","&#x03C7;"},      {"&circ;","&#x02C6;"},
+        {"&clubs;","&#x2663;"},  {"&cong;","&#x2245;"},     {"&copy;","&#x00A9;"},
+        {"&crarr;","&#x21B5;"},  {"&cup;","&#x222A;"},      {"&curren;","&#x00A4;"},
+        {"&dagger;","&#x2020;"}, {"&Dagger;","&#x2021;"},   {"&darr;","&#x2193;"},
+        {"&dArr;","&#x21D3;"},   {"&deg;","&#x00B0;"},      {"&Delta;","&#x0394;"},
+        {"&delta;","&#x03B4;"},  {"&diams;","&#x2666;"},    {"&divide;","&#x00F7;"},
+        {"&Eacute;","&#x00C9;"}, {"&eacute;","&#x00E9;"},   {"&Ecirc;","&#x00CA;"},
+        {"&ecirc;","&#x00EA;"},  {"&Egrave;","&#x00C8;"},   {"&egrave;","&#x00E8;"},
+        {"&empty;","&#x2205;"},  {"&emsp;","&#x2003;"},     {"&ensp;","&#x2002;"},
+        {"&Epsilon;","&#x0395;"},{"&epsilon;","&#x03B5;"},  {"&equiv;","&#x2261;"},
+        {"&Eta;","&#x0397;"},    {"&eta;","&#x03B7;"},      {"&ETH;","&#x00D0;"},
+        {"&eth;","&#x00F0;"},    {"&Euml;","&#x00CB;"},     {"&euml;","&#x00EB;"},
+        {"&euro;","&#x20AC;"},   {"&exist;","&#x2203;"},    {"&fnof;","&#x0192;"},
+        {"&forall;","&#x2200;"}, {"&frac12;","&#x00BD;"},   {"&frac14;","&#x00BC;"},
+        {"&frac34;","&#x00BE;"}, {"&frasl;","&#x2044;"},    {"&Gamma;","&#x0393;"},
+        {"&gamma;","&#x03B3;"},  {"&ge;","&#x2265;"},       {"&gt;","&#x003E;"},
+        {"&harr;","&#x2194;"},   {"&hArr;","&#x21D4;"},     {"&hearts;","&#x2665;"},
+        {"&hellip;","&#x2026;"}, {"&Iacute;","&#x00CD;"},   {"&iacute;","&#x00ED;"},
+        {"&Icirc;","&#x00CE;"},  {"&icirc;","&#x00EE;"},    {"&iexcl;","&#x00A1;"},
+        {"&Igrave;","&#x00CC;"}, {"&igrave;","&#x00EC;"},   {"&image;","&#x2111;"},
+        {"&infin;","&#x221E;"},  {"&int;","&#x222B;"},      {"&Iota;","&#x0399;"},
+        {"&iota;","&#x03B9;"},   {"&iquest;","&#x00BF;"},   {"&isin;","&#x2208;"},
+        {"&Iuml;","&#x00CF;"},   {"&iuml;","&#x00EF;"},     {"&Kappa;","&#x039A;"},
+        {"&kappa;","&#x03BA;"},  {"&Lambda;","&#x039B;"},   {"&lambda;","&#x03BB;"},
+        {"&lang;","&#x2329;"},   {"&laquo;","&#x00AB;"},    {"&larr;","&#x2190;"},
+        {"&lArr;","&#x21D0;"},   {"&lceil;","&#x2308;"},    {"&ldquo;","&#x201C;"},
+        {"&le;","&#x2264;"},     {"&lfloor;","&#x230A;"},   {"&lowast;","&#x2217;"},
+        {"&loz;","&#x25CA;"},    {"&lrm;","&#x200E;"},      {"&lsaquo;","&#x2039;"},
+        {"&lsquo;","&#x2018;"},  {"&lt;","&#x003C;"},       {"&macr;","&#x00AF;"},
+        {"&mdash;","&#x2014;"},  {"&micro;","&#x00B5;"},    {"&middot;","&#x00B7;"},
+        {"&minus;","&#x2212;"},  {"&Mu;","&#x039C;"},       {"&mu;","&#x03BC;"},
+        {"&nabla;","&#x2207;"},  {"&nbsp;","&#x00A0;"},     {"&ndash;","&#x2013;"},
+        {"&ne;","&#x2260;"},     {"&ni;","&#x220B;"},       {"&not;","&#x00AC;"},
+        {"&notin;","&#x2209;"},  {"&nsub;","&#x2284;"},     {"&Ntilde;","&#x00D1;"},
+        {"&ntilde;","&#x00F1;"}, {"&Nu;","&#x039D;"},       {"&nu;","&#x03BD;"},
+        {"&Oacute;","&#x00D3;"}, {"&oacute;","&#x00F3;"},   {"&Ocirc;","&#x00D4;"},
+        {"&ocirc;","&#x00F4;"},  {"&OElig;","&#x0152;"},    {"&oelig;","&#x0153;"},
+        {"&Ograve;","&#x00D2;"}, {"&ograve;","&#x00F2;"},   {"&oline;","&#x203E;"},
+        {"&Omega;","&#x03A9;"},  {"&omega;","&#x03C9;"},    {"&Omicron;","&#x039F;"},
+        {"&omicron;","&#x03BF;"},{"&oplus;","&#x2295;"},    {"&or;","&#x2228;"},
+        {"&ordf;","&#x00AA;"},   {"&ordm;","&#x00BA;"},     {"&Oslash;","&#x00D8;"},
+        {"&oslash;","&#x00F8;"}, {"&Otilde;","&#x00D5;"},   {"&otilde;","&#x00F5;"},
+        {"&otimes;","&#x2297;"}, {"&Ouml;","&#x00D6;"},     {"&ouml;","&#x00F6;"},
+        {"&para;","&#x00B6;"},   {"&part;","&#x2202;"},     {"&permil;","&#x2030;"},
+        {"&perp;","&#x22A5;"},   {"&Phi;","&#x03A6;"},      {"&phi;","&#x03C6;"},
+        {"&Pi;","&#x03A0;"},     {"&pi;","&#x03C0;"},       {"&piv;","&#x03D6;"},
+        {"&plusmn;","&#x00B1;"}, {"&pound;","&#x00A3;"},    {"&prime;","&#x2032;"},
+        {"&Prime;","&#x2033;"},  {"&prod;","&#x220F;"},     {"&prop;","&#x221D;"},
+        {"&Psi;","&#x03A8;"},    {"&psi;","&#x03C8;"},      {"&quot;","&#x0022;"},
+        {"&radic;","&#x221A;"},  {"&rang;","&#x232A;"},     {"&raquo;","&#x00BB;"},
+        {"&rarr;","&#x2192;"},   {"&rArr;","&#x21D2;"},     {"&rceil;","&#x2309;"},
+        {"&rdquo;","&#x201D;"},  {"&real;","&#x211C;"},     {"&reg;","&#x00AE;"},
+        {"&rfloor;","&#x230B;"}, {"&Rho;","&#x03A1;"},      {"&rho;","&#x03C1;"},
+        {"&rlm;","&#x200F;"},    {"&rsaquo;","&#x203A;"},   {"&rsquo;","&#x2019;"},
+        {"&sbquo;","&#x201A;"},  {"&Scaron;","&#x0160;"},   {"&scaron;","&#x0161;"},
+        {"&sdot;","&#x22C5;"},   {"&sect;","&#x00A7;"},     {"&shy;","&#x00AD;"},
+        {"&Sigma;","&#x03A3;"},  {"&sigma;","&#x03C3;"},    {"&sigmaf;","&#x03C2;"},
+        {"&sim;","&#x223C;"},    {"&spades;","&#x2660;"},   {"&sub;","&#x2282;"},
+        {"&sube;","&#x2286;"},   {"&sum;","&#x2211;"},      {"&sup1;","&#x00B9;"},
+        {"&sup2;","&#x00B2;"},   {"&sup3;","&#x00B3;"},     {"&sup;","&#x2283;"},
+        {"&supe;","&#x2287;"},   {"&szlig;","&#x00DF;"},    {"&Tau;","&#x03A4;"},
+        {"&tau;","&#x03C4;"},    {"&there4;","&#x2234;"},   {"&Theta;","&#x0398;"},
+        {"&theta;","&#x03B8;"},  {"&thetasym;","&#x03D1;"}, {"&thinsp;","&#x2009;"},
+        {"&THORN;","&#x00DE;"},  {"&thorn;","&#x00FE;"},    {"&tilde;","&#x02DC;"},
+        {"&times;","&#x00D7;"},  {"&trade;","&#x2122;"},    {"&Uacute;","&#x00DA;"},
+        {"&uacute;","&#x00FA;"}, {"&uarr;","&#x2191;"},     {"&uArr;","&#x21D1;"},
+        {"&Ucirc;","&#x00DB;"},  {"&ucirc;","&#x00FB;"},    {"&Ugrave;","&#x00D9;"},
+        {"&ugrave;","&#x00F9;"}, {"&uml;","&#x00A8;"},      {"&upsih;","&#x03D2;"},
+        {"&Upsilon;","&#x03A5;"},{"&upsilon;","&#x03C5;"},  {"&Uuml;","&#x00DC;"},
+        {"&uuml;","&#x00FC;"},   {"&weierp;","&#x2118;"},   {"&Xi;","&#x039E;"},
+        {"&xi;","&#x03BE;"},     {"&Yacute;","&#x00DD;"},   {"&yacute;","&#x00FD;"},
+        {"&yen;","&#x00A5;"},    {"&yuml;","&#x00FF;"},     {"&Yuml;","&#x0178;"},
+        {"&Zeta;","&#x0396;"},   {"&zeta;","&#x03B6;"},     {"&zwj;","&#x200D;"},
+        {"&zwnj;","&#x200C;"},   {NULL,NULL}};
     
     NSMutableString *replString = [NSMutableString stringWithCapacity:([self length] * 1.25)];
     [replString setString:self];
     
-    htmlEntity_t *entity = html4Entities;
+    htmlEntity_t *entity = htmlEntities;
     while (entity->entityName != NULL) {
         NSUInteger entities = [replString replaceOccurrencesOfString:[NSString stringWithUTF8String:entity->entityName]
                                                           withString:[NSString stringWithUTF8String:entity->entityCode]
@@ -141,7 +199,7 @@ typedef struct HTML4ENTITIES {
                                                                range:NSMakeRange(0, [replString length])];
         
         if (entities > 0) {
-            DLog(@"Replaced %lu of &%s; -> &%s;", (unsigned long)entities, entity->entityName, entity->entityCode);
+            DLog(@"Replaced %lu of %s -> %s", (unsigned long)entities, entity->entityName, entity->entityCode);
         }
         
         ++entity;
@@ -155,84 +213,28 @@ typedef struct HTML4ENTITIES {
                                       options:NSLiteralSearch].location;
     
     if (ampIndex == NSNotFound) {
-        return self;
+        return [NSString stringWithString:self];
     }
     
-    NSString *searchString = [self convertHTML4CharacterReferences:self];
-    NSUInteger strLength = [searchString length];
+    // May or may not have numeric character entities at this point
+    // so run it through the check anyway
+    NSString *searchString = [self stringByConvertingHTMLNamedEntities];
+    xmlParserCtxtPtr parserContext = htmlNewParserCtxt();
+    htmlCtxtUseOptions(parserContext,
+                       (HTML_PARSE_RECOVER |
+                        HTML_PARSE_NOERROR |
+                        HTML_PARSE_NOWARNING));
+    xmlChar *convertedString = xmlStringDecodeEntities(parserContext,
+                                                       (xmlChar*)[searchString UTF8String],
+                                                       XML_SUBSTITUTE_BOTH,
+                                                       0,
+                                                       0,
+                                                       0);
     
-    NSMutableString *result = [NSMutableString stringWithCapacity:strLength];
-    NSScanner *scanner = [NSScanner scannerWithString:searchString];
+    NSString *result = [NSString stringWithUTF8String:(char*)convertedString];
+    xmlFree(convertedString);
+    xmlFreeParserCtxt(parserContext);
     
-    while (![scanner isAtEnd]) {
-        NSString *nonEntityString = nil;
-        
-        if ([scanner scanUpToString:@"&" intoString:&nonEntityString]) {
-            [result appendString:nonEntityString];
-        }
-        
-        if ([scanner scanString:@"&amp;" intoString:NULL])
-        {
-            [result appendString:@"&"];
-        }
-        else if ([scanner scanString:@"&apos;" intoString:NULL])
-        {
-            [result appendString:@"'"];
-        }
-        else if ([scanner scanString:@"&quot;" intoString:NULL])
-        {
-            [result appendString:@"\""];
-        }
-        else if ([scanner scanString:@"&lt;" intoString:NULL])
-        {
-            [result appendString:@"<"];
-        }
-        else if ([scanner scanString:@"&gt;" intoString:NULL])
-        {
-            [result appendString:@">"];
-        }
-        else if ([scanner scanString:@"&#" intoString:NULL])
-        {
-            BOOL readNumber;
-            unsigned charCode;
-            NSString *hexIdentifier = @"";
-            
-            if ([scanner scanString:@"x" intoString:&hexIdentifier]) {
-                readNumber = [scanner scanHexInt:&charCode];
-            }
-            else {
-                readNumber = [scanner scanInt:(int*)&charCode];
-            }
-            
-            if (readNumber)
-            {
-                [result appendFormat:@"%C", charCode];
-            }
-            else
-            {
-                NSString *unknownEntity = @"";
-                
-                [scanner scanUpToString:@";" intoString:&unknownEntity];
-                [result appendFormat:@"&#%@%@;", hexIdentifier, unknownEntity];
-                
-                NSLog(@"Expected numeric character entity but got &#%@%@;", hexIdentifier, unknownEntity);
-            }
-            
-            [scanner scanString:@";" intoString:NULL];
-        }
-        else
-        {
-            NSString *unknownEntity = @"";
-            NSString *semicolon = @"";
-            
-            [scanner scanUpToString:@";" intoString:&unknownEntity];
-            [scanner scanString:@";" intoString:&semicolon];
-            
-            [result appendFormat:@"%@%@", unknownEntity, semicolon];
-            NSLog(@"Unsupported XML character entity %@%@", unknownEntity, semicolon);
-        }
-    }
-
     return result;
 }
 @end
