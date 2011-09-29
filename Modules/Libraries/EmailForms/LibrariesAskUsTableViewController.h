538b7db691758c9e6161aa1b3f1cc55d59966914
Brian Patt <blpatt@mit.edu>
2011-09-29 10:57:56 -0400

    some visuals tweaks to initial Ask Us! screen, and added the general help phone button to the initial ask us! screen.

--- Modules/Libraries/EmailForms/LibrariesAskUsTableViewController.h
+++ Modules/Libraries/EmailForms/LibrariesAskUsTableViewController.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
+#import "SecondaryGroupedTableViewCell.h"
 
+@interface PhoneTableViewCell : SecondaryGroupedTableViewCell {
+@private
+}
+@end
 
 @interface LibrariesAskUsTableViewController : UITableViewController {
     
--- Modules/Libraries/EmailForms/LibrariesAskUsTableViewController.m
+++ Modules/Libraries/EmailForms/LibrariesAskUsTableViewController.m
@@ -1,11 +1,22 @@
 #import "LibrariesAskUsTableViewController.h"
 #import "LibrariesAskUsViewController.h"
 #import "LibrariesAppointmentViewController.h"
+#import "MITUIConstants.h"
 #import "UIKit+MITAdditions.h"
+#import "SecondaryGroupedTableViewCell.h"
 
 #define ASK_US_ROW 0
 #define APPOINTMENT_ROW 1
-#define TOTAL_ROWS 2
+#define HELP_ROW 2
+#define TOTAL_ROWS 3
+
+#define TEXT_TAG 1
+
+#define PADDING 10
+
+#define TEXT_WIDTH 260
+#define ASK_US_TEXT @"Ask Us!"
+#define APPOINTMENT_TEXT @"Make a research consultation appointment"
 
 @implementation LibrariesAskUsTableViewController
 
@@ -38,6 +49,9 @@
     [super viewDidLoad];
 
     [self.tableView applyStandardColors];
+    [self.tableView applyStandardCellHeight];
+    
+    self.title = @"Ask Us!";
 }
 
 - (void)viewDidUnload
@@ -49,6 +63,13 @@
 {
     // Return YES for supported orientations
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+
+- (CGFloat)heightForText:(NSString *)text {
+    CGSize textSize = [text sizeWithFont:[UIFont fontWithName:BOLD_FONT size:CELL_STANDARD_FONT_SIZE] 
+                       constrainedToSize:CGSizeMake(260, 100)];
+    return textSize.height;
 }
 
 #pragma mark - Table view data source
@@ -65,24 +86,54 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    if ((indexPath.row == ASK_US_ROW) ||(indexPath.row == APPOINTMENT_ROW)) {
+        static NSString *CellIdentifier = @"LockedCell";
     
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    if (cell == nil) {
-        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
-        [cell applyStandardFonts];
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
+        if (cell == nil) {
+            cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier] autorelease];
+            cell.accessoryView = [UIImageView accessoryViewWithMITType:MITAccessoryViewSecure];
+            UILabel *label = [[[UILabel alloc] initWithFrame:CGRectMake(PADDING, PADDING, TEXT_WIDTH, 200)] autorelease];
+            label.textColor = CELL_STANDARD_FONT_COLOR;
+            label.font = [UIFont fontWithName:BOLD_FONT size:CELL_STANDARD_FONT_SIZE];
+            label.tag = TEXT_TAG;
+            label.numberOfLines = 0;
+            [cell.contentView addSubview:label];
+        }
+    
+        UILabel *titleLabel = (UILabel *)[cell viewWithTag:TEXT_TAG];
+        if (indexPath.row == ASK_US_ROW) {
+            titleLabel.text = ASK_US_TEXT;
+        } else if (indexPath.row == APPOINTMENT_ROW) {
+            titleLabel.text = APPOINTMENT_TEXT;
+        }
+        CGRect titleLabelFrame = titleLabel.frame;
+        titleLabelFrame.size.height = [self heightForText:titleLabel.text];
+        titleLabel.frame = titleLabelFrame;
+        return cell;
+    } else if (indexPath.row == HELP_ROW) {
+        PhoneTableViewCell *helpCell = (PhoneTableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"Help"];
+        if (!helpCell) {
+            helpCell = [[[PhoneTableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Help"] autorelease];
+            helpCell.accessoryView = [UIImageView accessoryViewWithMITType:MITAccessoryViewPhone];
+            helpCell.textLabel.text = @"General help";
+            helpCell.secondaryTextLabel.text = @"(617-324-2275)";
+        }
+        return helpCell;
     }
-    
-    if (indexPath.row == ASK_US_ROW) {
-        cell.textLabel.text = @"Ask Us!";
-    } else if (indexPath.row == APPOINTMENT_ROW) {
-        cell.textLabel.text = @"Make a research consultation appointment";
-    }
-    
-    return cell;
+    return nil;
 }
 
-
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ((indexPath.row == ASK_US_ROW) ||(indexPath.row == APPOINTMENT_ROW)) {
+        if (indexPath.row == ASK_US_ROW) {
+            return [self heightForText:ASK_US_TEXT] + 2 * PADDING;
+        } else if (indexPath.row == APPOINTMENT_ROW) {
+            return [self heightForText:APPOINTMENT_TEXT] + 2 * PADDING;
+        }
+    }
+    return tableView.rowHeight;
+}
 
 #pragma mark - Table view delegate
 
@@ -94,8 +145,25 @@
         vc = [[[LibrariesAskUsViewController alloc] init] autorelease];
     } else if (indexPath.row == APPOINTMENT_ROW) {
         vc = [[[LibrariesAppointmentViewController alloc] init] autorelease];
+    } else if (indexPath.row == HELP_ROW) {
+        NSURL *url = [NSURL URLWithString:@"tel://16173242275"];
+        if ([[UIApplication sharedApplication] canOpenURL:url]) {
+            [[UIApplication sharedApplication] openURL:url];
+        }
+        [tableView deselectRowAtIndexPath:indexPath animated:YES];
+        return;
     }
+    
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+@end
+
+@implementation PhoneTableViewCell 
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    self.backgroundColor = [UIColor whiteColor];
 }
 
 @end
