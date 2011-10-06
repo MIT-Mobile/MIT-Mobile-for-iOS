f80d30b8bb38a8bfb0014a4f2c7fe49c25956b98
Blake Skinner <bskinner@mit.edu>
2011-10-06 14:55:00 -0400

    Silence a warning regarding an implicit cast. UIInterfaceOrientation is defined in terms of UIDeviceOrientation so there is no problem here

--- Facebook Connect/FBDialog.m
+++ Facebook Connect/FBDialog.m
@@ -160,7 +160,7 @@ static CGFloat kBorderWidth = 10;
     CGFloat width = frame.size.width - kPadding * 2;
     CGFloat height = frame.size.height - kPadding * 2;
     
-    _orientation = [UIApplication sharedApplication].statusBarOrientation;
+    _orientation = (UIDeviceOrientation)[UIApplication sharedApplication].statusBarOrientation;
     if (UIInterfaceOrientationIsLandscape(_orientation)) {
         self.frame = CGRectMake(kPadding, kPadding, height, width);
     } else {
@@ -435,7 +435,7 @@ static CGFloat kBorderWidth = 10;
 // UIDeviceOrientationDidChangeNotification
 
 - (void)deviceOrientationDidChange:(void*)object {
-    UIDeviceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    UIDeviceOrientation orientation = (UIDeviceOrientation)[UIApplication sharedApplication].statusBarOrientation;
     if (!_showingKeyboard && [self shouldRotateToOrientation:orientation]) {
         [self updateWebOrientation];
         
