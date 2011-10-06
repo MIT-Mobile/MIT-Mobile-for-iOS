a3dd8bb31b697511fa8e5d4edb2b8bcaf5b0a9a6
Justin Anderson <jander@mit.edu>
2011-10-05 23:52:10 -0400

    Fix LLVM warnings for singletons. -(oneway void)release;

--- Modules/Emergency/EmergencyData.m
+++ Modules/Emergency/EmergencyData.m
@@ -44,7 +44,7 @@ static EmergencyData *sharedEmergencyData = nil;
     return UINT_MAX;  //denotes an object that cannot be released
 }
 
-- (void)release {
+- (oneway void)release {
     //do nothing
 }
 
