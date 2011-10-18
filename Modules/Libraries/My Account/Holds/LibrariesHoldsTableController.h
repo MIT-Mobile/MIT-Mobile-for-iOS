d7ec7cb5a01c06ed913cb861180b8cd1a0a14e7f
Blake Skinner <bskinner@mit.edu>
2011-10-18 15:33:50 -0400

    *Added the headerView property to setup, return and manage the proper header for the tab
    *Added a check in the code to prevent updating the
    account information if it has been less than 15 seconds
    since the last update. This should probably be changed
    to a 'pull-to-refresh' scheme in the future

--- Modules/Libraries/My Account/Holds/LibrariesHoldsTableController.h
+++ Modules/Libraries/My Account/Holds/LibrariesHoldsTableController.h
@@ -1,8 +1,10 @@
 #import <Foundation/Foundation.h>
+#import "LibrariesHoldsSummaryView.h"
 
 @interface LibrariesHoldsTableController : NSObject <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic,retain) UIViewController *parentController;
 @property (nonatomic,retain) UITableView *tableView;
+@property (nonatomic,retain) LibrariesHoldsSummaryView* headerView;
 
 - (id)initWithTableView:(UITableView*)tableView;
 
