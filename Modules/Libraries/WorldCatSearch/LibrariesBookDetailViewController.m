7ce096bd763722055b72392fd1167b930843a3da
Jim Kang <jim.kang@modolabs.com>
2011-10-26 20:00:14 -0400

    First pass at implementing new book detail view design.

--- Modules/Libraries/WorldCatSearch/LibrariesBookDetailViewController.m
+++ Modules/Libraries/WorldCatSearch/LibrariesBookDetailViewController.m
@@ -3,16 +3,39 @@
 #import "MITMobileWebAPI.h"
 #import "MITUIConstants.h"
 #import "LibrariesModule.h"
+#import "Foundation+MITAdditions.h"
+
 
 #define TITLE_ROW 0
 #define YEAR_AUTHOR_ROW 1
 #define ISBN_ROW 2
+
+static const CGFloat kWebViewHeight = 300.0f;
+
+typedef enum 
+{
+//    kInfoSection = 0,
+    kEmailAndCiteSection = 0
+}
+BookDetailSections;
+
+typedef enum
+{
+    kWebViewTag = 0x438
+}
+BookDetailViewTags;
+
 #define HORIZONTAL_MARGIN 10
-#define VERITCAL_PADDING 5
+#define VERTICAL_PADDING 5
+#define HORIZONTAL_PADDING 5
 
 @interface LibrariesBookDetailViewController (Private)
 - (void)loadBookDetails;
 - (void)updateUI;
+- (void)configureCell:(UITableViewCell *)cell 
+    forRowAtIndexPath:(NSIndexPath *)indexPath;
+- (NSString *)infoHeaderHtml;
+
 @end
 
 @implementation LibrariesBookDetailViewController
@@ -20,9 +43,9 @@
 @synthesize activityView;
 @synthesize loadingStatus;
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+- (id)init
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
         // Custom initialization
     }
@@ -57,6 +80,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.tableView.backgroundColor = [UIColor clearColor];
     self.activityView = [[[MITLoadingActivityView alloc] initWithFrame:self.view.bounds] autorelease];
     self.activityView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
     [self.view addSubview:self.activityView];
@@ -112,11 +136,31 @@
     return authorYearSize.height;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    if (self.loadingStatus == BookLoadingStatusCompleted) {
-        NSInteger rows = 2;
-        if ([self.book isbn]) {
-            rows++;
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    NSInteger sections = 2;
+    // TODO: When available libraries are included in the book object, use a 
+    // section for each of them.
+    sections++;
+    return sections;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView 
+ numberOfRowsInSection:(NSInteger)section {
+    if (self.loadingStatus == BookLoadingStatusCompleted) {        
+        NSInteger rows = 0;
+        if (kEmailAndCiteSection == section)
+        {
+            rows = 1;
+        }
+        else
+        {
+            // This will be one of the libraries sections.
+            // TODO: When libraries info is available, in the book object, 
+            // check to see if the library corresponding to the section is 
+            // MIT. If so, there should be two rows: one for Request Item, 
+            // and one for number of copies.
+            rows = 1;
         }
         return rows;
     }
@@ -124,82 +168,157 @@
     return 0;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    switch (indexPath.row) {
-        case TITLE_ROW:
-        {
-            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Title"];
-            if (!cell) {
-                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Title"];
-                UILabel *label = [[[UILabel alloc] initWithFrame:CGRectMake(HORIZONTAL_MARGIN, VERITCAL_PADDING, 
-                                                                            tableView.frame.size.width - 2*HORIZONTAL_MARGIN, [self titleHeight:tableView])] autorelease];
-                label.numberOfLines = 0;
-                label.font = [UIFont fontWithName:STANDARD_FONT size:CELL_STANDARD_FONT_SIZE];
-                label.textColor = CELL_STANDARD_FONT_COLOR;
-                label.text = self.book.title;
-                [cell.contentView addSubview:label];
-            }
-            return cell;
-        }
-            
-        case YEAR_AUTHOR_ROW:
-        {
-            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AuthorYear"];
-            if (!cell) {
-                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"AuthorYear"];
-                UILabel *label = [[[UILabel alloc] initWithFrame:CGRectMake(HORIZONTAL_MARGIN, VERITCAL_PADDING, 
-                                                                            tableView.frame.size.width - 2*HORIZONTAL_MARGIN , [self authorYearHeight:tableView])] autorelease];
-                label.numberOfLines = 0;
-                label.font = [UIFont fontWithName:STANDARD_FONT size:CELL_DETAIL_FONT_SIZE];
-                label.textColor = CELL_DETAIL_FONT_COLOR;
-                label.text = [self.book authorYear];
-                [cell.contentView addSubview:label];
-            }
-            return cell;
-        }            
+- (void)configureCell:(UITableViewCell *)cell 
+    forRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    if (kEmailAndCiteSection == indexPath.section) 
+    {
+        // TODO: Mail accessory view.
+        cell.accessoryType = UITableViewCellAccessoryNone;
+        cell.textLabel.text = @"Email & Cite Item";
+    }
+    else
+    {
+        // This will be one of the libraries sections.
+        cell.textLabel.text = @"View Holdings";
+        cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    }
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView 
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    UITableViewCell *cell = 
+    [tableView dequeueReusableCellWithIdentifier:@"Cell"];
+    
+    if (!cell)
+    {
+        cell = 
+        [[[UITableViewCell alloc] 
+          initWithStyle:UITableViewCellStyleDefault 
+          reuseIdentifier:@"Cell"] autorelease];
+    }
+    [self configureCell:cell forRowAtIndexPath:indexPath];
         
-        case ISBN_ROW:
-        {
-            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ISBN"];
-            if (!cell) {
-                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"ISBN"];
-                UIFont *fieldFont = [UIFont fontWithName:BOLD_FONT size:CELL_DETAIL_FONT_SIZE];
-                CGSize fieldSize = [@"ISBN:" sizeWithFont:fieldFont];
-                
-                UILabel *fieldLabel = [[[UILabel alloc] initWithFrame:CGRectMake(HORIZONTAL_MARGIN, VERITCAL_PADDING, 
-                                                                            fieldSize.width, fieldSize.height)] autorelease];
-                fieldLabel.text = @"ISBN:";
-                fieldLabel.font = fieldFont;
-                
-                UILabel *valueLabel = [[[UILabel alloc] initWithFrame:CGRectMake(HORIZONTAL_MARGIN + fieldSize.width + 5, VERITCAL_PADDING, 200, fieldSize.height)] autorelease];
-                valueLabel.font = [UIFont fontWithName:STANDARD_FONT size:CELL_DETAIL_FONT_SIZE];
-                valueLabel.text = [self.book isbn];
-                
-                [cell.contentView addSubview:fieldLabel];
-                [cell.contentView addSubview:valueLabel];
-            }
-            return cell;
-        }
-            
-            
-        default:
-            break;
+    return cell;
+}
+
+- (UIView *)tableView:(UITableView *)tableView 
+viewForHeaderInSection:(NSInteger)section
+{
+    if (section == kEmailAndCiteSection)
+    {
+        UIWebView *webView = 
+        [[[UIWebView alloc] initWithFrame:
+          CGRectMake(HORIZONTAL_PADDING, VERTICAL_PADDING, 
+                     tableView.frame.size.width - 2 * HORIZONTAL_PADDING, 
+                     kWebViewHeight)] autorelease];
+        //                webView.delegate = self;      
+        // Make web view background transparent.
+        webView.backgroundColor = [UIColor clearColor];
+        webView.opaque = NO;
+        webView.tag = kWebViewTag;
+        
+        [webView 
+         loadHTMLString:[self infoHeaderHtml] baseURL:nil];
+        
+        // The web view is not wrapped in another view, it won't be 
+        // transparent.
+        UIView *wrapperView = 
+        [[[UIView alloc] initWithFrame:
+          CGRectMake(0, 0, tableView.frame.size.width, kWebViewHeight)] 
+         autorelease];
+        wrapperView.opaque = NO;
+        wrapperView.backgroundColor = [UIColor clearColor];
+        [wrapperView addSubview:webView];
+        
+        return wrapperView;        
     }
     return nil;
 }
-    
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    switch (indexPath.row) {
-        case TITLE_ROW:
-            return [self titleHeight:tableView] + 2*VERITCAL_PADDING;
-            
-        case YEAR_AUTHOR_ROW:
-            return [self authorYearHeight:tableView] + 2*VERITCAL_PADDING;
-        default:
-            break;
+
+- (NSString *)tableView:(UITableView *)tableView 
+titleForHeaderInSection:(NSInteger)section
+{
+    if (kEmailAndCiteSection == section)
+    {
+        return nil;
     }
-    return 25;
-    
+    else
+    {
+        // This will be one of the libraries sections.
+        // TODO: Return name of library corresponding to section.
+        return @"MIT Libraries";
+    }
 }
+
+- (CGFloat)tableView:(UITableView *)tableView 
+heightForHeaderInSection:(NSInteger)section
+{    
+    if (kEmailAndCiteSection == section)
+    {
+        return kWebViewHeight;
+    }
+    else
+    {
+        // This will be one of the libraries sections.
+        return 44.0f;
+    }
+}
+
+#pragma mark Web view stuff
+
++ (NSString *)nonEmptyString:(NSString *)string
+{
+    if (!string)
+    {
+        return @"";
+    }
+    return string;
+}
+
+- (NSString *)infoHeaderHtml
+{
+    NSURL *baseURL = 
+    [NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory:YES];
+    NSURL *fileURL = 
+    [NSURL URLWithString:@"libraries/book_detail.html" relativeToURL:baseURL];
+    NSError *error;
+    NSMutableString *target = 
+    [NSMutableString 
+     stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
+    if (!target) 
+    {
+        ELog(@"Failed to load template at %@. %@", fileURL, [error userInfo]);
+    }
+     
+    
+    [target 
+     replaceOccurrencesOfStrings:
+     [NSArray arrayWithObjects:
+      @"__TITLE__", @"__AUTHORS__", @"__FORMAT__", @"__SUMMARY__",
+      @"__PUBLISHER__", @"__DATE__", @"__EDITION__", @"__DESCRIPTION__", 
+      @"__ISBN__", nil]
+     withStrings:
+     [NSArray arrayWithObjects:
+      [[self class] nonEmptyString:self.book.title], 
+      [[self class] nonEmptyString:
+       [self.book.authors componentsJoinedByString:@", "]],
+      @"Book", 
+      [[self class] nonEmptyString:
+       [self.book.summarys componentsJoinedByString:@"; "]],
+      [[self class] nonEmptyString:
+       [self.book.publishers componentsJoinedByString:@", "]], 
+      @"Date",
+      [[self class] nonEmptyString:
+       [self.book.years componentsJoinedByString:@", "]], 
+      @"Description",
+      [[self class] nonEmptyString:[self.book isbn]], 
+      nil] 
+     options:NSLiteralSearch];
+    
+    return target;
+}
+
 
 @end
