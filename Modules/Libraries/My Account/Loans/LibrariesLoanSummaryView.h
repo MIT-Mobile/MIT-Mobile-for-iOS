2e6c5cdb7fed5a0cd8b5b71ef804928a484d1e08
Blake Skinner <bskinner@chaoticminds.org>
2011-11-08 15:28:04 -0500

    Make the superclass of the various My Account summary views be the MITGradientView instead of UIView. This was changed in order to directly add the view to the top-level of the tab view hierarchy instead of having Tabs->UIView->Summary. This will also make the views a bit more customizable

--- Modules/Libraries/My Account/Loans/LibrariesLoanSummaryView.h
+++ Modules/Libraries/My Account/Loans/LibrariesLoanSummaryView.h
@@ -1,6 +1,7 @@
 #import <UIKit/UIKit.h>
+#import "MITGradientView.h"
 
-@interface LibrariesLoanSummaryView : UIView
+@interface LibrariesLoanSummaryView : MITGradientView
 @property (nonatomic,retain) UIBarButtonItem* renewButton;
 @property (nonatomic) UIEdgeInsets edgeInsets;
 @property (nonatomic,retain) NSDictionary* accountDetails;
