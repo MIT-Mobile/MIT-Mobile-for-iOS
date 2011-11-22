3ba82649c4e3a95107e91e8d275bfdfc654ced66
Justin Anderson <jander@mit.edu>
2011-11-22 16:32:07 -0500

    Add a footer to the Tell form. Still needs a "Suggested Purchase" external link button.

--- Modules/Libraries/EmailForms/LibrariesTellUsViewController.m
+++ Modules/Libraries/EmailForms/LibrariesTellUsViewController.m
@@ -14,16 +14,21 @@
 }
 
 - (NSArray *)formGroups {
-    return [NSArray arrayWithObject:
-            [LibraryFormElementGroup 
-             groupForName:nil
-             elements:[NSArray arrayWithObjects:
-                       [self statusMenuFormElementWithRequired:NO],
-                       [[[TextAreaLibraryFormElement alloc] 
-                         initWithKey:@"feedback" 
-                         displayLabel:@"Feedback" 
-                         required:YES] autorelease],
-                       nil]]];
+    LibraryFormElementGroup *feedbackGroup = 
+    [LibraryFormElementGroup groupForName:nil
+                                 elements:[NSArray arrayWithObjects:
+                                           [self statusMenuFormElementWithRequired:NO],
+                                           [[[TextAreaLibraryFormElement alloc] 
+                                             initWithKey:@"feedback" 
+                                             displayLabel:@"Feedback" 
+                                             required:YES] autorelease],
+                                           nil]];
+    
+    feedbackGroup.footerText = @"Please let us know your thoughts for improving our services. We'd also appreciate hearing what you like about our current services."
+    "\n"
+    "\n"
+    "If you would like to suggest a purchase for our collections, please see the Suggested Purchase form.";
+    return [NSArray arrayWithObject:feedbackGroup];
 }
 
 @end
