89509de9af2676456cb9c30ea6a3bdcc4ce67d45
Justin Anderson <jander@mit.edu>
2011-11-22 23:50:34 -0500

    Helper text for the Request an Appointment screen.

--- Modules/Libraries/EmailForms/LibrariesAppointmentViewController.m
+++ Modules/Libraries/EmailForms/LibrariesAppointmentViewController.m
@@ -19,48 +19,52 @@
       initWithKey:@"phone" displayLabel:@"Phone" required:NO] 
      autorelease];
     phoneElement.keyboardType = UIKeyboardTypePhonePad;
-
+    
+    LibraryFormElementGroup *researchGroup =
+    [LibraryFormElementGroup groupForName:@"Research Info" elements:
+     [NSArray arrayWithObjects:
+      [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"subject" 
+                                                   displayLabel:@"Topic" 
+                                                       required:YES] autorelease],
+      
+      [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"timeframe" 
+                                                   displayLabel:@"Timeframe" 
+                                                       required:YES] autorelease],
+      
+      [[[TextAreaLibraryFormElement alloc] initWithKey:@"description" 
+                                          displayLabel:@"How can we help you?"
+                                              required:YES] autorelease],
+      
+      [[[MenuLibraryFormElement alloc] initWithKey:@"why" 
+                                      displayLabel:@"Purpose" 
+                                          required:NO 
+                                            values:[NSArray arrayWithObjects:@"Course", @"Thesis", @"Research", nil]] autorelease],
+      
+      [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"course" 
+                                                   displayLabel:@"Course" 
+                                                       required:NO] autorelease],
+      
+      [[[MenuLibraryFormElement alloc] initWithKey:@"topic" 
+                                      displayLabel:@"Subject" 
+                                          required:YES 
+                                            values:[NSArray arrayWithObjects:
+                                                    @"General",
+                                                    @"Art, Architecture & Planning",
+                                                    @"Engineering & Computer Science",
+                                                    @"GIS",
+                                                    @"Humanities",
+                                                    @"Management & Business",
+                                                    @"Science",
+                                                    @"Social Sciences",
+                                                    @"Urban Planning", nil]] autorelease],
+      
+      nil]];
+    
+    researchGroup.footerText = @"Your request will be sent to the appropriate person, based on your choice of subject.";
+    
     return [NSArray arrayWithObjects:
-        [LibraryFormElementGroup groupForName:@"Research Info" elements:[NSArray arrayWithObjects:
-                                                                    // Temporary.
-            [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"subject" 
-                                            displayLabel:@"Topic" 
-                                                required:YES] autorelease],
+        researchGroup,
             
-            [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"timeframe" 
-                                            displayLabel:@"Timeframe" 
-                                                required:YES] autorelease],
-                                                                    
-            [[[TextAreaLibraryFormElement alloc] initWithKey:@"description" 
-                                            displayLabel:@"How can we help you?" 
-                                        displayLabelSubtitle:@"Describe the information you're looking for and the research you've already done."
-                                                required:YES] autorelease],
-                                                                    
-            [[[MenuLibraryFormElement alloc] initWithKey:@"why" 
-                                            displayLabel:@"Purpose" 
-                                                required:NO 
-                                                  values:[NSArray arrayWithObjects:@"Course", @"Thesis", @"Research", nil]] autorelease],
-                                                                    
-            [[[DedicatedViewTextLibraryFormElement alloc] initWithKey:@"course" 
-                                            displayLabel:@"Course" 
-                                                required:NO] autorelease],
-                                                                    
-            [[[MenuLibraryFormElement alloc] initWithKey:@"topic" 
-                                            displayLabel:@"Subject" 
-                                                required:YES 
-                                                  values:[NSArray arrayWithObjects:
-                                                          @"General",
-                                                          @"Art, Architecture & Planning",
-                                                          @"Engineering & Computer Science",
-                                                          @"GIS",
-                                                          @"Humanities",
-                                                          @"Management & Business",
-                                                          @"Science",
-                                                          @"Social Sciences",
-                                                          @"Urban Planning", nil]] autorelease],
-                                                                
-            nil]],
-        
         [LibraryFormElementGroup groupForName:@"Personal Info" elements:[NSArray arrayWithObjects:
             [self statusMenuFormElementWithRequired:YES],            
             [[[TextLibraryFormElement alloc] initWithKey:@"department" displayLabel:@"Department, Lab, or Center" required:YES] autorelease],
