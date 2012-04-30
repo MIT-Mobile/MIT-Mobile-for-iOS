c7436fd0e8306529d4e8f1b0b87b0d68f79337f6
Blake Skinner <bskinner@mit.edu>
2012-04-30 14:47:33 -0400

    Category for making working with NSMutableStrings a bit easier to work with.

---
+++ Common/NSMutableAttributedString+MITAdditions.h
@@ -0,0 +1,22 @@
+#import <UIKit/UIKit.h>
+#import <CoreText/CoreText.h>
+
+@interface NSMutableAttributedString (MITAdditions)
+
+- (NSRange)appendString:(NSString *)string
+            withFont:(UIFont*)font
+           textColor:(UIColor *)color;
+
+- (void)setFont:(UIFont *)font
+     withTraits:(CTFontSymbolicTraits)traits
+       forRange:(NSRange)attrRange;
+
+- (void)setUnderline:(CTUnderlineStyle)underlineStyle
+           withColor:(UIColor *)underlineColor
+            forRange:(NSRange)attrRange;
+
+- (void)setLineBreakStyle:(UILineBreakMode)lineBreakMode
+            textAlignment:(UITextAlignment)textAlignment
+                 forRange:(NSRange)attrRange;
+
+@end
---
+++ Common/NSMutableAttributedString+MITAdditions.m
@@ -0,0 +1,169 @@
+#import "NSMutableAttributedString+MITAdditions.h"
+
+static inline CTFontRef createCTFontFromUIFont(UIFont *uiFont);
+
+static inline UIFont *UIFontForCTFont(CTFontRef ctFont);
+
+static inline CTLineBreakMode CTLineBreakFromUILineBreak(UILineBreakMode lineBreakMode);
+
+
+static inline CTFontRef createCTFontFromUIFont(UIFont *uiFont) {
+    CTFontRef ctFont = NULL;
+
+    if (uiFont)
+    {
+        ctFont = CTFontCreateWithName((CFStringRef)(uiFont.fontName),
+                                      uiFont.pointSize,
+                NULL);
+    }
+
+    return ctFont;
+}
+
+static inline UIFont *UIFontForCTFont(CTFontRef ctFont) {
+    NSString *psFontName = [(NSString *)CTFontCopyName(ctFont, kCTFontPostScriptNameKey) autorelease];
+    CGFloat ptSize = CTFontGetSize(ctFont);
+
+    return [UIFont fontWithName:psFontName
+                           size:ptSize];
+}
+
+static inline CTLineBreakMode CTLineBreakFromUILineBreak(UILineBreakMode lineBreakMode) {
+    switch (lineBreakMode)
+    {
+        case UILineBreakModeWordWrap:
+            return kCTLineBreakByWordWrapping;
+        case UILineBreakModeCharacterWrap:
+            return kCTLineBreakByCharWrapping;
+        case UILineBreakModeClip:
+            return kCTLineBreakByClipping;
+        case UILineBreakModeHeadTruncation:
+            return kCTLineBreakByTruncatingHead;
+        case UILineBreakModeTailTruncation:
+            return kCTLineBreakByTruncatingTail;
+        case UILineBreakModeMiddleTruncation:
+            return kCTLineBreakByTruncatingMiddle;
+        default:
+            return kCTLineBreakByWordWrapping;
+    }
+}
+
+static inline CTTextAlignment CTTextAlignmentFromUITextAlignment(UITextAlignment textAlignment) {
+    switch (textAlignment)
+    {
+        case UITextAlignmentLeft:
+            return kCTLeftTextAlignment;
+        case UITextAlignmentRight:
+            return kCTRightTextAlignment;
+        case UITextAlignmentCenter:
+            return kCTCenterTextAlignment;
+        default:
+            return kCTLeftTextAlignment;
+    }
+}
+
+@implementation NSMutableAttributedString (MITAdditions)
+- (NSRange)appendString:(NSString *)string
+               withFont:(UIFont *)font
+              textColor:(UIColor *)color
+{
+    NSRange stringRange = NSMakeRange([self length] - 1, [string length]);
+
+    [[self mutableString] appendString:string];
+    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+
+    CTFontRef ctFont = createCTFontFromUIFont(font);
+    [attrs setObject:(id)ctFont
+              forKey:(id)kCTFontAttributeName];
+    CFRelease(ctFont);
+
+    CGColorRef textColor = [color CGColor];
+    [attrs setObject:(id)[color CGColor]
+              forKey:(id)kCTForegroundColorAttributeName];
+
+    [self setAttributes:attrs
+                  range:stringRange];
+
+    return stringRange;
+}
+
+- (void)setFont:(UIFont *)font
+     withTraits:(CTFontSymbolicTraits)traits
+       forRange:(NSRange)attrRange
+{
+    CTFontRef ctFont = createCTFontFromUIFont(font);
+    CTFontRef styledFont = CTFontCreateCopyWithSymbolicTraits(ctFont,
+                                                              0.0,
+            NULL,
+                                                              traits,
+                                                              traits);
+
+    if (styledFont == NULL)
+    {
+        ELog(@"Error: Unable to create font '%@' with traits 0x%x", [font fontName], traits);
+
+        CFRelease(ctFont);
+        ctFont = styledFont;
+        styledFont = NULL;
+    }
+
+    [self addAttribute:(id)ctFont
+                 value:(NSString *)kCTFontAttributeName
+                 range:attrRange];
+
+    CFRelease(ctFont);
+}
+
+- (void)setUnderline:(CTUnderlineStyle)underlineStyle
+           withColor:(UIColor *)underlineColor
+            forRange:(NSRange)attrRange
+{
+    [self beginEditing];
+
+    if (underlineColor)
+    {
+        [self addAttribute:(NSString *)kCTUnderlineColorAttributeName
+                     value:(id)[underlineColor CGColor]
+                     range:attrRange];
+    }
+
+    [self addAttribute:(NSString *)kCTUnderlineStyleAttributeName
+                 value:[NSNumber numberWithInteger:underlineStyle]
+                 range:attrRange];
+
+    [self endEditing];
+}
+
+
+- (void)setLineBreakStyle:(UILineBreakMode)lineBreakMode
+            textAlignment:(UITextAlignment)textAlignment
+                 forRange:(NSRange)attrRange
+{
+    CTParagraphStyleSetting styleSettings[2];
+    CTParagraphStyleSetting *pssPtr = NULL;
+
+    CTLineBreakMode ctLineBreakMode = CTLineBreakFromUILineBreak(lineBreakMode);
+    pssPtr = &(styleSettings[0]);
+    {
+        pssPtr->spec = kCTParagraphStyleSpecifierLineBreakMode;
+        pssPtr->valueSize = sizeof(CTLineBreakMode);
+        pssPtr->value = &ctLineBreakMode;
+    }
+
+
+    CTTextAlignment ctAlignment = CTTextAlignmentFromUITextAlignment(textAlignment);
+    pssPtr = &(styleSettings[1]);
+    {
+        pssPtr->spec = kCTParagraphStyleSpecifierAlignment;
+        pssPtr->valueSize = sizeof(CTTextAlignment);
+        pssPtr->value = &ctAlignment;
+    }
+
+    CTParagraphStyleRef paragraphStyle = CTParagraphStyleCreate(styleSettings, 2);
+    [self addAttribute:(NSString *)kCTParagraphStyleAttributeName
+                 value:(id)paragraphStyle
+                 range:attrRange];
+
+    CFRelease(paragraphStyle);
+}
+@end
