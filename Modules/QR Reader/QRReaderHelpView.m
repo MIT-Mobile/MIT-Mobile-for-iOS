de9690d242b150c2f79b998d6e1cbc954a0ebaed
Blake Skinner <bskinner@mit.edu>
2012-07-12 15:38:38 -0400

    Make sure the background is laid out using the bounds of the view, not its frame.

--- Modules/QR Reader/QRReaderHelpView.m
+++ Modules/QR Reader/QRReaderHelpView.m
@@ -7,19 +7,19 @@
 @implementation QRReaderHelpView
 @synthesize helpView = _helpView;
 
+- (id)init
+{
+    return [self initWithFrame:CGRectZero];
+}
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
+    
     if (self) {
+        self.autoresizesSubviews = YES;
         
-        NSString *fileName = nil;
-        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
-            fileName = @"qr-reader-help";
-        } else {
-            fileName = @"qr-reader-help-no-camera";
-        }
-        
-        NSString *path = [[NSBundle mainBundle] pathForResource:fileName
+        NSString *path = [[NSBundle mainBundle] pathForResource:@"qr-reader-help-no-camera"
                                                          ofType:@"html"
                                                     inDirectory:@"qrreader"];
         self.helpView = [[[UIWebView alloc] initWithFrame:self.bounds] autorelease];
@@ -29,12 +29,18 @@
                               baseURL:[NSURL fileURLWithPath:[[NSBundle mainBundle] bundlePath]]];
         self.helpView.backgroundColor = [UIColor clearColor];
         self.helpView.opaque = NO;
+        self.helpView.autoresizingMask = (UIViewAutoresizingFlexibleHeight |
+                                       UIViewAutoresizingFlexibleWidth);
         
-        UIImageView *background = [[[UIImageView alloc] initWithFrame:frame] autorelease];
+        UIImageView *background = [[[UIImageView alloc] initWithFrame:self.bounds] autorelease];
         background.image = [UIImage imageNamed:@"global/body-background"];
+        background.autoresizingMask = (UIViewAutoresizingFlexibleHeight |
+                                       UIViewAutoresizingFlexibleWidth);
         [self addSubview:background];
         [self addSubview:self.helpView];
         self.userInteractionEnabled = NO;
+        
+        [self setNeedsLayout];
     }
     return self;
 }
