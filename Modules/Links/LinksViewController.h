d1abbe5ec98b0ce8aa833b0c5229f64e816a7c7b
Austin Emmons <austin.emmons@modolabs.com>
2012-08-22 11:10:49 -0400

    [IPHONEAPP-612]
    Created links module
    	uses grouped table view to display sections of links.

--- Modules/Links/LinksViewController.h
+++ Modules/Links/LinksViewController.h
@@ -2,19 +2,33 @@
 #import "MITMobileWebApi.h"
 #import "MITLoadingActivityView.h"
 
-@interface LinksViewController : UIViewController <JSONLoadedDelegate>
+@interface LinksViewController : UIViewController <JSONLoadedDelegate, UITableViewDataSource, UITableViewDelegate>
 {
-
+    UITableView *table;
     BOOL requestWasDispatched;
 	MITMobileWebAPI *api;
     MITLoadingActivityView *_loadingView;
     
 }
 
-@property (nonatomic, retain) NSArray * linkResults;
+@property (nonatomic, retain) NSArray * linkResults;           // array holds sections of links
 
 - (void)request:(MITMobileWebAPI *)request jsonLoaded:(id)JSONObject;
 - (BOOL)request:(MITMobileWebAPI *)request shouldDisplayStandardAlertForError: (NSError *)error;
 
+
+// Table View Data Source
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section;
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath;
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView;
+
+// Table View
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath;
+
+- (UIView *) tableView: (UITableView *)tableView viewForHeaderInSection:(NSInteger)section;
+
+- (CGFloat)tableView: (UITableView *)tableView heightForHeaderInSection:(NSInteger)section;
 
 @end
--- Modules/Links/LinksViewController.m
+++ Modules/Links/LinksViewController.m
@@ -1,12 +1,27 @@
 
 #import "LinksViewController.h"
+#import "UIKit+MITAdditions.h"
+#import "MITUIConstants.h"
 
 @interface LinksViewController ()
 
 @end
 
+
+#define kLinksKeySectionTitle @"title"
+#define kLinksKeySectionLinks @"links"
+#define kLinksKeyLinkUrl @"link"
+#define kLinksKeyLinkTitle @"name"
+
 @implementation LinksViewController
-@synthesize linkResults;
+@synthesize linkResults = _linkResults;
+
+- (void) dealloc
+{
+    [table release];
+    [_linkResults release];
+    [super dealloc];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -22,7 +37,15 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.title = @"Links";
     
+    table = [[UITableView alloc]    initWithFrame:CGRectInset(self.view.bounds, 0, 0)
+                                        style:UITableViewStyleGrouped];
+    table.delegate = self;
+    table.dataSource = self;
+    [table setBackgroundColor:[UIColor clearColor]];
+    
+    [self.view addSubview:table];
 }
 
 - (void) viewDidAppear:(BOOL)animated
@@ -43,6 +66,7 @@
 
 - (void) showLoadingView {
     _loadingView = [[MITLoadingActivityView alloc] initWithFrame:CGRectInset(self.view.frame, 0, 0)];
+    _loadingView.usesBackgroundImage = NO;
     [self.view addSubview:_loadingView];
 }
 
@@ -54,7 +78,11 @@
     if (requestWasDispatched) {
         [self showLoadingView];
     }
-    
+}
+
+- (void) reloadTableView
+{
+    [table reloadData];
 }
 
 #pragma mark - JSONLoadedDelegate
@@ -67,11 +95,12 @@
 - (void)request:(MITMobileWebAPI *)request jsonLoaded:(id)result
 {
     [self cleanUpConnection];
-    
+    NSLog(@"Results Log     ::  \n%@", result);
     if (result && [result isKindOfClass:[NSArray class]]) {
-		self.linkResults = result;
+		_linkResults = [result copy];
+        [self reloadTableView];
 	} else {
-		self.linkResults = nil;
+		_linkResults = nil;
 	}
 }
 
@@ -80,5 +109,67 @@
     return false;
 }
 
+#pragma mark - Table View Data Source Delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    if (_linkResults) {
+        NSDictionary * sectionDict = [_linkResults objectAtIndex:section];
+        return [[sectionDict objectForKey:kLinksKeySectionLinks] count];
+    }
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSString *reuseID = @"links_cell";
+    UITableViewCell *cell = nil;
+    cell = [tableView dequeueReusableCellWithIdentifier:reuseID];
+    if (!cell) {
+        cell = [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:reuseID] autorelease];
+    }
+    
+    NSDictionary *section = [_linkResults objectAtIndex:indexPath.section];
+    NSArray *links = [section objectForKey:kLinksKeySectionLinks];
+    NSDictionary *currentLink = [links objectAtIndex:indexPath.row];
+    
+    cell.textLabel.text = [currentLink objectForKey:kLinksKeyLinkTitle];
+    cell.detailTextLabel.text = [currentLink objectForKey:kLinksKeyLinkUrl];
+    
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    return cell;
+}
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    if (_linkResults) {
+        return [_linkResults count];
+    }
+    return 0;
+}
+
+- (UIView *) tableView: (UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    if (_linkResults) {
+        NSString *headerTitle = [[_linkResults objectAtIndex:section] objectForKey:kLinksKeySectionTitle];
+        return [UITableView groupedSectionHeaderWithTitle:headerTitle];
+    }
+    return nil;
+}
+
+- (CGFloat)tableView: (UITableView *)tableView heightForHeaderInSection:(NSInteger)section {
+	return GROUPED_SECTION_HEADER_HEIGHT;
+}
+
+#pragma mark - Table View Delegate
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    
+    NSDictionary *section = [_linkResults objectAtIndex:indexPath.section];
+    NSArray *links = [section objectForKey:kLinksKeySectionLinks];
+    NSDictionary *currentLink = [links objectAtIndex:indexPath.row];
+    
+    NSString *urlString = [currentLink objectForKey:kLinksKeyLinkUrl];
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString: urlString]];
+}
 
 @end
