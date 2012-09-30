1ff4cdb327e04d1baeb261fd45ee3c5ca8e67d56
Justin Anderson <jander@mit.edu>
2012-09-30 10:47:33 -0400

    Remove Stellar from the home screen.

--- App Delegate/MIT_MobileAppDelegate+ModuleList.m
+++ App Delegate/MIT_MobileAppDelegate+ModuleList.m
@@ -37,14 +37,12 @@
     [result addObject:[[[ShuttleModule alloc] init] autorelease]];
 	[result addObject:[[[CMModule alloc] init] autorelease]];
 	[result addObject:[[[CalendarModule alloc] init] autorelease]];
-	[result addObject:[[[StellarModule alloc] init] autorelease]];
 	[result addObject:[[[PeopleModule alloc] init] autorelease]];
     [result addObject:[[[ToursModule alloc] init] autorelease]];
     [result addObject:[[[EmergencyModule alloc] init] autorelease]];
     [result addObject:[[[LibrariesModule alloc] init] autorelease]];
     [result addObject:[[[FacilitiesModule alloc] init] autorelease]];
     [result addObject:[[[QRReaderModule alloc] init] autorelease]];
-//    [result addObject:[[[MobileWebModule alloc] init] autorelease]];
     [result addObject:[[[LinksModule alloc] init] autorelease]];
     [result addObject:[[[SettingsModule alloc] init] autorelease]];
     [result addObject:[[[AboutModule alloc] init] autorelease]];
@@ -63,22 +61,6 @@
 }
 
 - (void)showModuleForTag:(NSString *)tag {
-    /*
-    if ([tag isEqualToString:MobileWebTag]) {
-        NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@/", MITMobileWebGetCurrentServerDomain()]];
-        if ([[UIApplication sharedApplication] canOpenURL:url]) {
-            [[UIApplication sharedApplication] openURL:url];
-        }
-        return;
-    }
-    
-    MITModule *module = [self moduleForTag:tag];
-    [self.rootNavigationController pushViewController:module.rootViewController
-                                             animated:YES];
-    module.hasLaunchedBegun = YES;
-    [module didAppear];
-     */
-    
     [self.springboardController pushModuleWithTag:tag];
 }
 
