fb5773d22a3c6f86ac8fec0d4b4cd5718aa59e98
Justin Anderson <jander@mit.edu>
2012-10-13 12:50:29 -0400

    Limit the home screen to portrait mode.

--- App Delegate/MITSpringboard.m
+++ App Delegate/MITSpringboard.m
@@ -250,6 +250,12 @@
     
 }
 
+#pragma mark UIViewController
+
+- (BOOL)shouldAutorotate {
+    return NO;
+}
+
 #pragma mark -
 
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
