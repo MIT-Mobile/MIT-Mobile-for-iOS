13e39b52ab27bb7a253a02868c3b0afce0b48f74
Blake Skinner <bskinner@mit.edu>
2012-11-29 11:24:46 -0500

    Added a few extra bits from MGSMapAnnotation that were used elsewhere and made them into utility functions.

--- Modules/ArcGIS Map/Core/Internal/MGSUtility.h
+++ Modules/ArcGIS Map/Core/Internal/MGSUtility.h
@@ -1,6 +1,16 @@
+#import <UIKit/UIKit.h>
 #import <ArcGIS/ArcGIS.h>
 #import <MapKit/MapKit.h>
 #import <CoreLocation/CoreLocation.h>
+#import "MGSAnnotation.h"
+
+FOUNDATION_EXTERN NSString* const MGSAnnotationAttributeKey;
+
+typedef enum _MGSGraphicType {
+    MGSGraphicDefault = 0,
+    MGSGraphicStop
+} MGSGraphicType;
 
 FOUNDATION_EXPORT AGSPoint* AGSPointFromCLLocationCoordinate(CLLocationCoordinate2D coord);
 FOUNDATION_EXPORT CLLocationCoordinate2D CLLocationCoordinateFromAGSPoint(AGSPoint *point);
+
--- Modules/ArcGIS Map/Core/Internal/MGSUtility.m
+++ Modules/ArcGIS Map/Core/Internal/MGSUtility.m
@@ -1,4 +1,5 @@
 #import "MGSUtility.h"
+NSString* const MGSAnnotationAttributeKey = @"MGSAnnotationAttribute";
 
 
 AGSPoint* AGSPointFromCLLocationCoordinate(CLLocationCoordinate2D coord)
@@ -14,4 +15,110 @@ CLLocationCoordinate2D CLLocationCoordinateFromAGSPoint(AGSPoint *point)
     AGSPoint *projectedPoint = (AGSPoint*)[geoEngine projectGeometry:point
                                                   toSpatialReference:[AGSSpatialReference spatialReferenceWithWKID:WKID_WGS84]];
     return CLLocationCoordinate2DMake(projectedPoint.y, projectedPoint.x);
+}
+
+
+AGSSymbol* AGSSymbolFromAnnotation(id<MGSAnnotation> annotation, MGSMarker *templateMarker)
+{
+    AGSSymbol *symbol = nil;
+    
+    MGSMarker *marker =nil;
+    
+    if ([annotation respondsToSelector:@selector(marker)])
+    {
+        marker = annotation.marker;
+    }
+    
+    if (marker == nil)
+    {
+        marker = templateMarker;
+    }
+    
+    UIColor *markerColor = (marker.color ?
+                            marker.color :
+                            [UIColor blueColor]);
+    
+    CGFloat markerSize = MAX(marker.size.height, marker.size.height);
+    
+    // Don't use a zero check here, floats lie
+    markerSize = (markerSize < 0.1 ? 32.0 : markerSize);
+    
+    switch (marker.style) {
+        case MGSMarkerStyleSquare:
+        {
+            AGSSimpleMarkerSymbolStyle symbolStyle = AGSSimpleMarkerSymbolStyleSquare;
+            AGSSimpleMarkerSymbol *markerSymbol = [AGSSimpleMarkerSymbol simpleMarkerSymbolWithColor:markerColor];
+            markerSymbol.style = symbolStyle;
+            markerSymbol.size = markerSize;
+            symbol = markerSymbol;
+            break;
+        }
+            
+        case MGSMarkerStyleCircle:
+        {
+            AGSSimpleMarkerSymbolStyle symbolStyle = AGSSimpleMarkerSymbolStyleCircle;
+            AGSSimpleMarkerSymbol *markerSymbol = [AGSSimpleMarkerSymbol simpleMarkerSymbolWithColor:markerColor];
+            markerSymbol.style = symbolStyle;
+            markerSymbol.size = markerSize;
+            symbol = markerSymbol;
+        }
+            break;
+            
+        case MGSMarkerStyleIcon:
+        {
+            UIImage *markerImage = marker.icon;
+            AGSPictureMarkerSymbol *pictureSymbol = [AGSPictureMarkerSymbol pictureMarkerSymbolWithImage:markerImage];
+            pictureSymbol.yoffset = (CGFloat) (ceil(markerImage.size.height / 2.0) - 1);
+            symbol = pictureSymbol;
+            break;
+        }
+            
+        case MGSMarkerStyleRemote:
+        {
+            symbol = nil;
+        }
+            
+        case MGSMarkerStylePin:
+        default:
+        {
+            AGSPictureMarkerSymbol *pictureSymbol = [AGSPictureMarkerSymbol pictureMarkerSymbolWithImageNamed:@"map_pin_complete"];
+            pictureSymbol.yoffset = (CGFloat) (ceil(pictureSymbol.image.size.height / 2.0) - 1);
+            symbol = pictureSymbol;
+            break;
+        }
+    }
+    
+    return symbol;
+}
+
+
+AGSGraphic* AGSGraphicFromAnnotation(id<MGSAnnotation> annotation, MGSGraphicType graphicType, MGSMarker *template)
+{
+    AGSGraphic *graphic = nil;
+    
+    switch(graphicType)
+    {
+        case MGSGraphicStop:
+            graphic = [AGSStopGraphic graphicWithGeometry:AGSPointFromCLLocationCoordinate(annotation.coordinate)
+                                                   symbol:[self symbolForAnnotation:annotation
+                                                                      defaultMarker:template]
+                                               attributes:[NSMutableDictionary dictionary]
+                                     infoTemplateDelegate:[MGSAnnotationInfoTemplateDelegate sharedInfoTemplate]];
+        case MGSGraphicDefault:
+        default:
+            graphic = [AGSGraphic graphicWithGeometry:AGSPointFromCLLocationCoordinate(annotation.coordinate)
+                                               symbol:[self symbolForAnnotation:annotation
+                                                                  defaultMarker:template]
+                                           attributes:[NSMutableDictionary dictionary]
+                                 infoTemplateDelegate:[MGSAnnotationInfoTemplateDelegate sharedInfoTemplate]];
+    }
+    
+    return graphic;
+}
+
++ (AGSGraphic*)graphicForAnnotation:(id<MGSAnnotation>)annotation template:(MGSMarker*)template
+{
+    return [self graphicOfType:MGSGraphicDefault
+                withAnnotation:annotation
+                      template:template];
 }
