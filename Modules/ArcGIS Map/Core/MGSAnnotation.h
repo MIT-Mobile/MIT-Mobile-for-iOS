7db4731eff69e16e143ef3a36425913d429926b3
Blake Skinner <bskinner@chaoticminds.org>
2012-11-28 20:26:37 -0500

    Added an interface for a lightweight annotation object. The MGSMapAnnotation is moving around far more data than it should be.

--- Modules/ArcGIS Map/Core/MGSAnnotation.h
+++ Modules/ArcGIS Map/Core/MGSAnnotation.h
@@ -1,13 +1,22 @@
-//
-//  MGSAnnotation.h
-//  MIT Mobile
-//
-//  Created by Blake Skinner on 11/26/12.
-//
-//
-
 #import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@class MGSMarker;
 
 @protocol MGSAnnotation <NSObject>
+@property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy) NSString *detail;
 
+@optional
+@property (nonatomic, readonly, copy) MGSMarker *marker;
+@property (nonatomic, readonly, copy) UIImage *image;
+@property (nonatomic, readonly, weak) UIView *calloutView;
+@property (nonatomic, readonly, weak) id<NSObject> userData;
+
+- (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate;
+@end
+
+@protocol MGSAnnotationView <NSObject>
+- (void)prepareForReuseWithAnnotation:(id<MGSAnnotation>) annotation;
 @end
