8fb6e1865c404ec5c6fb1d69f58ce731185df846
Blake Skinner <bskinner@chaoticminds.org>
2012-11-28 20:33:21 -0500

    Added a definition for the MGSLayerDelegate protocol

--- Modules/ArcGIS Map/Core/MGSMapLayer.h
+++ Modules/ArcGIS Map/Core/MGSMapLayer.h
@@ -4,13 +4,38 @@
 @class MGSMapCoordinate;
 @class MGSMarker;
 @class MGSMapView;
+@class MGSMapLayer;
+
+@protocol MGSAnnotation;
 
 @protocol MGSCalloutController
 - (BOOL)isPresentingCalloutForAnnotation:(MGSMapAnnotation*)annotation;
 - (UIView*)viewForAnnotation:(MGSMapAnnotation*)annotation;
 @end
 
+@protocol MGSLayerDelegate <NSObject>
+- (void)mapLayer:(MGSMapLayer*)layer willMoveToMapView:(MGSMapView*)mapView;
+- (void)mapLayer:(MGSMapLayer*)layer didMoveToMapView:(MGSMapView*)mapView;
+
+- (void)mapLayer:(MGSMapLayer*)layer willAddAnnotations:(NSSet*)annotations;
+- (void)mapLayer:(MGSMapLayer*)layer didAddAnnotations:(NSSet*)annotations;
+
+- (void)mapLayer:(MGSMapLayer*)layer willRemoveAnnotations:(NSSet*)annotations;
+- (void)mapLayer:(MGSMapLayer*)layer didRemoveAnnotations:(NSSet*)annotations;
+
+- (void)willReloadMapLayer:(MGSMapLayer*)mapLayer;
+
+- (BOOL)mapLayer:(MGSMapLayer*)layer shouldDisplayCalloutForAnnotation:(id<MGSAnnotation>)annotation;
+- (void)mapLayer:(MGSMapLayer*)layer willDisplayCalloutForAnnotation:(id<MGSAnnotation>)annotation;
+
+- (UIView*)mapLayer:(MGSMapLayer*)layer calloutViewForAnnotation:(id<MGSAnnotation>)annotation;
+- (void)mapLayer:(MGSMapLayer*)layer calloutAccessoryDidReceiveTapForAnnotation:(id<MGSAnnotation>)annotation;
+
+- (void)mapLayer:(MGSMapLayer*)layer didPresentCalloutForAnnotation:(id<MGSAnnotation>)annotation;
+@end
+
 @interface MGSMapLayer : NSObject
+@property (weak) id<MGSLayerDelegate> delegate;
 @property (strong) NSString *name;
 @property (weak,readonly) MGSMapView *mapView;
 @property (strong) UIViewController<MGSCalloutController> *calloutController;
