0da88b1cba451600975350f1b561002acb315dae
Blake Skinner <bskinner@mit.edu>
2012-12-04 13:24:26 -0500

    Added a missing type and expand on the (potential) capabilities of the annotation protocol to allow for other forms of geometry.

--- Modules/ArcGIS Map/Core/MGSAnnotation.h
+++ Modules/ArcGIS Map/Core/MGSAnnotation.h
@@ -3,15 +3,24 @@
 
 @class MGSMarker;
 
+typedef enum _MGSAnnotationType {
+    MGSAnnotationMarker = 0,
+    MGSAnnotationPoint,
+    MGSAnnotationPolyline
+} MGSAnnotationType;
+
 @protocol MGSAnnotation <NSObject>
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
-@property (nonatomic, readonly, copy) NSString *title;
-@property (nonatomic, readonly, copy) NSString *detail;
 
 @optional
-@property (nonatomic, readonly, copy) MGSMarker *marker;
+@property (nonatomic, readonly, copy) NSString *title;
+@property (nonatomic, readonly, copy) NSString *detail;
 @property (nonatomic, readonly, copy) UIImage *image;
 @property (nonatomic, readonly, weak) UIView *calloutView;
+
+@property (nonatomic, readonly, copy) MGSMarker *marker;
+@property (nonatomic, readonly) MGSAnnotationType annotationType;
+
 @property (nonatomic, readonly, weak) id<NSObject> userData;
 
 - (void)setCoordinate:(CLLocationCoordinate2D)newCoordinate;
