d9935cb99335c6a1d4b962d1023e6c36e5491c8b
Blake Skinner <bskinner@mit.edu>
2013-01-03 14:45:57 -0500

    Add some comments relating to the creating of the graphics layer and requesting graphic creation for an annotation

--- Modules/ArcGIS Map/Core/MGSLayer+Protected.h
+++ Modules/ArcGIS Map/Core/MGSLayer+Protected.h
@@ -11,8 +11,18 @@
 @property (nonatomic,strong) AGSGraphicsLayer *graphicsLayer;
 @property (nonatomic,strong) AGSRenderer *renderer;
 @property (nonatomic,readonly) BOOL hasGraphicsLayer;
-@property (nonatomic,strong) NSMutableArray *layerAnnotations;
 
+// This method is similar to -[UIViewController loadView]
+// It should create a new instance of AGSGraphicsLayer
+// (or a subclass) and assign it to the 'graphicsLayer'
+// property before returning
 - (void)loadGraphicsLayer;
+
+// Called wherever a new graphic is needed for an annotation.
+// This should allow subclasses to tweak how the AGSGraphic objects
+// are created without having to poke at the internals of MGSLayer.
+// The default implementation
+- (AGSGraphic*)loadGraphicForAnnotation:(id<MGSAnnotation>)annotation;
+
 - (MGSLayerAnnotation*)layerAnnotationForAnnotation:(id<MGSAnnotation>)annotation;
 @end
