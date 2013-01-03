0dfe52891a9e462b317bcfa574bc4c9fd33532db
Blake Skinner <bskinner@mit.edu>
2013-01-03 14:36:59 -0500

    Add a base implementation of the MGSAnnotation protocol. This class implements all of the properties and methods present in the interface.

---
+++ Modules/ArcGIS Map/Core/MGSSimpleAnnotation.h
@@ -0,0 +1,20 @@
+#import <Foundation/Foundation.h>
+#import "MGSAnnotation.h"
+
+// This class is a wrapper for an object which implements
+// all of the methods and property in the MGSAnnotation protocol
+// and wraps them so that even the optional methods
+// should return a sane value and not die
+@interface MGSSimpleAnnotation : NSObject <MGSAnnotation>
+@property (nonatomic,) CLLocationCoordinate2D coordinate;
+@property (nonatomic, assign) BOOL canShowCallout;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *detail;
+@property (nonatomic, strong) UIImage *image;
+
+@property (nonatomic, strong) UIView *annotationView;
+@property (nonatomic, strong) UIView *calloutView;
+@property (nonatomic) MGSAnnotationType annotationType;
+
+@property (nonatomic, strong) id<NSObject> userData;
+@end
---
+++ Modules/ArcGIS Map/Core/MGSSimpleAnnotation.m
@@ -0,0 +1,5 @@
+#import "MGSSimpleAnnotation.h"
+
+@implementation MGSSimpleAnnotation
+
+@end
