dd5499bbd37639ebc56a6e2a0925c79c371b6f93
Blake Skinner <bskinner@chaoticminds.org>
2013-01-05 20:08:23 -0500

    Initial implementation of a layer to display routes. There is currently a bug where the path does not appear on the map view.

--- Modules/ArcGIS Map/Core/MGSRouteLayer.h
+++ Modules/ArcGIS Map/Core/MGSRouteLayer.h
@@ -7,11 +7,12 @@
 @property (nonatomic,readonly,weak) id<MGSAnnotation> nextStop;
 @property (nonatomic,readonly,weak) id<MGSAnnotation> previousStop;
 
-@property (nonatomic,strong) NSArray *pathCoordinates;
+@property (nonatomic,readonly,strong) NSArray *pathCoordinates;
+@property (nonatomic,readonly,strong) NSArray *stopAnnotations;
 
 @property (nonatomic,assign) BOOL requireIdentification;
 @property (nonatomic,assign) BOOL requireHandicapAccess;
 
-- (id)initWithName:(NSString*)name;
+- (id)initWithName:(NSString*)name withStops:(NSArray*)stopAnnotations pathCoordinates:(NSArray*)coordinates;
 - (NSArray*)directionsFromStart:(id<MGSAnnotation>)start toEnd:(id<MGSAnnotation>)end;
 @end
--- Modules/ArcGIS Map/Core/MGSRouteLayer.m
+++ Modules/ArcGIS Map/Core/MGSRouteLayer.m
@@ -1,10 +1,15 @@
 #import <CoreFoundation/CoreFoundation.h>
+#import <CoreLocation/CoreLocation.h>
 #import "MGSRouteLayer.h"
 #import "MGSLayer+Protected.h"
+#import "MGSUtility.h"
+#import "CoreLocation+MITAdditions.h"
 
 @interface MGSRouteLayer ()
-@property (nonatomic,strong) NSDictionary *routes;
-
+@property (nonatomic,strong) NSArray *pathCoordinates;
+@property (nonatomic,strong) NSArray *stopAnnotations;
+@property (nonatomic,weak) AGSMutablePolyline *polyline;
+@property (nonatomic,weak) AGSGraphic *lineGraphic;
 @end
 
 @implementation MGSRouteLayer
@@ -14,9 +19,49 @@
     
     if (self)
     {
-        
+        self.stopAnnotations = stopAnnotations;
+        self.pathCoordinates = pathCoordinates;
     }
     
     return self;
 }
+
+
+- (void)didReloadMapLayer
+{
+    [super didReloadMapLayer];
+    
+    AGSGraphicsLayer *layer = self.graphicsLayer;
+    
+    AGSMutablePolyline *polyline = self.polyline;
+    if (self.polyline == nil)
+    {
+        polyline = [[AGSMutablePolyline alloc] initWithSpatialReference:[AGSSpatialReference wgs84SpatialReference]];
+    }
+    
+    if (polyline.numPaths > 0)
+    {
+        [polyline removePathAtIndex:0];
+    }
+    
+    [polyline addPathToPolyline];
+    
+    for (NSValue *value in self.pathCoordinates)
+    {
+        CLLocationCoordinate2D coordinate = [value MKCoordinateValue];
+        if (CLLocationCoordinate2DIsValid(coordinate))
+        {
+            [polyline addPointToPath:AGSPointFromCLLocationCoordinate(coordinate)];
+        }
+    }
+    
+    AGSGraphic *pathGraphic = [AGSGraphic graphicWithGeometry:polyline
+                                                       symbol:[AGSSimpleLineSymbol simpleLineSymbolWithColor:[UIColor redColor] width:16.0]
+                                                   attributes:[NSMutableDictionary dictionary]
+                                         infoTemplateDelegate:nil];
+    
+    [layer addGraphic:pathGraphic];
+    [layer dataChanged];
+}
+
 @end
