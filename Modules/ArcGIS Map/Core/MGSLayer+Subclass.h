6bc6f9d274443edcc3d123fc1762a46bcc1af8b1
Blake Skinner <bskinner@chaoticminds.org>
2013-02-13 10:55:56 -0500

    Renamed the category to +Subclass instead of +Protected to clearly indicate its use since 'protected' doesn't really have any meaning in ObjC

--- Modules/ArcGIS Map/Core/MGSLayer+Subclass.h
+++ Modules/ArcGIS Map/Core/MGSLayer+Subclass.h
@@ -5,23 +5,23 @@
 @class MGSLayerAnnotation;
 @protocol MGSAnnotation;
 
-@interface MGSLayer ()
+@interface MGSLayer (Subclass)
 @property (nonatomic,weak) MGSMapView *mapView;
 @property (nonatomic,strong) AGSGraphicsLayer *graphicsLayer;
-@property (nonatomic,strong) AGSRenderer *renderer;
 @property (nonatomic,readonly) BOOL hasGraphicsLayer;
 
 // This method is similar to -[UIViewController loadView]
 // It should create a new instance of AGSGraphicsLayer
 // (or a subclass) and assign it to the 'graphicsLayer'
-// property before returning
+// property before returning. If the graphicsLayer
+// property is nil after this method returns, a
+// NSInternalInconsistencyException will be thrown
 - (void)loadGraphicsLayer;
 
 // Called wherever a new graphic is needed for an annotation.
 // This should allow subclasses to tweak how the AGSGraphic objects
 // are created without having to poke at the internals of MGSLayer.
 // The default implementation
-- (AGSGraphic*)loadGraphicForAnnotation:(id<MGSAnnotation>)annotation;
-
+- (AGSGraphic*)graphicForAnnotation:(id<MGSAnnotation>)annotation;
 - (MGSLayerAnnotation*)layerAnnotationForAnnotation:(id<MGSAnnotation>)annotation;
 @end
