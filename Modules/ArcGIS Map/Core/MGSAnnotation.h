b5a0894a1efb9499d9bca061bd2a8ff6371a00d3
Blake Skinner <bskinner@chaoticminds.org>
2013-02-13 13:08:36 -0500

    Fixed up the marker type definitions so that, by default, everything starts off of as basic picture marker. This way we avoid invisible graphics on the screen

--- Modules/ArcGIS Map/Core/MGSAnnotation.h
+++ Modules/ArcGIS Map/Core/MGSAnnotation.h
@@ -8,7 +8,7 @@ typedef enum _MGSAnnotationType {
     // The maker will be the default pin unless the markerImage
     // image property returns a non-nil value. A marker is the only object
     // that can pop up a callout. By default, markers will respond to taps.
-    MGSAnnotationMarker = 0x01,
+    MGSAnnotationMarker = 0x00,
     
     // Indicates that the annotation should draw a line to the map.
     // A polyline requires that the points property return an array of
@@ -17,12 +17,12 @@ typedef enum _MGSAnnotationType {
     // If the coordinate returns a valid CLLocationCoordinate2D point (verified
     // by CLLocationCoordinate2DIsValid), then the values in the 'points'
     // property are assumed to be relative offsets from the 'coordinate'
-    MGSAnnotationPolyline = 0x02,
-    MGSAnnotationPolygon = 0x04,
+    MGSAnnotationPolyline = 0x01,
+    MGSAnnotationPolygon = 0x02,
     
     // Indicates that the annotation is a marker for some sort of
     // pre-existing data on the layer. For example, if we were to 
-    MGSAnnotationPointOfInterest = 0x08
+    MGSAnnotationPointOfInterest = 0x04
 } MGSAnnotationType;
 
 @protocol MGSAnnotation <NSObject>
