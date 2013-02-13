b59e336b4e43cb33858ce32d94fe1187d5510206
Blake Skinner <bskinner@chaoticminds.org>
2013-02-13 13:37:02 -0500

    Clean up the code for generating the marker image. Looks like the scaling but was fixed in 10.1

--- Modules/Campus Map/MITAnnotationAdaptor.m
+++ Modules/Campus Map/MITAnnotationAdaptor.m
@@ -10,7 +10,7 @@
     {
         self.mkAnnotation = annotation;
     }
-
+    
     return self;
 }
 
@@ -31,46 +31,41 @@
 
 - (UIImage*)markerImage
 {
-    __block UIImage *image = nil;
+    UIImage *image = nil;
     
     if (self.legacyAnnotationView)
     {
         UIView *annotationView = self.legacyAnnotationView;
         
-        dispatch_sync(dispatch_get_main_queue(), ^{
-            CGRect superRect = CGRectZero;
-            CGRect annotationRect = annotationView.frame;
-            superRect.size = annotationView.frame.size;
-            
-            if (annotationView.frame.origin.x < 0)
-            {
-                superRect.size.width += fabs(annotationView.frame.origin.x);
-                annotationRect.origin.x = 0.0;
-            }
-            
-            if (annotationView.frame.origin.y < 0)
-            {
-                superRect.size.height += fabs(annotationView.frame.origin.y);
-                annotationRect.origin.y = 0.0;
-            }
-            
-            UIView *superView = [[UIView alloc] initWithFrame:superRect];
-            superView.backgroundColor = [UIColor blueColor];
-            annotationView.backgroundColor = [UIColor blackColor];
-            annotationView.frame = annotationRect;
-            
-            [superView addSubview:annotationView];
-            [superView layoutIfNeeded];
-            
-            UIGraphicsBeginImageContextWithOptions(CGSizeMake(superView.frame.size.height, superView.frame.size.width), YES, 0.0);
-            CGContextRef context = UIGraphicsGetCurrentContext();
-            
-            [superView.layer.presentationLayer renderInContext:context];
-            image = UIGraphicsGetImageFromCurrentImageContext();
-            UIGraphicsEndImageContext();
-            
-            [annotationView removeFromSuperview];
-        });
+        CGRect superRect = CGRectZero;
+        CGRect annotationRect = annotationView.frame;
+        superRect.size = annotationView.frame.size;
+        
+        if (annotationView.frame.origin.x < 0)
+        {
+            superRect.size.width += fabs(annotationView.frame.origin.x);
+            annotationRect.origin.x = 0.0;
+        }
+        
+        if (annotationView.frame.origin.y < 0)
+        {
+            superRect.size.height += fabs(annotationView.frame.origin.y);
+            annotationRect.origin.y = 0.0;
+        }
+        
+        UIView *superView = [[UIView alloc] initWithFrame:superRect];
+        annotationView.frame = annotationRect;
+        
+        [superView addSubview:annotationView];
+        [superView layoutIfNeeded];
+        
+        UIGraphicsBeginImageContextWithOptions(CGSizeMake(superView.frame.size.width, superView.frame.size.height), NO, 0.0);
+        superView.layer.backgroundColor = [[UIColor clearColor] CGColor];
+        [superView.layer renderInContext:UIGraphicsGetCurrentContext()];
+        image = UIGraphicsGetImageFromCurrentImageContext();
+        UIGraphicsEndImageContext();
+        
+        [annotationView removeFromSuperview];
     }
     
     return image;
@@ -82,13 +77,13 @@
     {
         return YES;
     }
-
+    
     if ([object isKindOfClass:[self class]])
     {
         MITAnnotationAdaptor *adaptor = (MITAnnotationAdaptor*)object;
         return [self.mkAnnotation isEqual:adaptor.mkAnnotation];
     }
-
+    
     return NO;
 }
 @end
