acf061462f74bedde4147a278df2591cc4bf9be4
Blake Skinner <bskinner@mit.edu>
2013-02-15 15:14:44 -0500

    Add a few more methods to the MGSMapView delegate protocol. These may be thinned a bit in the future depending on what we use.

--- Modules/ArcGIS Map/View/MGSMapViewDelegate.h
+++ Modules/ArcGIS Map/View/MGSMapViewDelegate.h
@@ -9,9 +9,12 @@
 @optional
 - (void)didFinishLoadingMapView:(MGSMapView*)mapView;
 
+- (BOOL)mapView:(MGSMapView*)mapView shouldShowCalloutForAnnotation:(id<MGSAnnotation>)annotation;
 - (void)mapView:(MGSMapView*)mapView willShowCalloutForAnnotation:(id<MGSAnnotation>)annotation;
+- (UIView*)mapView:(MGSMapView*)mapView calloutViewForAnnotation:(id<MGSAnnotation>)annotation;
+- (void)mapView:(MGSMapView*)mapView calloutDidReceiveTapForAnnotation:(id<MGSAnnotation>)annotation;
 - (void)mapView:(MGSMapView*)mapView didShowCalloutForAnnotation:(id<MGSAnnotation>)annotation;
-- (void)mapView:(MGSMapView*)mapView calloutAccessoryDidReceiveTapForAnnotation:(id<MGSAnnotation>)annotation;
+- (void)mapView:(MGSMapView*)mapView didDismissCalloutForAnnotation:(id<MGSAnnotation>)annotation;
 
 - (void)mapView:(MGSMapView*)mapView willAddLayer:(MGSLayer*)layer;
 - (void)mapView:(MGSMapView*)mapView didAddLayer:(MGSLayer*)layer;
