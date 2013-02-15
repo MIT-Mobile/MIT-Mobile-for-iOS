efc0ac15d018584cbe49b68293c75ba5ce12751e
Blake Skinner <bskinner@mit.edu>
2013-02-15 11:58:27 -0500

    Remove all the wrapper code for the annotation; all that logic is handled by the superclass now

--- Modules/ArcGIS Map/Core/Internal/MGSLayerAnnotation.m
+++ Modules/ArcGIS Map/Core/Internal/MGSLayerAnnotation.m
@@ -28,9 +28,7 @@
     if ([super isEqual:object])
     {
         result = YES;
-    }
-    else if ([object isKindOfClass:[MGSLayerAnnotation class]])
-    {
+    } else if ([object isKindOfClass:[MGSLayerAnnotation class]]) {
         result = [self isEqualToLayerAnnotation:(MGSLayerAnnotation*)object];
     }
     
@@ -41,129 +39,6 @@
 {
     return ([self.annotation isEqual:annotation.annotation] &&
             [self.graphic isEqual:annotation.graphic]);
-}
-
-#pragma mark - AGSAnnotation wrapper delegate
-- (BOOL)canShowCallout
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation canShowCallout];
-    }
-    
-    return NO;
-}
-
-- (NSString*)title
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation title];
-    }
-    
-    return nil;
-}
-
-- (NSString*)detail
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation detail];
-    }
-    
-    return nil;
-}
-
-- (UIImage*)calloutImage
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation calloutImage];
-    }
-    
-    return nil;
-}
-
-- (UIImage*)markerImage
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation markerImage];
-    }
-    
-    return [UIImage imageNamed:@"map/map_pin_complete"];
-}
-
-- (UIView*)calloutView
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation calloutView];
-    }
-    
-    return nil;
-}
-
-- (MGSAnnotationType)annotationType
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation annotationType];
-    }
-    
-    return MGSAnnotationMarker;
-}
-
-- (id<NSObject>)userData
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation userData];
-    }
-    
-    return nil;
-}
-
-- (CLLocationCoordinate2D)coordinate
-{
-    if ([self.annotation respondsToSelector:_cmd])
-    {
-        return [self.annotation coordinate];
-    }
-    
-    return CLLocationCoordinate2DMake(0,0);
-}
-
-- (NSArray*)points {
-    if ([self.annotation respondsToSelector:_cmd]) {
-        return [self.annotation points];
-    }
-    
-    return nil;
-}
-
-- (UIColor*)strokeColor {
-    if ([self.annotation respondsToSelector:_cmd]) {
-        return [self.annotation strokeColor];
-    }
-    
-    return nil;
-}
-
-- (UIColor*)fillColor {
-    if ([self.annotation respondsToSelector:_cmd]) {
-        return [self.annotation fillColor];
-    }
-    
-    return nil;
-}
-
-- (CGFloat)lineWidth {
-    if ([self.annotation respondsToSelector:_cmd]) {
-        return [self.annotation lineWidth];
-    }
-    
-    return 0.0;
 }
 @end
 
