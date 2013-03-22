a3330f4cb953f7640bf0b4e1d1094270646331f6
Blake Skinner <bskinner@mit.edu>
2013-03-22 15:50:22 -0400

    Implement NSCopying
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Modules/ArcGIS Map/Core/MGSSimpleAnnotation.m
+++ Modules/ArcGIS Map/Core/MGSSimpleAnnotation.m
@@ -1,7 +1,6 @@
 #import "MGSSimpleAnnotation.h"
 
 @implementation MGSSimpleAnnotation
-
 - (id)init {
     self = [super init];
     
@@ -10,6 +9,28 @@
     }
     
     return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone
+{
+    MGSSimpleAnnotation *copy = [[[self class] alloc] init];
+    
+    if (copy) {
+        copy.annotationType = self.annotationType;
+        copy.calloutImage = self.calloutImage;
+        copy.coordinate = self.coordinate;
+        copy.detail = self.detail;
+        copy.fillColor = self.fillColor;
+        copy.lineWidth = self.lineWidth;
+        copy.markerImage = self.markerImage;
+        copy.markerOptions = self.markerOptions;
+        copy.points = self.points;
+        copy.strokeColor = self.strokeColor;
+        copy.title = self.title;
+        copy.userData = self.userData;
+    }
+    
+    return copy;
 }
 
 @end
