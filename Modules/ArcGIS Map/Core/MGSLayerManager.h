9dff56707d3a87c6c2ff6c737dd5593c3ca265ba
Blake Skinner <bskinner@mit.edu>
2013-03-26 11:30:24 -0400

    Hold off on this for now until I get Doxygen installed, configured and bring myself up to speed on the supported directives
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Modules/ArcGIS Map/Core/MGSLayerManager.h
+++ Modules/ArcGIS Map/Core/MGSLayerManager.h
@@ -11,24 +11,10 @@
 @end
 
 @interface MGSLayerManager : NSObject
-/**
- *  The current layer being managed.
- *
- */
+
 @property (nonatomic,readonly,strong) MGSLayer *layer;
 
-/**
- *  The backing ArcGIS graphics layer.
- *
- *  The graphics layer will be created on access. If a delegate
- *  has been set and it responds to the layerManager:graphicsLayerForLayer:
- *  selector, this property will be set to the value returned
- *  by the method.
- *
- *  @warning This property may be nil. If it is, the MGSLayer will not be
- *  added to the map
- *
- */
+
 @property (nonatomic,readonly,strong) AGSGraphicsLayer *graphicsLayer;
 @property (nonatomic,strong) NSArray *graphics;
 @property (nonatomic,strong) AGSSpatialReference *defaultSpatialReference;
