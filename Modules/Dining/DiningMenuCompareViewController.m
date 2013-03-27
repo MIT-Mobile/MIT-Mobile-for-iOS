161924f96243682841fab74514e09bcfc085c9a6
Austin Emmons <austin.emmons@modolabs.com>
2013-03-27 18:17:04 -0400

    [IPHONEAPP-642] Added simple PSTCollectionView to DiningMenuCompareViewController 	using debug data to get section count

--- Modules/Dining/DiningMenuCompareViewController.m
+++ Modules/Dining/DiningMenuCompareViewController.m
@@ -9,11 +9,18 @@
 #import "DiningMenuCompareViewController.h"
 #import "PSTCollectionView.h"
 
-@interface DiningMenuCompareViewController ()
+@interface DiningMenuCompareViewController () <PSTCollectionViewDataSource, PSTCollectionViewDelegateFlowLayout>
+
+@property (nonatomic, strong) PSTCollectionView *collectionView;
 
 @end
 
 @implementation DiningMenuCompareViewController
+
+- (NSArray *) debugHouseDiningData
+{
+    return [NSArray arrayWithObjects:@"Baker", @"The Howard Dining Hall", @"McCormick", @"Next", @"Simmons", nil];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -29,6 +36,16 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    PSTCollectionViewFlowLayout *layout = [[PSTCollectionViewFlowLayout alloc] init];
+    layout.scrollDirection = PSTCollectionViewScrollDirectionHorizontal;
+    _collectionView = [[PSTCollectionView alloc] initWithFrame:self.view.bounds collectionViewLayout:layout];
+    _collectionView.dataSource = self;
+    _collectionView.delegate = self;
+    
+    _collectionView.backgroundColor = [UIColor whiteColor];
+    _collectionView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    [_collectionView registerClass:[PSTCollectionViewCell class] forCellWithReuseIdentifier:@"cell-reuse"];
+    [self.view addSubview:_collectionView];
     
 }
 
@@ -46,5 +63,69 @@
         [self dismissModalViewControllerAnimated:YES];
     }
 }
+
+#pragma mark - PSTCollectionViewDatasource
+
+- (NSInteger)numberOfSectionsInCollectionView:(PSTCollectionView *)collectionView
+{
+    return [[self debugHouseDiningData] count];
+}
+
+- (NSInteger)collectionView:(PSTCollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
+- (PSTCollectionViewCell *)collectionView:(PSTCollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    PSTCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"cell-reuse" forIndexPath:indexPath];
+    if (!cell) {
+        cell = [[PSTCollectionViewCell alloc] init];
+    }
+    cell.backgroundColor = [UIColor greenColor];
+    
+    return cell;
+}
+
+
+#pragma mark - PSTCollectionViewDelegateFlowLayout
+
+- (CGSize)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
+{
+    return CGSizeMake(120, 50);
+}
+
+- (UIEdgeInsets)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout insetForSectionAtIndex:(NSInteger)sectio{
+    return UIEdgeInsetsMake(1, 1, 1, 1);
+}
+
+- (CGFloat)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 2;
+}
+
+- (CGFloat)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout minimumInteritemSpacingForSectionAtIndex:(NSInteger)section
+{
+    return 1;
+}
+
+- (CGSize)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout referenceSizeForHeaderInSection:(NSInteger)section
+{
+    return CGSizeZero;
+}
+
+- (CGSize)collectionView:(PSTCollectionView *)collectionView layout:(PSTCollectionViewLayout*)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section
+{
+    return CGSizeZero;
+}
+
+- (PSTCollectionViewLayoutAttributes *)layoutAttributesForSupplementaryViewOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath
+{
+    return nil;
+}
+
+
+
 
 @end
