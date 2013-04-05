0a73f2bfb1339171f2041b7fb256e2c8c719536c
Blake Skinner <bskinner@mit.edu>
2013-04-05 15:46:34 -0400

    Add a bootstrapper to handle retrieving and caching the map configuration data
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

---
+++ Common/MapCore/Internal/MGSBootstrapper.m
@@ -0,0 +1,132 @@
+#import "MGSBootstrapper.h"
+#import "MobileRequestOperation.h"
+
+@interface MGSBootstrapper ()
+@property (strong) NSOperationQueue *requestQueue;
+@property (strong) NSDictionary *cachedResponse;
+@property (strong) NSError *cachedError;
+@property (strong) NSDate *lastRetrieved;
+@property (assign) NSTimeInterval cacheExpiryInterval;
+@property (assign,getter=isRequestInFlight) BOOL requestInFlight;
+@end
+
+@implementation MGSBootstrapper
++ (id)sharedBootstrapper
+{
+    static MGSBootstrapper *bootstrapper = nil;
+    
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        bootstrapper = [[MGSBootstrapper alloc] init];
+    });
+    
+    return bootstrapper;
+}
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        self.requestQueue = [[NSOperationQueue alloc] init];
+        self.requestQueue.maxConcurrentOperationCount = 1;
+        self.cachedResponse = nil;
+        self.lastRetrieved = [NSDate distantPast];
+        self.cacheExpiryInterval = 60.0 * 5.0; // 5 Minute cache
+    }
+    
+    return self;
+}
+
+- (void)validateResponse:(id)content
+                   error:(NSError**)error
+{
+    NSString* contentClass = @"NSDictionary";
+
+    if ([content isKindOfClass:NSClassFromString(contentClass)]) {
+        NSDictionary *bootstrap = (NSDictionary*)content;
+        NSDictionary *basemaps = bootstrap[@"basemaps"];
+        
+        if ([basemaps count]) {
+            [basemaps enumerateKeysAndObjectsUsingBlock:^(NSString *setName, id obj, BOOL *stop) {
+                NSString *objectClass = @"NSArray";
+                if ([obj isKindOfClass:NSClassFromString(objectClass)] == NO) {
+                    (*stop) = YES;
+                    NSString *errorDescription = [NSString stringWithFormat:@"invalid set type, expected %@, received %@",
+                                                  objectClass, NSStringFromClass([obj class])];
+                    (*error) = [NSError errorWithDomain:NSURLErrorDomain
+                                                   code:NSURLErrorBadServerResponse
+                                               userInfo:@{ NSLocalizedDescriptionKey : errorDescription }];
+                }
+            }];
+        } else {
+            (*error) = [NSError errorWithDomain:NSURLErrorDomain
+                                           code:NSURLErrorBadServerResponse
+                                       userInfo:@{ NSLocalizedDescriptionKey : @"missing or malformed 'basemaps' key" }];
+        }
+    } else {
+        NSString *errorDescription = [NSString stringWithFormat:@"invalid content type, expected %@, received %@",
+                                      contentClass, NSStringFromClass([content class])];
+        (*error) = [NSError errorWithDomain:NSURLErrorDomain
+                                       code:NSURLErrorBadServerResponse
+                                   userInfo:@{ NSLocalizedDescriptionKey : errorDescription }];
+    }
+}
+
+
+- (void)requestBootstrap:(void (^)(NSDictionary*,NSError*))resultBlock
+{
+    void (^requestResultBlock)(NSDictionary*,NSError*) = [resultBlock copy];
+    
+    [self.requestQueue addOperationWithBlock:^{
+        NSTimeInterval interval = [[NSDate date] timeIntervalSinceDate:self.lastRetrieved];
+        BOOL needsUpdate = ((interval > self.cacheExpiryInterval) ||
+                            (self.cachedResponse == nil) ||
+                            (self.cachedError));
+        
+        if (needsUpdate && (self.requestInFlight == NO)) {
+            DDLogVerbose(@"Requesting fresh copy of the bootstrap");
+            MobileRequestOperation* operation = [MobileRequestOperation operationWithModule:@"map"
+                                                                                    command:@"bootstrap"
+                                                                                 parameters:nil];
+            [operation setCompleteBlock:^(MobileRequestOperation* blockOperation, id content, NSString* contentType, NSError* error) {
+                NSError *localError = error;
+                if (localError == nil) {
+                    [self validateResponse:content
+                                     error:&localError];
+                }
+                
+                
+                if (localError) {
+                    self.cachedError = error;
+                    self.cachedResponse = nil;
+                } else {
+                    self.cachedResponse = content;
+                    self.cachedError = nil;
+                }
+                
+                self.lastRetrieved = [NSDate date];
+                self.requestInFlight = NO;
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (requestResultBlock) {
+                        requestResultBlock(self.cachedResponse,self.cachedError);
+                    }
+                });
+            }];
+            
+            self.requestInFlight = YES;
+            [self.requestQueue addOperation:operation];
+        } else {
+            DDLogVerbose(@"Using bootstrap retreived at %@", self.lastRetrieved);
+            [self.requestQueue addOperationWithBlock:^{
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    if (requestResultBlock) {
+                        requestResultBlock(self.cachedResponse,self.cachedError);
+                    }
+                });
+            }];
+        }
+    }];
+}
+
+@end
