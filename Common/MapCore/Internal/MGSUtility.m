98239171a1714204046faeb4990ad68ec8eb92e0
Blake Skinner <bskinner@mit.edu>
2013-04-11 11:47:53 -0400

    Add functions to simplify the moving back and forth between ArcGIS and MapKit/CoreLocation data types
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Common/MapCore/Internal/MGSUtility.m
+++ Common/MapCore/Internal/MGSUtility.m
@@ -1,4 +1,5 @@
 #import "MGSUtility.h"
+#import "MGSGeometry.h"
 
 AGSPoint* AGSPointFromCLLocationCoordinate2D(CLLocationCoordinate2D coord) {
     // Set the spatial reference to nil here so the point will inherit whatever
@@ -29,4 +30,56 @@ CLLocationCoordinate2D CLLocationCoordinate2DFromAGSPoint(AGSPoint *point) {
     AGSPoint *projectedPoint = (AGSPoint *) [geoEngine projectGeometry:point
                                                     toSpatialReference:[AGSSpatialReference wgs84SpatialReference]];
     return CLLocationCoordinate2DMake(projectedPoint.y, projectedPoint.x);
+}
+
+MKCoordinateRegion MKCoordinateRegionFromAGSEnvelope(AGSEnvelope *envelope)
+{
+    MKCoordinateRegion region = MKCoordinateRegionInvalid;
+    
+    BOOL regionValid = ([envelope isValid] &&
+                        ([envelope isEmpty] == NO));
+    if (regionValid) {
+        AGSGeometryEngine *engine = [AGSGeometryEngine defaultGeometryEngine];
+        AGSEnvelope *projectedEnvelope = (AGSEnvelope*) [engine projectGeometry:envelope
+                                                             toSpatialReference:[AGSSpatialReference wgs84SpatialReference]];
+        
+        CLLocationDegrees latitudeDelta = fabs(projectedEnvelope.ymax - projectedEnvelope.ymin);
+        CLLocationDegrees longitudeDelta = fabs(projectedEnvelope.xmax - projectedEnvelope.xmin);
+        region = MKCoordinateRegionMake(CLLocationCoordinate2DMake(projectedEnvelope.ymin + (latitudeDelta / 2.0),
+                                                                   projectedEnvelope.xmin + (longitudeDelta / 2.0)),
+                                        MKCoordinateSpanMake(latitudeDelta, longitudeDelta));
+    }
+    
+    return region;
+}
+
+
+AGSEnvelope* AGSEnvelopeFromMKCoordinateRegion(MKCoordinateRegion region)
+{
+    if (MKCoordinateRegionIsValid(region)) {
+        CLLocationDegrees latitudeCenterOffset = region.span.latitudeDelta / 2.0;
+        CLLocationDegrees longitudeCenterOffset = region.span.longitudeDelta / 2.0;
+        
+        AGSEnvelope *envelope = [AGSEnvelope envelopeWithXmin:(double) (region.center.longitude - longitudeCenterOffset)
+                                                         ymin:(double) (region.center.latitude - latitudeCenterOffset)
+                                                         xmax:(double) (region.center.longitude + longitudeCenterOffset)
+                                                         ymax:(double) (region.center.latitude + latitudeCenterOffset)
+                                             spatialReference:[AGSSpatialReference wgs84SpatialReference]];
+        return envelope;
+    } else {
+        return nil;
+    }
+}
+
+
+AGSEnvelope* AGSEnvelopeFromMKCoordinateRegionWithSpatialReference(MKCoordinateRegion region, AGSSpatialReference *reference)
+{
+    AGSEnvelope *envelope = AGSEnvelopeFromMKCoordinateRegion(region);
+    
+    if (envelope && reference) {
+        return (AGSEnvelope*) [[AGSGeometryEngine defaultGeometryEngine] projectGeometry:envelope
+                                                                      toSpatialReference:reference];
+    }
+    
+    return nil;
 }
