3dd2c579d62df341beca918230b4715922cecaca
Austin Emmons <austin.emmons@modolabs.com>
2013-04-11 17:28:46 -0400

    [IPHONEAPP-642] Added UIImage+PDF External library. 	Also added dietary type PDF vector image resources

---
+++ External/UIImage+PDF/PDFView.h
@@ -0,0 +1,26 @@
+//
+//  PDFView.h
+//
+//  Created by Nigel Barber on 15/10/2011.
+//  Copyright 2011 Mindbrix Limited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PDFView : UIView 
+{
+
+}
+
+@property( nonatomic, assign ) int page;
+@property( nonatomic, strong ) NSString *resourceName;
+@property( nonatomic, strong ) NSURL *resourceURL;
+
++(CGRect) mediaRect:(NSString *)resourceName;
++(CGRect) mediaRectForURL:(NSURL *)resourceURL;
++(CGRect) mediaRectForURL:(NSURL *)resourceURL atPage:(int)page;
++(int) pageCountForURL:(NSURL *)resourceURL;
++(NSURL *)resourceURLForName:(NSString *)resourceName;
+  
+@end
