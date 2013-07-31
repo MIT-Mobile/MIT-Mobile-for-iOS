10f22acde2d1f2e3197ec9ccef78e1041f39c6fb
Blake Skinner <bskinner@mit.edu>
2013-07-31 16:56:33 -0400

    This is not a view controller and these methods are useless.
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Modules/Libraries/Your Account/Fines/LibrariesFinesTabController.m
+++ Modules/Libraries/Your Account/Fines/LibrariesFinesTabController.m
@@ -30,17 +30,6 @@
     return self;
 }
 
-// Override to allow orientations other than the default portrait orientation.
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    // Return YES for supported orientations
-    return MITCanAutorotateForOrientation(interfaceOrientation, [self supportedInterfaceOrientations]);
-}
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    return UIInterfaceOrientationMaskPortrait;
-}
-
 - (void)setupTableView
 {
     {
