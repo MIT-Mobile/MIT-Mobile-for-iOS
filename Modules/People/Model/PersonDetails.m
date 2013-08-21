6e99742a91e825f1847319af274274628ebbe397
Blake Skinner <bskinner@mit.edu>
2013-08-21 16:05:21 -0400

    'Directory' module updates - CoreData (#14) Updates to PersonDetails entity - ARC compatibility
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Modules/People/Model/PersonDetails.m
+++ Modules/People/Model/PersonDetails.m
@@ -3,46 +3,41 @@
 #import "CoreDataManager.h"
 
 @implementation PersonDetails
+@dynamic uid;
+@dynamic dept;
+@dynamic email;
+@dynamic fax;
+@dynamic givenname;
+@dynamic office;
+@dynamic surname;
+@dynamic phone;
+@dynamic title;
+@dynamic lastUpdate;
 
-@synthesize uid, dept, email, fax, givenname, office, /*room, */surname, phone, title, lastUpdate;
-
-// figure out if we already have this person in Favorites
-// (CoreDataManager.h does not do selecting by criteria yet)
-// this creates an insertedObject that needs to be committed or rolled back in results view
 + (PersonDetails *)retrieveOrCreate:(NSDictionary *)selectedResult
 {
-	NSString *uid = [selectedResult valueForKey:@"id"];
-	if (uid.length > 8) {
+	NSString *uid = selectedResult[@"id"];
+	if ([uid length] > 8) {
 		uid = [uid substringToIndex:8];
 	}
-	NSPredicate *pred = [NSPredicate predicateWithFormat:@"(uid = %@)", uid];
+	NSPredicate *pred = [NSPredicate predicateWithFormat:@"uid = %@", uid];
 	NSArray *results = [CoreDataManager objectsForEntity:PersonDetailsEntityName matchingPredicate:pred];
 
-	if ((results == nil) || ([results count] == 0))
+	if ([results count]) {
+		return [results lastObject];
+	} else {
 		return [PeopleRecentsData createFromSearchResult:selectedResult];
-	else 
-		return [results objectAtIndex:0];
+    }
 }
 
 - (NSString*)displayName {
-    NSString *display = nil;
-    NSString *givenName = self.givenname;
-    NSString *surName = self.surname;
-    
-    if (givenName && [givenName length]) {
-        display = givenName;
-    }
-    
-    if (surName && [surName length]) {
-        if (display == nil) {
-            display = surName;
-        } else {
-            display = [NSString stringWithFormat:@"%@ %@", givenName, surName];
+    if ([self.surname length]) {
+        if ([self.givenname length]) {
+            return [NSString stringWithFormat:@"%@ %@",self.givenname, self.surname];
         }
     }
-    
-    return [NSString stringWithString:display];
-}
 
+    return self.givenname;
+}
 
 @end
