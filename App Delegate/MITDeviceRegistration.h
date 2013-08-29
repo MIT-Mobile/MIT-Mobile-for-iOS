62788b85e05d487c43a8b970e0c77859ecb1f1d1
Blake Skinner <bskinner@mit.edu>
2013-08-29 17:01:32 -0400

    Convert to ARC and add a method which allows for a block callback when the device registration is complete.
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- App Delegate/MITDeviceRegistration.h
+++ App Delegate/MITDeviceRegistration.h
@@ -2,26 +2,18 @@
 #import <Foundation/Foundation.h>
 
 @interface MITIdentity : NSObject
-{
-	NSString *deviceID;
-	NSString *passKey;
-}
-
-- (id) initWithDeviceId: (NSString *)aDeviceId passKey: (NSString *)aPassKey;
-
-- (NSMutableDictionary *) mutableDictionary;
-
 @property (readonly) NSString *deviceID;
 @property (readonly) NSString *passKey;
+
+- (id)initWithDeviceId:(NSString *)aDeviceId passKey:(NSString *)aPassKey;
+- (NSMutableDictionary *) mutableDictionary;
 @end
 
 
-@interface MITDeviceRegistration : NSObject {
-
-}
-
-+ (void) registerNewDeviceWithToken: (NSData *)deviceToken;
-+ (void) newDeviceToken: (NSData *)deviceToken;
+@interface MITDeviceRegistration : NSObject
++ (void)registerNewDeviceWithToken:(NSData*)deviceToken;
++ (void)registerDeviceWithToken:(NSData*)deviceToken registered:(void (^)(MITIdentity *identity,NSError *error))block;
++ (void)newDeviceToken:(NSData*)deviceToken;
 + (MITIdentity *)identity;
 + (void)clearIdentity;
 @end
