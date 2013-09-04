99682243aafbf1b3b59ea0d4b4cf666ff50b55f9
Blake Skinner <bskinner@mit.edu>
2013-09-04 17:36:35 -0400

    Springboard cleanup (#5)
    Remove the MITSpringboardDelegate. It no longer reflects the behavior of the app and nothing uses it.
    
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- App Delegate/MITSpringboard.h
+++ App Delegate/MITSpringboard.h
@@ -1,24 +1,9 @@
 #import <UIKit/UIKit.h>
 
-@class MITSpringboard;
 @class IconGrid;
 @class MITModule;
 
-@protocol MITSpringboardDelegate <NSObject>
-
-@optional
-
-- (void)springboard:(MITSpringboard *)springboard didPushModuleForTag:(NSString *)moduleTag;
-- (void)springboard:(MITSpringboard *)springboard willPushModule:(MITModule*)module;
-- (void)springboard:(MITSpringboard *)springboard didPushModule:(MITModule*)module;
-
-- (void)springboard:(MITSpringboard *)springboard willPopModule:(MITModule*)module;
-- (void)springboard:(MITSpringboard *)springboard didPopModule:(MITModule*)module;
-
-@end
-
 @interface MITSpringboard : UIViewController <UINavigationControllerDelegate>
-@property (nonatomic,weak) id<MITSpringboardDelegate> delegate;
 @property (nonatomic,strong) IconGrid *grid;
 @property (nonatomic,strong) NSArray *primaryModules;
 
