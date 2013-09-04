2d7f0e8a6596d5464e5c09ca0b6235cd762d310b
Blake Skinner <bskinner@mit.edu>
2013-09-04 17:02:39 -0400

    Move to auto-layout
    Signed-off-by: Blake Skinner <bskinner@mit.edu>

--- Common/UI Classes/MITMultilineTableViewCell.m
+++ Common/UI Classes/MITMultilineTableViewCell.m
@@ -2,6 +2,7 @@
 #import "MITUIConstants.h"
 
 @interface MITMultilineTableViewCell ()
+@property UIEdgeInsets contentInsets;
 @property (nonatomic,weak) UILabel *headlineLabel;
 @property (nonatomic,weak) UILabel *bodyLabel;
 @end
@@ -11,8 +12,9 @@
 {
     self = [super init];
     if (self) {
-        _contentInset = UIEdgeInsetsMake(10, 10, 10, 8);
-        self.contentView.layer.masksToBounds = YES;
+        self.contentInsets = UIEdgeInsetsMake(4, 10, 4, 4);
+        self.contentView.autoresizesSubviews = YES;
+        [self setNeedsUpdateConstraints];
     }
     return self;
 }
@@ -21,8 +23,9 @@
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        _contentInset = UIEdgeInsetsMake(10, 10, 10, 8);
-        self.contentView.layer.masksToBounds = YES;
+        self.contentInsets = UIEdgeInsetsMake(4, 10, 4, 8);
+        self.contentView.autoresizesSubviews = YES;
+        [self setNeedsUpdateConstraints];
     }
     return self;
 }
@@ -38,19 +41,17 @@
 {
     if (!_headlineLabel) {
         UILabel *headlineLabel = [[UILabel alloc] init];
-        headlineLabel.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
-                                          UIViewAutoresizingFlexibleHeight |
-                                          UIViewAutoresizingFlexibleBottomMargin);
         headlineLabel.backgroundColor = [UIColor clearColor];
         headlineLabel.font = [UIFont boldSystemFontOfSize:CELL_STANDARD_FONT_SIZE];
         headlineLabel.textColor = CELL_STANDARD_FONT_COLOR;
         headlineLabel.highlightedTextColor = [UIColor whiteColor];
         headlineLabel.numberOfLines = 0;
         headlineLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        headlineLabel.translatesAutoresizingMaskIntoConstraints = NO;
         [self.contentView addSubview:headlineLabel];
         self.headlineLabel = headlineLabel;
-        [self setNeedsLayout];
     }
+
     return _headlineLabel;
 }
 
@@ -58,38 +59,47 @@
 {
     if (!_bodyLabel) {
         UILabel *bodyLabel = [[UILabel alloc] init];
-        bodyLabel.autoresizingMask = (UIViewAutoresizingFlexibleWidth |
-                                      UIViewAutoresizingFlexibleHeight);
         bodyLabel.backgroundColor = [UIColor clearColor];
         bodyLabel.font = [UIFont systemFontOfSize:CELL_DETAIL_FONT_SIZE];
         bodyLabel.textColor = CELL_DETAIL_FONT_COLOR;
         bodyLabel.highlightedTextColor = [UIColor whiteColor];
         bodyLabel.numberOfLines = 0;
         bodyLabel.lineBreakMode = NSLineBreakByWordWrapping;
+        bodyLabel.translatesAutoresizingMaskIntoConstraints = NO;
         [self.contentView addSubview:bodyLabel];
         self.bodyLabel = bodyLabel;
-        [self setNeedsLayout];
     }
     
     return _bodyLabel;
 }
 
-- (void)layoutSubviews
+- (void)updateConstraints
 {
-    [super layoutSubviews];
-    CGRect bounds = UIEdgeInsetsInsetRect(self.contentView.bounds,self.contentInset);
-    
-    CGSize headlineSize = [self.headlineLabel sizeThatFits:bounds.size];
-    self.headlineLabel.frame = CGRectMake(CGRectGetMinX(bounds),
-                                          CGRectGetMinY(bounds),
-                                          CGRectGetWidth(bounds),
-                                          headlineSize.height);
-    
-    CGSize bodySize = [self.bodyLabel sizeThatFits:bounds.size];
-    self.bodyLabel.frame = CGRectMake(CGRectGetMinX(bounds),
-                                      CGRectGetMaxY(self.headlineLabel.frame),
-                                      CGRectGetWidth(bounds),
-                                      bodySize.height);
+    [super updateConstraints];
+
+    CGFloat contentViewWidth = CGRectGetWidth(self.contentView.bounds);
+    self.headlineLabel.preferredMaxLayoutWidth = contentViewWidth;
+    self.bodyLabel.preferredMaxLayoutWidth = contentViewWidth;
+
+    NSDictionary *constraintViews = @{@"headlineLabel" : self.headlineLabel,
+                                      @"bodyLabel" : self.bodyLabel};
+    NSDictionary *constraintMetrics = @{@"top" : @(self.contentInsets.top),
+                                        @"left" : @(self.contentInsets.left),
+                                        @"bottom" : @(self.contentInsets.bottom),
+                                        @"right" : @(self.contentInsets.right)};
+
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left)-[headlineLabel(>=0@250)]-(right@500)-|"
+                                                                             options:0
+                                                                             metrics:constraintMetrics
+                                                                               views:constraintViews]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-(left)-[bodyLabel(>=0@250)]-(right@500)-|"
+                                                                             options:0
+                                                                             metrics:constraintMetrics
+                                                                               views:constraintViews]];
+    [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(top)-[headlineLabel][bodyLabel]-(>=bottom@250)-|"
+                                                                             options:NSLayoutFormatAlignAllLeft
+                                                                             metrics:constraintMetrics
+                                                                               views:constraintViews]];
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
@@ -100,22 +110,29 @@
 
 - (CGSize)sizeThatFits:(CGSize)size
 {
-    // Make sure everything is layed out before we start asking things
-    // about their frames.
-    [self layoutIfNeeded];
+    // TODO: Make sure that checking the accessory view here is kosher.
+    CGFloat textWidth = size.width - self.contentInsets.left - self.contentInsets.right;
+
+    if (self.accessoryType != UITableViewCellAccessoryNone) {
+        textWidth -= 20.; // *should* work for most cases (increase if needed). Worst case scenario, we should
+                          // overestimate and end up with a bit more whitespace.
+    } else if (self.accessoryView) {
+        textWidth -= CGRectGetWidth(self.accessoryView.frame);
+    }
+
+    NSString *headlineText = self.headlineLabel.text;
+    CGSize headlineTextSize = [headlineText sizeWithFont:self.headlineLabel.font
+                                                  constrainedToSize:CGSizeMake(textWidth,CGFLOAT_MAX)
+                                                      lineBreakMode:self.headlineLabel.lineBreakMode];
+    CGSize headlineLabelSize = [self.headlineLabel sizeThatFits:headlineTextSize];
+
+
+    NSString *bodyText = self.bodyLabel.text;
+    CGSize bodyLabelSize = [self.bodyLabel sizeThatFits:[bodyText sizeWithFont:self.bodyLabel.font
+                                                             constrainedToSize:CGSizeMake(textWidth,CGFLOAT_MAX)
+                                                                 lineBreakMode:self.bodyLabel.lineBreakMode]];
     
-    CGFloat textWidth = size.width - fabs(self.contentView.frame.size.width - self.bounds.size.width);
-    textWidth -= self.contentInset.top + self.contentInset.bottom;
-    
-    CGSize headlineSize = [self.headlineLabel.text sizeWithFont:self.headlineLabel.font
-                                              constrainedToSize:CGSizeMake(textWidth,CGFLOAT_MAX)
-                                                  lineBreakMode:self.headlineLabel.lineBreakMode];
-    
-    CGSize bodySize = [self.bodyLabel.text sizeWithFont:self.bodyLabel.font
-                                      constrainedToSize:CGSizeMake(textWidth,CGFLOAT_MAX)
-                                          lineBreakMode:self.bodyLabel.lineBreakMode];
-    
-    size.height = headlineSize.height + bodySize.height + self.contentInset.top + self.contentInset.bottom;
+    size.height = headlineLabelSize.height + bodyLabelSize.height + self.contentInsets.top + self.contentInsets.bottom;
     return size;
 }
 
