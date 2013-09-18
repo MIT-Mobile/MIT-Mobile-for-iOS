MIT-Mobile/MIT-Mobile-for-iOS

commit b2e05a9058434c87045dddd33129b427e07c1f7f

Update root view controller for 'News' [#13] - ARC compatibility (enabled for StoryThumbnail and XML parser as well, build is broken) - Move from static primitives for News categories to class methods. The   method should be removed in the future since the view controller   shouldn't be managing these. - Switch to literals, convert bare instance variables to properties & general cleanup

Modules/News/StoryListViewController.h
@@ -5,52 +5,17 @@
 
 typedef enum {
     NewsCategoryIdTopNews = 0,
-    NewsCategoryIdEngineering = 1,
-    NewsCategoryIdScience = 2,
-    NewsCategoryIdManagement = 3,
-    NewsCategoryIdArchitecture = 5,
-    NewsCategoryIdHumanities = 6,
+    NewsCategoryIdEngineering,
+    NewsCategoryIdScience,
+    NewsCategoryIdManagement,
+    NewsCategoryIdArchitecture,
+    NewsCategoryIdHumanities,
     NewsCategoryIdCampus = 99
 } NewsCategoryId;
 
 @class NewsStory;
 
-@interface StoryListViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate, MITSearchDisplayDelegate, StoryXMLParserDelegate, NavScrollerDelegate> {
-	UITableView *storyTable;
-    NSArray *_categories;
-    NSInteger activeCategoryId;
-	StoryXMLParser *xmlParser;
-    
-    NSArray *navButtons;
-    
-	// Nav Scroll View
-    NavScrollerView *navScrollView;
-	//UIScrollView *navScrollView;
-	//UIButton *leftScrollButton;
-	//UIButton *rightScrollButton;  
-
-	// Search bits
-	NSString *searchQuery;
-	NSArray *searchResults;
-	NSInteger searchTotalAvailableResults;
-	UISearchBar *theSearchBar;
-    MITSearchDisplayController *searchController;
-	
-	BOOL hasBookmarks;
-	BOOL showingBookmarks;
-	
-    UIView *activityView;
-    
-    NSIndexPath *tempTableSelection;
-    BOOL lastRequestSucceeded;
-}
-
-@property (nonatomic, retain) NSArray *stories;
-@property (nonatomic, retain) NSString *searchQuery;
-@property (nonatomic, retain) NSArray *searchResults;
-@property (nonatomic, retain) NSArray *categories;
-@property (nonatomic, assign) NSInteger activeCategoryId;
-@property (nonatomic, retain) StoryXMLParser *xmlParser;
+@interface StoryListViewController : UIViewController
 
 - (void)pruneStories;
 - (void)switchToCategory:(NewsCategoryId)category;

MIT Mobile.xcodeproj/project.pbxproj
@@ -309,10 +309,10 @@
 		DE71537C102FD66A005E6111 /* MIT_MobileAppDelegate+ModuleList.m in Sources */ = {isa = PBXBuildFile; fileRef = DE71537B102FD66A005E6111 /* MIT_MobileAppDelegate+ModuleList.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
 		DE71F6E3104234F900661DED /* ShuttleRouteViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = DE71F6E2104234F900661DED /* ShuttleRouteViewController.m */; };
 		DE71F7D510424E3400661DED /* ShuttleStop.m in Sources */ = {isa = PBXBuildFile; fileRef = DE71F7D410424E3400661DED /* ShuttleStop.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
-		DE728E6C10D6FF4700BFED37 /* StoryThumbnailView.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E4610D6FF4700BFED37 /* StoryThumbnailView.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
 		DE728E7B10D6FF4700BFED37 /* StoryDetailViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E6510D6FF4700BFED37 /* StoryDetailViewController.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
-		DE728E7C10D6FF4700BFED37 /* StoryListViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E6710D6FF4700BFED37 /* StoryListViewController.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
-		DE728E7E10D6FF4700BFED37 /* StoryXMLParser.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E6B10D6FF4700BFED37 /* StoryXMLParser.m */; settings = {COMPILER_FLAGS = "-fno-objc-arc"; }; };
+		DE728E6C10D6FF4700BFED37 /* StoryThumbnailView.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E4610D6FF4700BFED37 /* StoryThumbnailView.m */; };
+		DE728E7C10D6FF4700BFED37 /* StoryListViewController.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E6710D6FF4700BFED37 /* StoryListViewController.m */; };
+		DE728E7E10D6FF4700BFED37 /* StoryXMLParser.m in Sources */ = {isa = PBXBuildFile; fileRef = DE728E6B10D6FF4700BFED37 /* StoryXMLParser.m */; };
 		DE7720D910C57DBC00620A7D /* CoreGraphics.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = DE7720D810C57DBC00620A7D /* CoreGraphics.framework */; };
 		DE7720E010C57E1C00620A7D /* QuartzCore.framework in Frameworks */ = {isa = PBXBuildFile; fileRef = DE7720DF10C57E1C00620A7D /* QuartzCore.framework */; };
 		DE81CF6210C6BC390085573D /* NewsModule.m in Sources */ = {isa = PBXBuildFile; fileRef = DE81CF6110C6BC390085573D /* NewsModule.m */; };

commit b4957fb8cc101e93e0f3902c958f9d513a517456

Fix a bug I introduced in commit b2e05a905. The news categories are not sequential and by removing the defined values, the Architecture and Humanities channels broke.
Signed-off-by: Blake Skinner <bskinner@mit.edu>

Modules/News/StoryListViewController.h
@@ -3,13 +3,15 @@
 #import "MITSearchDisplayController.h"
 #import "NavScrollerView.h"
 
+// TODO: Get this out of here! We should be getting this
+// data from the news API, not hardcoding it.
 typedef enum {
     NewsCategoryIdTopNews = 0,
-    NewsCategoryIdEngineering,
-    NewsCategoryIdScience,
-    NewsCategoryIdManagement,
-    NewsCategoryIdArchitecture,
-    NewsCategoryIdHumanities,
+    NewsCategoryIdEngineering = 1,
+    NewsCategoryIdScience = 2,
+    NewsCategoryIdManagement = 3,
+    NewsCategoryIdArchitecture = 5,
+    NewsCategoryIdHumanities = 6,
     NewsCategoryIdCampus = 99
 } NewsCategoryId;
 