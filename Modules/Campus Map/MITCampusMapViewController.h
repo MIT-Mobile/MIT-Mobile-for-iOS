5c8ae032fd773c6f8cdcad9fe1f995d7ca8a2f13
Blake Skinner <bskinner@mit.edu>
2013-10-08 11:04:01 -0400

    Get rid of Xcode's copyright template

--- Modules/Campus Map/MITCampusMapViewController.h
+++ Modules/Campus Map/MITCampusMapViewController.h
@@ -1,11 +1,3 @@
-//
-//  MITCampusMapViewController.h
-//  MIT Mobile
-//
-//  Created by Blake Skinner on 2013/09/19.
-//
-//
-
 #import <UIKit/UIKit.h>
 
 @interface MITCampusMapViewController : UIViewController
