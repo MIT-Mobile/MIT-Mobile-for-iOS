06739b18ea8e33d885be3bb9d92bf680b4eeaae8
Blake Skinner <bskinner@mit.edu>
2013-10-08 15:28:04 -0400

    Add a comment so at least it's indicated what class is expected to be in the ordered set

--- Modules/Campus Map/MITMapCategoryBrowseController.h
+++ Modules/Campus Map/MITMapCategoryBrowseController.h
@@ -3,5 +3,9 @@
 
 @interface MITMapCategoryBrowseController : UITableViewController
 
+/** Initializes an instance of the category browser with 
+ *  the 'placesSelected' handler block. The 'selectedPlaces' block
+ *  parameter will contain instances of the 'MITMapPlace' class
+ */
 - (id)init:(void (^)(NSOrderedSet* selectedPlaces))placesSelected;
 @end
