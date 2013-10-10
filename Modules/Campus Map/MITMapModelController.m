28e3c3c8998b92e1a0b7e95f5fc008b12c8baa0b
Blake Skinner <bskinner@mit.edu>
2013-10-10 15:57:37 -0400

    Ensure that the block callbacks are performed on the main thread.

--- Modules/Campus Map/MITMapModelController.m
+++ Modules/Campus Map/MITMapModelController.m
@@ -43,86 +43,95 @@ static NSString* const MITMapResourceCategory = @"category";
 
 - (void)searchMapWithQuery:(NSString*)queryString loaded:(MITMapResponse)block
 {
-    if (block) {
-        NSDictionary *parameters = nil;
-        if (queryString) {
-            parameters = @{@"q" : queryString};
+    NSDictionary *parameters = nil;
+    if (queryString) {
+        parameters = @{@"q" : queryString};
+    }
+    
+    MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
+                                                                                command:@"search"
+                                                                             parameters:parameters];
+    
+    apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
+        NSMutableOrderedSet *places = [[NSMutableOrderedSet alloc] init];
+
+        for (NSDictionary *placeData in content) {
+            MITMapPlace *place = [[MITMapPlace alloc] initWithDictionary:placeData];
+            [places addObject:place];
         }
         
-        MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
-                                                                                    command:@"search"
-                                                                                 parameters:parameters];
-        
-        apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
-            NSMutableOrderedSet *places = [[NSMutableOrderedSet alloc] init];
-
-            for (NSDictionary *placeData in content) {
-                MITMapPlace *place = [[MITMapPlace alloc] initWithDictionary:placeData];
-                [places addObject:place];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (block) {
+                block(places,[NSDate date],YES,error);
             }
-
-            block(places,[NSDate date],YES,error);
-        };
-        
-        [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
-    }
+        });
+    };
+    
+    [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
 }
 
 
 - (void)categories:(MITMapResponse)block
 {
-    if (block) {
-        MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
-                                                                                    command:@"categorytitles"
-                                                                                 parameters:nil];
+    MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
+                                                                                command:@"categorytitles"
+                                                                             parameters:nil];
+    
+    apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
+        NSMutableOrderedSet *categories = [[NSMutableOrderedSet alloc] init];
         
-        apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
-            NSMutableOrderedSet *categories = [[NSMutableOrderedSet alloc] init];
-            
-            for (NSDictionary *categoryData in content) {
-                MITMapCategory *category = [[MITMapCategory alloc] initWithDictionary:categoryData];
-                [categories addObject:category];
+        for (NSDictionary *categoryData in content) {
+            MITMapCategory *category = [[MITMapCategory alloc] initWithDictionary:categoryData];
+            [categories addObject:category];
+        }
+        
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (block) {
+                block(categories, [NSDate date], YES, error);
             }
-            
-            block(categories, [NSDate date], YES, error);
-        };
-        
-        [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
-    }
+        });
+    };
+    
+    [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
 }
 
 - (void)places:(MITMapResponse)block
 {
     // The v2 API does not support getting a list of all
     // the available 'places' back (a subcategory is required)
-    block(nil,[NSDate date], YES, nil);
+    if (block) {
+        block(nil,[NSDate date], YES, nil);
+    }
 }
 
 - (void)placesInCategory:(MITMapCategory*)category loaded:(MITMapResponse)block
 {
-    if (block) {
-        NSDictionary *requestParameters = nil;
-        if (category) {
-            requestParameters = @{@"id" : category.identifier};
-        }
-
-        MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
-                                                                                    command:@"category"
-                                                                                 parameters:requestParameters];
-
-        apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
-            NSMutableOrderedSet *places = [[NSMutableOrderedSet alloc] init];
-
-            for (NSDictionary *placeData in content) {
-                MITMapPlace *place = [[MITMapPlace alloc] initWithDictionary:placeData];
-                [places addObject:place];
-            }
-
-            block(places,[NSDate date],YES,error);
-        };
-
-        [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
+    NSDictionary *requestParameters = nil;
+    if (category) {
+        requestParameters = @{@"id" : category.identifier};
     }
+
+    MobileRequestOperation *apiRequest = [[MobileRequestOperation alloc] initWithModule:@"map"
+                                                                                command:@"category"
+                                                                             parameters:requestParameters];
+
+    apiRequest.completeBlock = ^(MobileRequestOperation *operation, NSArray* content, NSString *mimeType, NSError *error) {
+        NSMutableOrderedSet *places = [[NSMutableOrderedSet alloc] init];
+
+        for (NSDictionary *placeData in content) {
+            MITMapPlace *place = [[MITMapPlace alloc] initWithDictionary:placeData];
+            [places addObject:place];
+        }
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (block) {
+                block(places,[NSDate date],YES,error);
+            }
+        });
+    };
+
+    [[MobileRequestOperation defaultQueue] addOperation:apiRequest];
 }
 
 @end
