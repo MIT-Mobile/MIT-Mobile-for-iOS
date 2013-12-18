69f9cbcf313b6c1d1ccb1abc454d50a6ad3ae92b
Blake Skinner <bskinner@mit.edu>
2013-12-18 15:58:26 -0500

    Add support for multiple API servers
    Generate RKObjectManager objects on-the-fly so we can handle server changes a bit easier.

--- Common/Core Data/MITMobile.h
+++ Common/Core Data/MITMobile.h
@@ -41,16 +41,29 @@ FOUNDATION_EXTERN NSString* const MITMobileTechcash;
 FOUNDATION_EXTERN NSString* const MITMobileTechcashAccounts;
 FOUNDATION_EXTERN NSString* const MITMobileTechcashAccount;
 
+
+typedef void (^MITMobileResult)(NSOrderedSet *objects, NSError *error);
+
+/** The callback handler for any requests which can be fulfilled by
+ *  CoreData.
+ *
+ *  @param objects A sorted set of the fetched objects. These are guaranteed to be in the main queue context. This will be nil if an error occurs.
+ *  @param fetchRequest The fetch request used to retreive the returned objects.  This will be nil if an error occurs.
+ *  @param lastUpdated The date of the last successfully refresh of the cached data.
+ *  @param error An error.
+ */
+typedef void (^MITMobileManagedResult)(NSFetchRequest *fetchRequest, NSDate *lastUpdated, NSError *error);
+
 @interface MITMobile : NSObject
-@property (nonatomic,readonly) NSSet *resources;
+@property (nonatomic,readonly) NSDictionary *resources;
 
 + (MITMobile*)defaultManager;
-+ (void)setDefaultManager:(MITMobile*)manager;
 
 - (instancetype)init;
-- (instancetype)initWithPersistentStoreCoordinator:(NSPersistentStoreCoordinator*)coordinator;
+- (void)setManagedObjectStore:(RKManagedObjectStore *)managedObjectStore;
 
-- (void)addResource:(MITMobileResource*)resource;
+- (MITMobileResource*)resourceForName:(NSString*)name;
+- (void)setResource:(MITMobileResource*)resource forName:(NSString*)name;
 
 - (NSFetchRequest*)getObjectsForResourceNamed:(NSString *)routeName object:(id)object parameters:(NSDictionary *)parameters completion:(void (^)(RKMappingResult *result, NSError *error))loaded;
 @end
--- Common/Core Data/MITMobile.m
+++ Common/Core Data/MITMobile.m
@@ -5,6 +5,7 @@
 
 #import "MITMapModelController.h"
 #import "MITMobileResource.h"
+#import "MITMobileServerConfiguration.h"
 
 #pragma mark - Route Definitions
 #pragma mark /calendars
@@ -49,109 +50,78 @@ NSString* const MITMobileTechcashAccount = @"/techcash/accounts/:id";
 
 typedef void (^MITResourceLoadedBlock)(RKMappingResult *result, NSError *error);
 
-
 #pragma mark - MITMobile
 #pragma mark Private Extension
 @interface MITMobile ()
-@property (nonatomic,strong) RKObjectManager *objectManager;
+@property (nonatomic,strong) NSMutableDictionary *objectManagers;
+@property (nonatomic,strong) NSMutableDictionary *mutableResources;
+@property (nonatomic,strong) RKManagedObjectStore *managedObjectStore;
+
+- (RKObjectManager*)objectManagerForURL:(NSURL*)url;
 @end
 
-static MITMobile *gMITMobileDefaultManager = nil;
 @implementation MITMobile
 + (MITMobile*)defaultManager
 {
-    __block MITMobile *defaultManager = nil;
-    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        defaultManager = gMITMobileDefaultManager;
-    });
-    
-    return defaultManager;
+    return [[MIT_MobileAppDelegate applicationDelegate] remoteObjectManager];
 }
 
-+ (void)setDefaultManager:(MITMobile*)manager
+- (instancetype)init;
 {
-    dispatch_sync(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-        gMITMobileDefaultManager = manager;
-    });
-}
-
-- (instancetype)initWithPersistentStoreCoordinator:(NSPersistentStoreCoordinator*)coordinator
-{
-    NSParameterAssert(coordinator);
-    
     self = [super init];
     if (self) {
-        RKManagedObjectStore *store = [[RKManagedObjectStore alloc] initWithPersistentStoreCoordinator:coordinator];
-        [RKManagedObjectStore setDefaultStore:store];
-        [store createManagedObjectContexts];
-        
-        _objectManager = [RKObjectManager managerWithBaseURL:[NSURL URLWithString:@"http://mobile-dev.mit.edu"]];
-        _objectManager.managedObjectStore = store;
-        
+        _objectManagers = [[NSMutableDictionary alloc] init];
+        _mutableResources = [[NSMutableDictionary alloc] init];
     }
     
     return self;
 }
 
-- (instancetype)init
+- (void)setManagedObjectStore:(RKManagedObjectStore *)managedObjectStore
 {
-    return [self initWithPersistentStoreCoordinator:[CoreDataManager persistentStoreCoordinator]];
+    if (![self.managedObjectStore isEqual:managedObjectStore]) {
+        _managedObjectStore = managedObjectStore;
+
+        [self.objectManagers enumerateKeysAndObjectsUsingBlock:^(id key, RKObjectManager *objectManager, BOOL *stop) {
+            [objectManager.operationQueue cancelAllOperations];
+            objectManager.managedObjectStore = _managedObjectStore;
+        }];
+    }
 }
 
-- (void)addResource:(MITMobileResource *)resource
+- (void)setResource:(MITMobileResource*)resource forName:(NSString*)name
 {
     NSParameterAssert(resource);
+    NSAssert([resource isKindOfClass:[MITMobileResource class]], @"resource is not descended from MITMobileResource");
+    NSAssert(!(self.mutableResources[name]), @"resource with name '%@' already exists",name);
+    NSAssert([self.objectManagers count] == 0, @"resources can not be added after requests have been made");
 
-    NSIndexSet *successfulStatusCodes = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful);
+    self.mutableResources[name] = resource;
+}
 
-    // Setup the response descriptors for the resource. This will probably be done a different way
-    // when (if) we move away from RKObjectManager.
-    [resource enumerateMappingsByRequestMethodUsingBlock:^(RKRequestMethod method, NSDictionary *mappings) {
-        [mappings enumerateKeysAndObjectsUsingBlock:^(NSString *keyPath, RKMapping *mapping, BOOL *stop) {
-            RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping
-                                                                                                    method:method
-                                                                                               pathPattern:resource.pathPattern
-                                                                                                   keyPath:keyPath
-                                                                                               statusCodes:successfulStatusCodes];
-            [self.objectManager addResponseDescriptor:responseDescriptor];
-        }];
+- (MITMobileResource*)resourceForName:(NSString *)name
+{
+    return self.mutableResources[name];
+}
 
-
-        // And now register the route with the object manager's router
-        RKRoute *route = [RKRoute routeWithName:resource.name
-                                    pathPattern:resource.pathPattern
-                                         method:method];
-        [self.objectManager.router.routeSet addRoute:route];
-    }];
-
-
-    // Setup the fetch request generators so we can have nice things (like
-    // killing orphans) for resources that provide the support
-    __weak MITMobileResource *weakResource = resource;
-    [self.objectManager addFetchRequestBlock:^NSFetchRequest *(NSURL *URL) {
-        MITMobileResource *blockResource = weakResource;
-        if (blockResource && blockResource.fetchGenerator) {
-            return blockResource.fetchGenerator(URL);
-        } else {
-            return nil;
-        }
-    }];
+- (NSDictionary*)resources
+{
+    return [self.mutableResources copy];
 }
 
 - (NSFetchRequest*)getObjectsForResourceNamed:(NSString *)routeName object:(id)object parameters:(NSDictionary *)parameters completion:(MITResourceLoadedBlock)block;
 {
-    NSURL *url = [self.objectManager.router URLForRouteNamed:routeName method:NULL object:nil];
+    // Trim off any additional paths. Right now, the API prefix (for the Mobile v3, '/apis')
+    // is included in the route name but the current server URL uses '/api'.
+    // Passing the current server URL directly into the routing subsystem confuses it.
+    NSURL *serverURL = [[NSURL URLWithString:@"/"
+                              relativeToURL:MITMobileWebGetCurrentServerURL()] absoluteURL];
+    RKObjectManager *objectManager = [self objectManagerForURL:serverURL];
+    
+    NSString *uniquedRouteName = [NSString stringWithFormat:@"%@ %@",RKStringFromRequestMethod(RKRequestMethodGET),routeName];
+    NSURL *url = [objectManager.router URLForRouteNamed:uniquedRouteName method:NULL object:nil];
 
-    __block NSFetchRequest *fetchRequest = nil;
-    [self.objectManager.fetchRequestBlocks enumerateObjectsWithOptions:NSEnumerationReverse
-                                                            usingBlock:^(NSFetchRequest* (^block)(NSURL*), NSUInteger idx, BOOL *stop) {
-                                                                fetchRequest = block(url);
-                                                                if (fetchRequest) {
-                                                                    (*stop) = YES;
-                                                                }
-                                                            }];
-
-    [self.objectManager getObjectsAtPathForRouteNamed:routeName
+    [objectManager getObjectsAtPathForRouteNamed:uniquedRouteName
                                                object:nil
                                            parameters:parameters
                                               success:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
@@ -165,6 +135,61 @@ static MITMobile *gMITMobileDefaultManager = nil;
                                                   }
                                               }];
 
-    return fetchRequest;
+    MITMobileResource *requestedResource = self.resources[routeName];
+    return [requestedResource fetchRequestForURL:url];
 }
+
+- (RKObjectManager*)objectManagerForURL:(NSURL *)url
+{
+    RKObjectManager *objectManager = self.objectManagers[url];
+    if (!objectManager) {
+        objectManager = [RKObjectManager managerWithBaseURL:url];
+
+        [self.resources enumerateKeysAndObjectsUsingBlock:^(NSString *name, MITMobileResource *resource, BOOL *stop) {
+            NSIndexSet *successfulStatusCodes = RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful);
+            
+            // Setup the response descriptors for the resource. This will probably be done a different way
+            // when (if) we move away from RKObjectManager.
+            [resource enumerateMappingsByRequestMethodUsingBlock:^(RKRequestMethod method, NSDictionary *mappings) {
+                [mappings enumerateKeysAndObjectsUsingBlock:^(NSString *keyPath, RKMapping *mapping, BOOL *stop) {
+                    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping
+                                                                                                            method:method
+                                                                                                       pathPattern:resource.pathPattern
+                                                                                                           keyPath:keyPath
+                                                                                                       statusCodes:successfulStatusCodes];
+                    [objectManager addResponseDescriptor:responseDescriptor];
+                }];
+                
+                // And now register the route with the object manager's router. The request method is being
+                // added here to unique the route name, otherwise, RKRouter will complain if there
+                // are multiple routes with the same name (even if the method differs)
+                RKRoute *route = [RKRoute routeWithName:[NSString stringWithFormat:@"%@ %@",RKStringFromRequestMethod(method),name]
+                                            pathPattern:resource.pathPattern
+                                                 method:method];
+                
+                [objectManager.router.routeSet addRoute:route];
+            }];
+            
+            
+            // Setup the fetch request generators so we can have nice things (like
+            // killing orphans) for resources that provide the support
+            __weak MITMobileResource *weakResource = resource;
+            [objectManager addFetchRequestBlock:^(NSURL *URL) {
+                return [weakResource fetchRequestForURL:URL];
+            }];
+        }];
+        
+        self.objectManagers[url] = objectManager;
+    }
+
+    if (!(objectManager.managedObjectStore || self.managedObjectStore)) {
+        DDLogWarn(@"an RKManagedObjectStore has not been assigned; mappings requiring CoreData will not be performed");
+    } else if (!objectManager.managedObjectStore) {
+        objectManager.managedObjectStore = self.managedObjectStore;
+    }
+
+
+    return objectManager;
+}
+
 @end
