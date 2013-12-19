c169df0df515e26e256fdaa75e3f0e86ee7262d5
Blake Skinner <bskinner@mit.edu>
2013-12-19 15:40:13 -0500

    Use the CoreData and Mobile resource convenience headers instead of trying to add in everything by hand

--- Modules/Campus Map/Model/MITMapModel.h
+++ Modules/Campus Map/Model/MITMapModel.h
@@ -1,19 +1,13 @@
 #ifndef _MIT_MAP_MODEL_H_
 #define _MIT_MAP_MODEL_H_
 
-#import <Foundation/Foundation.h>
-#import <CoreData/CoreData.h>
-#import <MapKit/MapKit.h>
-
-#import "CoreData+MITAdditions.h"
-
-#import "MIT_MobileAppDelegate.h"
-#import "MITCoreDataController.h"
-#import "CoreDataManager.h"
+#import "MITCoreData.h"
+#import "MITMobileResources.h"
 
 #import "MITMapModelController.h"
 #import "MITMapCategory.h"
 #import "MITMapPlace.h"
+#import "MITMapPlaceContent.h"
 #import "MITMapBookmark.h"
 #import "MITMapSearch.h"
 
