1c902148ceffe107be11fac7137c0cd9e5b393e7
Blake Skinner <bskinner@chaoticminds.edu>
2014-02-13 07:57:35 -0500

    Get the image gallery working!
    Currently view controllers are not lazily instantiated. Profile the memory usage and look into making things a bit more dynamic later.

--- Modules/News/View Controllers/MITNewsMediaGalleryViewController.m
+++ Modules/News/View Controllers/MITNewsMediaGalleryViewController.m
@@ -1,12 +1,16 @@
 #import "MITNewsMediaGalleryViewController.h"
+#import "MITCoreDataController.h"
 #import "MITNewsImageViewController.h"
 
 #import "MITNewsImageRepresentation.h"
+#import "MITNewsImage.h"
 
 
 #import "MITAdditions.h"
 
 @interface MITNewsMediaGalleryViewController () <UIPageViewControllerDataSource,UIPageViewControllerDelegate>
+@property (nonatomic,strong) NSMutableArray *galleryPageViewControllers;
+@property (nonatomic) NSInteger selectedIndex;
 
 @end
 
@@ -25,11 +29,14 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    self.title = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
-    DDLogVerbose(@"View will appear!");
+    // Call this to force an update of the image caption
+    // for the first view controller
+    [self didChangeSelectedIndex];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,29 +50,41 @@
     return UIStatusBarStyleLightContent;
 }
 
+- (NSArray*)galleryImages
+{
+    return [_galleryImages arrayByMappingObjectsUsingBlock:^id(MITNewsImage *newsImage, NSUInteger idx) {
+        if (newsImage.managedObjectContext != self.managedObjectContext) {
+            return (MITNewsImage*)[self.managedObjectContext objectWithID:[newsImage objectID]];
+        } else {
+            return newsImage;
+        }
+    }];
+}
+
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
     NSAssert(self.managedObjectContext, @"parent did not assign a managed object context");
 
     if ([segue.identifier isEqualToString:@"embedPageViewController"]) {
         UIPageViewController *pageViewController = [segue destinationViewController];
-        pageViewController.view.backgroundColor = [UIColor clearColor];
+        pageViewController.dataSource = self;
+        pageViewController.delegate = self;
+        pageViewController.view.backgroundColor = [UIColor blackColor];
 
-        NSMutableArray *imageViewControllers = [[NSMutableArray alloc] init];
+        self.galleryPageViewControllers = [[NSMutableArray alloc] init];
         [self.managedObjectContext performBlockAndWait:^{
-            [self.galleryImages enumerateObjectsUsingBlock:^(MITNewsImageRepresentation *imageRepresentation, NSUInteger idx, BOOL *stop) {
-                MITNewsImageRepresentation *localRepresentation = imageRepresentation;
-                if (localRepresentation.managedObjectContext != self.managedObjectContext) {
-                    localRepresentation = (MITNewsImageRepresentation*)[self.managedObjectContext objectWithID:[imageRepresentation objectID]];
-                }
-
-                NSURL *url = localRepresentation.url;
+            [self.galleryImages enumerateObjectsUsingBlock:^(MITNewsImage *image, NSUInteger idx, BOOL *stop) {
                 MITNewsImageViewController *imageViewController = [[MITNewsImageViewController alloc] initWithNibName:@"MITNewsImageViewController" bundle:nil];
-                imageViewController.imageURL = url;
-                [imageViewControllers addObject:imageViewControllers];
+                
+                NSManagedObjectContext *context = [[MITCoreDataController defaultController] newManagedObjectContextWithConcurrencyType:NSMainQueueConcurrencyType trackChanges:NO];
+                imageViewController.managedObjectContext = context;
+                imageViewController.image = (MITNewsImage*)[context objectWithID:[image objectID]];
+                
+                [self.galleryPageViewControllers addObject:imageViewController];
             }];
+            
 
-            [pageViewController setViewControllers:imageViewControllers
+            [pageViewController setViewControllers:@[[self.galleryPageViewControllers firstObject]]
                                          direction:UIPageViewControllerNavigationDirectionForward
                                           animated:NO
                                         completion:nil];
@@ -73,6 +92,17 @@
     }
 }
 
+#pragma mark Properties
+- (void)setSelectedIndex:(NSInteger)selectedIndex
+{
+    if (_selectedIndex != selectedIndex) {
+        _selectedIndex = selectedIndex;
+        
+        [self didChangeSelectedIndex];
+    }
+}
+
+#pragma mark UI Actions
 - (IBAction)dismissGallery:(id)sender
 {
 
@@ -89,10 +119,76 @@
     self.captionView.hidden = !self.captionView.hidden;
 }
 
+- (void)didChangeSelectedIndex
+{
+    UINavigationItem *navigationItem = [[self.navigationBar items] lastObject];
+    navigationItem.title = [NSString stringWithFormat:@"%d of %d",self.selectedIndex + 1,[_galleryImages count]];
+    
+    __block NSString *description = nil;
+    __block NSString *credits = nil;
+    [self.managedObjectContext performBlockAndWait:^{
+        MITNewsImage *image = self.galleryImages[self.selectedIndex];
+        description = image.descriptionText;
+        credits = image.credits;
+    }];
+    
+    self.descriptionLabel.text = description;
+    self.creditLabel.text = credits;
+}
+
 #pragma mark - UIPageViewController
 #pragma mark UIPageViewControllerDataSource
+- (UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController
+{
+    if ([viewController isKindOfClass:[MITNewsImageViewController class]]) {
+        MITNewsImageViewController *galleryImageViewController = (MITNewsImageViewController*)viewController;
+        NSUInteger galleryImageIndex = [self.galleryPageViewControllers indexOfObject:galleryImageViewController];
+        if (galleryImageIndex == NSNotFound) {
+            // No idea where this object came from.
+            DDLogWarn(@"[%@] asked to create a page for an unknown object '%@'",self,galleryImageViewController);
+            return nil;
+        } else if (galleryImageIndex == ([self.galleryImages count] - 1)) {
+            // This is the last page so we just return nil here
+            return nil;
+        } else {
+            return self.galleryPageViewControllers[galleryImageIndex + 1];
+        }
+    } else {
+        return nil;
+    }
+}
 
+- (UIViewController*)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController
+{
+    if ([viewController isKindOfClass:[MITNewsImageViewController class]]) {
+        MITNewsImageViewController *galleryImageViewController = (MITNewsImageViewController*)viewController;
+        NSUInteger galleryImageIndex = [self.galleryPageViewControllers indexOfObject:galleryImageViewController];
+        if (galleryImageIndex == NSNotFound) {
+            // No idea where this object came from.
+            DDLogWarn(@"[%@] asked to create a page for an unknown object '%@'",self,galleryImageViewController);
+            return nil;
+        } else if (galleryImageIndex == 0) {
+            // This is the last page so we just return nil here
+            return nil;
+        } else {
+            return self.galleryPageViewControllers[galleryImageIndex - 1];
+        }
+    } else {
+        return nil;
+    }
+}
 
 #pragma mark UIPageViewControllerDelegate
+
+- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed
+{
+    if (completed) {
+        NSMutableOrderedSet *viewControllers = [[NSMutableOrderedSet alloc] initWithArray:[pageViewController viewControllers]];
+        [viewControllers minusSet:[NSSet setWithArray:previousViewControllers]];
+        
+        MITNewsImageViewController *imageViewController = [viewControllers firstObject];
+        self.selectedIndex = [self.galleryPageViewControllers indexOfObject:imageViewController];
+    }
+}
 
 @end
