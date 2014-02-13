002beb6baea40551c1906b17ada5cae87be57959
Blake Skinner <bskinner@chaoticminds.edu>
2014-02-13 07:35:34 -0500

    Remove the code which tweaked the cell subview background colors (it was for debugging purposes)

--- Modules/News/MITNewsStoryCell.m
+++ Modules/News/MITNewsStoryCell.m
@@ -6,14 +6,6 @@
 @end
 
 @implementation MITNewsStoryCell
-- (void)awakeFromNib
-{
-    self.contentView.backgroundColor = [UIColor colorWithRed:0.25 green:0.25 blue:0 alpha:0.5];
-    self.titleLabel.backgroundColor = [UIColor colorWithRed:0 green:0 blue:.25 alpha:0.5];
-    self.dekLabel.backgroundColor = [UIColor colorWithRed:0 green:0 blue:.25 alpha:0.5];
-    self.storyImageView.backgroundColor = [UIColor colorWithRed:.25 green:0 blue:0 alpha:0.5];
-}
-
 - (void)prepareForReuse
 {
     [self.storyImageView cancelCurrentImageLoad];
