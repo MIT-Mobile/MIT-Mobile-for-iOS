21b343dc8e7271fc0846a934ddeea8f2cb18db2a
Blake Skinner <bskinner@chaoticminds.edu>
2014-02-13 07:58:11 -0500

    Tweak the logic for selecting the best image. Still has a few rough edges but it seems to work better

--- Modules/News/Model/MITNewsImage.m
+++ Modules/News/Model/MITNewsImage.m
@@ -28,12 +28,28 @@
 
 - (MITNewsImageRepresentation*)bestRepresentationForSize:(CGSize)size
 {
-#warning Add logic for selecting the best fitting image (instead of just the smallest)
-    NSArray *sortedRepresentations = [[self.representations allObjects] sortedArrayUsingComparator:^NSComparisonResult(MITNewsImageRepresentation *representation1,MITNewsImageRepresentation *representation2) {
-        CGFloat area1 = [representation1.width doubleValue] * [representation2.height doubleValue];
+    NSMutableArray *sortedRepresentations = [[[self.representations allObjects] sortedArrayUsingComparator:^NSComparisonResult(MITNewsImageRepresentation *representation1,MITNewsImageRepresentation *representation2) {
+        CGFloat area1 = [representation1.width doubleValue] * [representation1.height doubleValue];
         CGFloat area2 = [representation2.width doubleValue] * [representation2.height doubleValue];
         
         return [@(area1) compare:@(area2)];
+    }] mutableCopy];
+
+#warning potentially ugly behavior when the height or width of the size is really large
+    CGFloat targetArea = size.width * size.height;
+    [sortedRepresentations sortUsingComparator:^NSComparisonResult(MITNewsImageRepresentation *representation1,MITNewsImageRepresentation *representation2) {
+        CGFloat distance1 = ([representation1.width doubleValue] * [representation1.height doubleValue]) - targetArea;
+        CGFloat distance2 = ([representation2.width doubleValue] * [representation2.height doubleValue]) - targetArea;
+        
+        if (distance1 < 0) {
+            distance1 = CGFLOAT_MAX;
+        }
+        
+        if (distance2 < 0) {
+            distance1 = CGFLOAT_MAX;
+        }
+        
+        return [@(distance1) compare:@(distance2)];
     }];
     
     return [sortedRepresentations firstObject];
