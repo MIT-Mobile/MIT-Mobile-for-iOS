c6a3984b58be0aded244a9167cbd373c0e5dbb9d
Blake Skinner <bskinner@mit.edu>
2014-02-19 16:24:41 -0500

    Change the UILabel outlet to 'titleLabel' so it doesn't interfere with UITableViewHeaderFooterView's textLabel property.

--- Modules/News/Views/MITDisclosureHeaderView.h
+++ Modules/News/Views/MITDisclosureHeaderView.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
 @interface MITDisclosureHeaderView : UITableViewHeaderFooterView
-@property (nonatomic,weak) IBOutlet UILabel *textLabel;
+@property (nonatomic,weak) IBOutlet UILabel *titleLabel;
 @property (nonatomic,weak) IBOutlet UIView *accessoryView;
 @end
