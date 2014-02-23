d9f48bc9acb33f97e555c5ff645594b8f78f15d8
Blake Skinner <bskinner@mit.edu>
2014-02-23 11:34:42 -0500

    Add Nibs and nib/identifier constants for the 'LoadMore' and 'External' image-less story cells

--- Modules/News/View Controllers/MITNewsConstants.h
+++ Modules/News/View Controllers/MITNewsConstants.h
@@ -1,14 +1,24 @@
 #import <Foundation/Foundation.h>
 
+extern NSString* const MITNewsLoadMoreCellIdentifier;
+extern NSString* const MITNewsLoadMoreCellNibName;
+
+// Regular stories consist of an optional image,
+// an optional dek, and a title
+#pragma mark Regular stories
 extern NSString* const MITNewsStoryCellIdentifier;
 extern NSString* const MITNewsStoryCellNibName;
-
 extern NSString* const MITNewsStoryNoDekCellIdentifier;
 extern NSString* const MITNewsStoryNoDekCellNibName;
 
+// 'External' stories are stories that just consist of an optional
+// branding image (NPR, New York Times, etc), a dek, and a link
+#pragma mark External stories
 extern NSString* const MITNewsStoryExternalType;
 extern NSString* const MITNewsStoryExternalCellIdentifier;
 extern NSString* const MITNewsStoryExternalCellNibName;
+extern NSString* const MITNewsStoryExternalNoImageCellIdentifier;
+extern NSString* const MITNewsStoryExternalNoImageCellNibName;
 
 extern NSString* const MITNewsCategoryHeaderIdentifier;
 extern NSString* const MITNewsStoryFeaturedStoriesRequestToken;
--- Modules/News/View Controllers/MITNewsConstants.m
+++ Modules/News/View Controllers/MITNewsConstants.m
@@ -1,14 +1,20 @@
 #import "MITNewsConstants.h"
 
+NSString* const MITNewsLoadMoreCellIdentifier = @"NewsLoadMoreCell";
+NSString* const MITNewsLoadMoreCellNibName = @"NewsLoadMoreTableCell";
+
 NSString* const MITNewsStoryCellIdentifier = @"StoryCell";
 NSString* const MITNewsStoryCellNibName = @"NewsStoryTableCell";
 
 NSString* const MITNewsStoryNoDekCellIdentifier = @"StoryNoDekCell";
 NSString* const MITNewsStoryNoDekCellNibName = @"NewsStoryNoDekTableCell";
 
+
 NSString* const MITNewsStoryExternalType = @"news_clip";
 NSString* const MITNewsStoryExternalCellIdentifier = @"StoryExternalCell";
 NSString* const MITNewsStoryExternalCellNibName = @"NewsStoryExternalTableCell";
+NSString* const MITNewsStoryExternalNoImageCellIdentifier = @"StoryExternalNoImageCell";
+NSString* const MITNewsStoryExternalNoImageCellNibName = @"NewsStoryExternalNoImageTableCell";
 
 NSString* const MITNewsCategoryHeaderIdentifier = @"MITNewsCategoryHeader";
 NSString* const MITNewsStoryFeaturedStoriesRequestToken = @"MITNewsFeaturedStoriesRequest";
