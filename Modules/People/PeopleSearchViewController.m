11c4efbc280ceeb83be19a3e42f93f2047829b38
Blake Skinner <bskinner@mit.edu>
2014-02-27 16:41:11 -0500

    [#30] Cleanup work on People
    - Directory Assistance and Emergency Contacts should be their own section
    
    - Initial search tweaks
    
    - Search bar should have MITTintColor on iOS 6.

--- Modules/People/PeopleSearchViewController.m
+++ Modules/People/PeopleSearchViewController.m
@@ -13,10 +13,17 @@
 
 #import "MITPeopleResource.h"
 
+typedef NS_ENUM(NSInteger, MITPeopleSearchTableViewSection) {
+    MITPeopleSearchTableViewSectionExample = 0,
+    MITPeopleSearchTableViewSectionContacts,
+    MITPeopleSearchTableViewSectionRecentlyViewed
+};
+
 @interface PeopleSearchViewController () <UISearchBarDelegate, UIAlertViewDelegate, UIActionSheetDelegate>
 
-@property (nonatomic,strong) IBOutlet UITableView *searchResultsTableView;
-@property (nonatomic,strong) UIView *loadingView;
+@property (nonatomic,weak) UITableView *searchResultsTableView;
+@property (nonatomic,weak) MITLoadingActivityView *searchResultsLoadingView;
+
 @property (nonatomic,strong) UIView *recentlyViewedHeader;
 
 @property (nonatomic,copy) NSString *searchTerms;
@@ -51,38 +58,34 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-    
-    UITableView *searchTableView = [[UITableView alloc] initWithFrame:CGRectZero style:UITableViewStylePlain];
-    self.searchResultsTableView = searchTableView;
 
     if (NSFoundationVersionNumber <= NSFoundationVersionNumber_iOS_6_1) {
         self.tableView.backgroundView = nil;
         self.tableView.backgroundColor = [UIColor mit_backgroundColor];
-        searchTableView.backgroundView = nil;
-        searchTableView.backgroundColor = [UIColor mit_backgroundColor];
+
+        self.searchBar.tintColor = [UIColor MITTintColor];
     }
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
 	[super viewWillAppear:animated];
-    if (![self.searchDisplayController isActive]) {
-        [self.tableView reloadData];
+
+    if (self.searchResultsTableView) {
+        NSIndexPath *selectedRow = [self.searchResultsTableView indexPathForSelectedRow];
+        if (selectedRow && self.clearsSelectionOnViewWillAppear) {
+            [self.searchResultsTableView deselectRowAtIndexPath:selectedRow animated:animated];
+        }
     } else {
-        [self.searchResultsTableView deselectRowAtIndexPath:[self.searchResultsTableView indexPathForSelectedRow] animated:YES];
+        [self.tableView reloadData];
     }
 }
 
 #pragma mark -
 #pragma mark Search methods
-
 - (void) searchBarTextDidBeginEditing:(UISearchBar *)searchBar
 {
-    if ([self.searchResults count] > 0) {
-        self.searchDisplayController.searchResultsTableView.hidden = NO;
-    } else {
-        self.searchDisplayController.searchResultsTableView.hidden = YES;
-    }
+
 }
 
 - (void) searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText
@@ -105,12 +108,10 @@
 {
 	self.searchResults = nil;
     self.searchCancelled = YES;
-    [self.searchBar resignFirstResponder];
-    [self.tableView reloadData];
+    self.searchTerms = nil;
+
     self.searchDisplayController.searchResultsTableView.hidden = YES;
-    if (self.loadingView.superview) {
-        [self.loadingView removeFromSuperview];
-    }
+    [self.searchResultsLoadingView removeFromSuperview];
 }
 
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar
@@ -136,8 +137,10 @@
             self.searchResults = objects;
             [self.searchDisplayController.searchResultsTableView reloadData];
         }
-        [self.loadingView removeFromSuperview];
+
+        [self.searchResultsTableView removeFromSuperview];
     }];
+
     self.searchResultsTableView.hidden = NO;
     [self showLoadingView];
 
@@ -150,140 +153,167 @@
 	if (tableView == self.searchDisplayController.searchResultsTableView)
 		return 1;
 	else if ([[[PeopleRecentsData sharedData] recents] count] > 0)
-		return 2;
+		return 3;
 	else
-		return 1;
+		return 2;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 	if (tableView == self.tableView) {
-		switch (section) {
-			case 0: // sample cell, phone directory, & emergency contacts
-				return 3;
-				break;
-			case 1: // recently viewed
+        MITPeopleSearchTableViewSection tableViewSection = section;
+		switch (tableViewSection) {
+            case MITPeopleSearchTableViewSectionExample: // sample cell
+                return 1;
+			case MITPeopleSearchTableViewSectionContacts: // phone directory, & emergency contacts
+				return 2;
+			case MITPeopleSearchTableViewSectionRecentlyViewed: // recently viewed
 				return [[[PeopleRecentsData sharedData] recents] count];
-				break;
 			default:
 				return 0;
-				break;
 		}
+	} else if (tableView == self.searchResultsTableView) {
+		return ([self.searchResults count] ? [self.searchResults count] : 1); //Force a single row
 	} else {
-		return [self.searchResults count];
-	}
+        return 0;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
 	static NSString *recentCellID = @"RecentCell";
-    
     static NSString * directoryAssistanceID = @"DirectoryAssistanceCell";
 
+    UITableViewCell *cell = nil;
+
 	if (tableView == self.tableView) { // show phone directory tel #, recents
-	
-	
-		if (indexPath.section == 0) {
-			
-            UITableViewCell *cell;
+        if (MITPeopleSearchTableViewSectionExample == indexPath.section) {
+            cell = [tableView dequeueReusableCellWithIdentifier:@"SampleCell" forIndexPath:indexPath];
+        } else if (MITPeopleSearchTableViewSectionContacts == indexPath.section) {
             if (indexPath.row == 0) {
-                cell = [tableView dequeueReusableCellWithIdentifier:@"SampleCell" forIndexPath:indexPath];
-            } else if (indexPath.row == 1) {
                 cell = [tableView dequeueReusableCellWithIdentifier:directoryAssistanceID forIndexPath:indexPath];
                 cell.accessoryView = [UIImageView accessoryViewWithMITType:MITAccessoryViewPhone];
-            } else if (indexPath.row == 2) {
+            } else if (indexPath.row == 1) {
                 cell = [tableView dequeueReusableCellWithIdentifier:@"EmergencyContactsCell" forIndexPath:indexPath];
-//                cell.accessoryView = [UIImageView accessoryViewWithMITType:MITAccessoryViewEmergency];
+                //cell.accessoryView = [UIImageView accessoryViewWithMITType:MITAccessoryViewEmergency];
             }
-			
-			return cell;
-		
-		} else { // recents
-			
-			UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:recentCellID forIndexPath:indexPath];
+        } else if (MITPeopleSearchTableViewSectionRecentlyViewed == indexPath.section) {
+            cell = [tableView dequeueReusableCellWithIdentifier:recentCellID forIndexPath:indexPath];
 
-			PersonDetails *recent = [[PeopleRecentsData sharedData] recents][indexPath.row];
-			cell.textLabel.text = recent.name;
-            
-			// show person's title, dept, or email as cell's subtitle text
-			cell.detailTextLabel.text = @" ";
-			NSString *displayText = nil;
-			for (NSString *tag in @[@"title", @"dept"]) {
-				displayText = [recent valueForKey:tag];
-				if (displayText) {
-					cell.detailTextLabel.text = displayText;
-					break;
-				}
-			}
-			return cell;
-		}
-	} else { // search results
-		UITableViewCell *cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"ResultCell"];
+            NSArray *recentPeople = [[PeopleRecentsData sharedData] recents];
+            if (indexPath.row < [recentPeople count]) {
+                PersonDetails *recent = recentPeople[indexPath.row];
+                cell.textLabel.text = recent.name;
+
+                // show person's title, dept, or email as cell's subtitle text
+                // Setting the detailTextLabel's text to a space solves 2 issues:
+                //  * Top-aligns the primary text field
+                //  * In the case of cell reuse, clears out the detail field in case
+                //      the person we are currently display does not have values for
+                //      the below tags.
+                cell.detailTextLabel.text = @" ";
+                NSString *displayText = nil;
+                for (NSString *tag in @[@"title", @"dept"]) {
+                    displayText = [recent valueForKey:tag];
+                    if (displayText) {
+                        cell.detailTextLabel.text = displayText;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return cell;
+	} else if (tableView == self.searchResultsTableView) { // search results
+		cell = (UITableViewCell *)[tableView dequeueReusableCellWithIdentifier:@"ResultCell"];
+
 		if (!cell) {
 			cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"ResultCell"];
 			cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
 		}
-			
-		PersonDetails *searchResult = self.searchResults[indexPath.row];
-		NSString *fullname = searchResult.name;
 
-		if (searchResult.title) {
-			cell.detailTextLabel.text = searchResult.title;
-		} else if (searchResult.dept) {
-			cell.detailTextLabel.text = searchResult.dept;
-		} else {
-            cell.detailTextLabel.text = @" "; // if this is empty textlabel will be bottom aligned
+        // Make sure to sanity check the current row. Since tableView:numberOfRowsInSection:
+        //  returns a minimum value of 1 (even if there are no actual results). If the table
+        //  view is asking for a cell and we don't have anything to display, just return a blank cell
+        // (bskinner - 2014.02.27)
+        if (indexPath.row < [self.searchResults count]) {
+            PersonDetails *searchResult = self.searchResults[indexPath.row];
+            NSString *fullname = searchResult.name;
+
+            if (searchResult.title) {
+                cell.detailTextLabel.text = searchResult.title;
+            } else if (searchResult.dept) {
+                cell.detailTextLabel.text = searchResult.dept;
+            } else {
+                cell.detailTextLabel.text = @" "; // if this is empty textlabel will be bottom aligned
+            }
+            
+            
+            // in this section we try to highlight the parts of the results that match the search terms
+            UIFont *labelFont = cell.textLabel.font;
+            UIFont *boldFont = [UIFont boldSystemFontOfSize:labelFont.pointSize];   // This assumes labelFont will be using the systemFont
+            __block NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc] initWithString:fullname];
+            [self.searchTokens enumerateObjectsUsingBlock:^(NSString *token, NSUInteger idx, BOOL *stop) {
+                NSRange boldRange = [[fullname lowercaseString] rangeOfString:token];
+                if (boldRange.location != NSNotFound) {
+                    [attributeString addAttribute:NSFontAttributeName value:boldFont range:boldRange];
+                }
+            }];
+            
+            cell.textLabel.attributedText = attributeString;
+        } else {
+            // Clear out the text fields in the event of cell reuse
+            cell.textLabel.text = nil;
+            cell.detailTextLabel.text = nil;
+            cell.accessoryType = UITableViewCellAccessoryNone;
         }
-		
-        
-		// in this section we try to highlight the parts of the results that match the search terms
-        UIFont *labelFont = cell.textLabel.font;
-        UIFont *boldFont = [UIFont boldSystemFontOfSize:labelFont.pointSize];   // This assumes labelFont will be using the systemFont
-        __block NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc] initWithString:fullname];
-        [self.searchTokens enumerateObjectsUsingBlock:^(NSString *token, NSUInteger idx, BOOL *stop) {
-            NSRange boldRange = [[fullname lowercaseString] rangeOfString:token];
-			if (boldRange.location != NSNotFound) {
-				[attributeString addAttribute:NSFontAttributeName value:boldFont range:boldRange];
-			}
-        }];
-		
-		cell.textLabel.attributedText = attributeString;
-		return cell;
-	}
-	
+
+        return cell;
+	} else {
+        return nil;
+    }
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-	if (tableView == self.tableView && indexPath.section == 0) {
-        switch (indexPath.row) {
-            case 0:
-                return 86.;
+	if (tableView == self.tableView) {
+        MITPeopleSearchTableViewSection section = indexPath.section;
 
-            case 1:
-                return 60.;
-                
-            case 2:
-            default:
-                return 44.;
+        if (MITPeopleSearchTableViewSectionExample == section) {
+            return 86.;
+        } else if (MITPeopleSearchTableViewSectionContacts == section) {
+            switch (indexPath.row) {
+                case 0:
+                    return 60.;
+                case 1:
+                    return 44.;
+                default:
+                    return 44.;
+            }
+        } else if (MITPeopleSearchTableViewSectionRecentlyViewed == section) {
+            return UITableViewAutomaticDimension;
         }
-	} else {
+	} else if (tableView == self.searchResultsTableView) {
 		return 60.;
 	}
+
+    return UITableViewAutomaticDimension;
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
 {
-    if (section == 1) {
-        return @"Recently Viewed";
-    } else {
-        return nil;
+    if (tableView == self.tableView) {
+        if (MITPeopleSearchTableViewSectionRecentlyViewed == section) {
+            return @"Recently Viewed";
+        }
     }
+
+    return nil;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 	if (tableView == self.searchDisplayController.searchResultsTableView) { // user selected search result
 		PersonDetails *personDetails = nil;
-        
+
+        // TODO: Switch to using the segues
 		UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"People" bundle:nil];
         PeopleDetailsViewController *vc = [storyboard instantiateViewControllerWithIdentifier:@"PeopleDetailsVC"];
 		if (tableView == self.searchDisplayController.searchResultsTableView) {
@@ -294,22 +324,22 @@
 
 		vc.personDetails = personDetails;
 		[self.navigationController pushViewController:vc animated:YES];
-        [tableView deselectRowAtIndexPath:indexPath animated:YES];
 	}
 }
 
 #pragma mark - Connection methods
 
 - (void)showLoadingView {
-	if (!self.loadingView) {
+	if (!self.searchResultsLoadingView) {
         CGRect frame = self.tableView.bounds;
         frame.origin.y = CGRectGetMaxY(self.searchBar.frame);
         frame.size.height -= frame.origin.y;
 
 		MITLoadingActivityView *loadingView = [[MITLoadingActivityView alloc] initWithFrame:frame];
-        self.loadingView = loadingView;
+        self.searchResultsLoadingView = loadingView;
 	}
-    [self.view addSubview:self.loadingView];
+
+    [self.view addSubview:self.searchResultsLoadingView];
 }
 
 #pragma mark - Storyboard Segues
@@ -352,6 +382,24 @@
 		[[UIApplication sharedApplication] openURL:self.directoryPhoneURL];
 }
 
+#pragma mark UISearchDisplay Delegate
+- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView
+{
+    tableView.backgroundView = nil;
+    tableView.backgroundColor = [UIColor mit_backgroundColor];
+    self.searchResultsTableView = tableView;
+}
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    return NO;
+}
+
+- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView
+{
+    // Not really needed since self.searchResultsTableView is weak, but just for correctness-sake
+    self.searchResultsTableView = nil;
+}
 
 @end
 
