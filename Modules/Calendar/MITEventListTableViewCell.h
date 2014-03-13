139a60b268fed99ebb00cdf1e3e09361e615cf5a
Blake Skinner <bskinner@mit.edu>
2014-03-13 14:09:42 -0400

    [#41] Add a nib-based UITableViewCell subclass for the Events table view

---
+++ Modules/Calendar/MITEventListTableViewCell.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface MITEventListTableViewCell : UITableViewCell
+@property (nonatomic,strong) IBOutlet UILabel *headlineLabel;
+@property (nonatomic,strong) IBOutlet UILabel *bodyLabel;
+@property (nonatomic,strong) IBOutlet UILabel *rightBodyLabel;
+
+@end
---
+++ Modules/Calendar/MITEventListTableViewCell.m
@@ -0,0 +1,17 @@
+#import "MITEventListTableViewCell.h"
+
+@implementation MITEventListTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
