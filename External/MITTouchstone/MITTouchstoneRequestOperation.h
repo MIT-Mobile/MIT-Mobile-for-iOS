46ed6536d86be0412a9bc6d7ef8e809da204d1b8
Blake Skinner <bskinner@mit.edu>
2014-04-09 21:40:11 -0400

    [#39] add the designation initializer to the header

--- External/MITTouchstone/MITTouchstoneRequestOperation.h
+++ External/MITTouchstone/MITTouchstoneRequestOperation.h
@@ -4,6 +4,7 @@
 + (void)setUserAgent:(NSString*)userAgent;
 + (NSString*)userAgent;
 
+- (instancetype)initWithRequest:(NSURLRequest*)request;
 - (void)setCompletionBlockWithSuccess:(void (^)(MITTouchstoneRequestOperation *operation, id responseObject))success
                               failure:(void (^)(MITTouchstoneRequestOperation *operation, NSError *error))failure;
 @end
