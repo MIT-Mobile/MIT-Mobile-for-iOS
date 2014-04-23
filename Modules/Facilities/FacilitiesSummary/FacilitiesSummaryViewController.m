20b8d84a85671ffa8c59c792586fa35c733d2c2e
Blake Skinner <bskinner@mit.edu>
2014-04-23 13:22:10 -0400

    Clean up the process for creating the uploaded image a bit.

--- Modules/Facilities/FacilitiesSummary/FacilitiesSummaryViewController.m
+++ Modules/Facilities/FacilitiesSummary/FacilitiesSummaryViewController.m
@@ -258,7 +258,7 @@
 - (void)setAttachedImage:(UIImage *)image {
     NSMutableDictionary *dictionary = [NSMutableDictionary dictionaryWithDictionary:self.reportData];
     if (self.imageData) {
-        [dictionary setObject:self.imageData
+        [dictionary setObject:[self.imageData copy]
                        forKey:FacilitiesRequestImageDataKey];
     } else if (self.imageData == nil) {
         [dictionary removeObjectForKey:FacilitiesRequestImageDataKey];
@@ -421,10 +421,10 @@
 
 #pragma mark - UIImagePickerDelegate
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info {
-    UIImage *image = [info objectForKey:UIImagePickerControllerEditedImage];
+    UIImage *image = info[UIImagePickerControllerEditedImage];
     
-    if (image == nil) {
-        image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    if (!image) {
+        image = info[UIImagePickerControllerOriginalImage];
     }
     
     self.imageData = nil;
@@ -448,50 +448,58 @@
     }
     
     void(^updateBlock)(UIImage*, NSDictionary*) = ^(UIImage *image, NSDictionary *metadata) {
-        NSMutableDictionary *imgProperties = nil;
+        // This is just so we don't have the '(__bridge NSString*)' copy-pasta everywhere
+        NSString* MITCGImageDestinationLossyCompressionQuality = (__bridge NSString*)kCGImageDestinationLossyCompressionQuality;
+        NSString* MITCGImagePropertyOrientation = (__bridge NSString*)kCGImagePropertyOrientation;
+        NSString* MITCGImagePropertyGPSDictionary = (__bridge NSString*)kCGImagePropertyGPSDictionary;
+        NSString* MITCGImagePropertyGPSLatitude = (__bridge NSString*)kCGImagePropertyGPSLatitude;
+        NSString* MITCGImagePropertyGPSLatitudeRef = (__bridge NSString*)kCGImagePropertyGPSLatitudeRef;
+        NSString* MITCGImagePropertyGPSLongitude = (__bridge NSString*)kCGImagePropertyGPSLongitude;
+        NSString* MITCGImagePropertyGPSLongitudeRef = (__bridge NSString*)kCGImagePropertyGPSLongitudeRef;
         
-        if (metadata) {
-            imgProperties = [NSMutableDictionary dictionaryWithDictionary:metadata];
-        } else {
-            imgProperties = [NSMutableDictionary dictionary];
-        }
         
-        NSMutableData *imageData = [NSMutableData data];
-        CGImageDestinationRef imageDestination = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)imageData,
-                                                                                  kUTTypeJPEG,
+        NSData *imageData = UIImageJPEGRepresentation(image, 1.0);
+        CGImageSourceRef imageSource = CGImageSourceCreateWithData((__bridge CFDataRef)imageData, NULL);
+        
+        NSString *imageUTI = (__bridge_transfer NSString*)CGImageSourceGetType(imageSource);
+        DDLogVerbose(@"found %lu images in source of type %@",CGImageSourceGetCount(imageSource),imageUTI);
+        
+        NSMutableData *outputImageData = [[NSMutableData alloc] init];
+        CGImageDestinationRef imageDestination = CGImageDestinationCreateWithData((__bridge CFMutableDataRef)outputImageData,
+                                                                                  (__bridge CFStringRef)imageUTI,
                                                                                   1,
                                                                                   NULL);
         
-        [imgProperties setObject:[NSNumber numberWithFloat:0.75]
-                            forKey:(NSString*)kCGImageDestinationLossyCompressionQuality];
+        NSDictionary *existingMetadata = (__bridge_transfer NSDictionary*)CGImageSourceCopyPropertiesAtIndex(imageSource,0,NULL);
+        NSMutableDictionary *imageMetadata = [[NSMutableDictionary alloc] initWithDictionary:metadata];
+        [imageMetadata addEntriesFromDictionary:existingMetadata];
         
-        if ([imgProperties objectForKey:(NSString*)kCGImagePropertyOrientation] == nil) {
-            [imgProperties setObject:[NSNumber numberWithInteger:[image imageOrientation]]
-                              forKey:(NSString*)kCGImagePropertyOrientation];
+        imageMetadata[MITCGImageDestinationLossyCompressionQuality] = @(0.75);
+        
+        if (!imageMetadata[MITCGImagePropertyOrientation]) {
+            imageMetadata[MITCGImagePropertyOrientation] = @([image imageOrientation]);
         }
         
-        if ([imgProperties objectForKey:(NSString*)kCGImagePropertyGPSDictionary] == nil) {
-            NSMutableDictionary *gpsDict = [NSMutableDictionary dictionaryWithCapacity:5];
+        if (!imageMetadata[MITCGImagePropertyGPSDictionary]) {
+            NSMutableDictionary *gpsMetadata = [[NSMutableDictionary alloc] init];
             CLLocationManager *locationManager = [[CLLocationManager alloc] init];
             CLLocation *location = [locationManager location];
             
             if (location) {
-                gpsDict[(NSString*)kCGImagePropertyGPSLatitude] = @(fabs(location.coordinate.latitude));
-                gpsDict[(NSString*)kCGImagePropertyGPSLatitudeRef] = ((location.coordinate.latitude >= 0) ? @"N" : @"S");
-                gpsDict[(NSString*)kCGImagePropertyGPSLongitude] = @(fabs(location.coordinate.longitude));
-                gpsDict[(NSString*)kCGImagePropertyGPSLongitudeRef] = ((location.coordinate.longitude >= 0) ? @"E" : @"W");
-                gpsDict[(NSString*)kCGImagePropertyGPSTimeStamp] = [location.timestamp descriptionWithLocale:nil];
-                imgProperties[(NSString*)kCGImagePropertyGPSDictionary] = gpsDict;
+                gpsMetadata[MITCGImagePropertyGPSLatitude] = @(fabs(location.coordinate.latitude));
+                gpsMetadata[MITCGImagePropertyGPSLatitudeRef] = ((location.coordinate.latitude >= 0) ? @"N" : @"S");
+                gpsMetadata[MITCGImagePropertyGPSLongitude] = @(fabs(location.coordinate.longitude));
+                gpsMetadata[MITCGImagePropertyGPSLongitudeRef] = ((location.coordinate.longitude >= 0) ? @"E" : @"W");
+                imageMetadata[MITCGImagePropertyGPSDictionary] = gpsMetadata;
             }
         }
         
-        CGImageDestinationAddImage(imageDestination,
-                                   [image CGImage],
-                                   (__bridge CFDictionaryRef)imgProperties);
+        CGImageDestinationAddImageFromSource(imageDestination, imageSource, 0, (__bridge CFDictionaryRef)imageMetadata);
         CGImageDestinationFinalize(imageDestination);
         CFRelease(imageDestination);
+        CFRelease(imageSource);
         
-        self.imageData = imageData;
+        self.imageData = outputImageData;
         
         dispatch_async(dispatch_get_main_queue(), ^(void) {
             self.submitButton.enabled = submitState;
