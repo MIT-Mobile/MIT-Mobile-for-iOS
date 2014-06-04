d6c1353da8c82a69e76063bb83562526eb2caed9
Ross LeBeau <ross.lebeau@gmail.com>
2014-06-03 21:17:12 -0400

    Navbar and buttons for map

--- Modules/ShuttleTrack/MITShuttleMapViewController.h
+++ Modules/ShuttleTrack/MITShuttleMapViewController.h
@@ -3,11 +3,26 @@
 @class MITShuttleRoute;
 @class MITShuttleStop;
 
+@protocol MITShuttleMapViewControllerDelegate;
+
+typedef enum {
+    MITShuttleMapStateContracted = 0,
+    MITShuttleMapStateExpanded
+} MITShuttleMapState;
+
 @interface MITShuttleMapViewController : UIViewController
 
 - (instancetype)initWithRoute:(MITShuttleRoute *)route;
 
 @property (strong, nonatomic) MITShuttleRoute *route;
 @property (strong, nonatomic) MITShuttleStop *stop;
+@property (nonatomic) MITShuttleMapState state;
+@property (nonatomic, weak) id<MITShuttleMapViewControllerDelegate> delegate;
+
+@end
+
+@protocol MITShuttleMapViewControllerDelegate <NSObject>
+
+- (void)shuttleMapViewControllerExitFullscreenButtonPressed:(MITShuttleMapViewController *)mapViewController;
 
 @end
