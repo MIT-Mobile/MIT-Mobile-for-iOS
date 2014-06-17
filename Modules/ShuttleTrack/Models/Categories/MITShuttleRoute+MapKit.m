efa8845695445a8bc6b3a988b3e804744bddbb29
Mark Daigneault <markd@intrepid.io>
2014-06-17 10:02:52 -0400

    Fixed popover disappearance bug when swiping between routes

--- Modules/ShuttleTrack/Models/Categories/MITShuttleRoute+MapKit.m
+++ Modules/ShuttleTrack/Models/Categories/MITShuttleRoute+MapKit.m
@@ -22,4 +22,42 @@
     return MKCoordinateRegionMake(centerCoordinate, boundingBoxSpan);
 }
 
+- (BOOL)pathSegmentsAreValid
+{
+    return [self.pathSegments isKindOfClass:[NSArray class]] &&
+           [self.pathSegments count] > 0 &&
+           [[self.pathSegments firstObject] isKindOfClass:[NSArray class]];
+}
+
+- (NSArray *)pathSegmentPolylines
+{
+    NSArray *pathSegments = [self.pathSegments isKindOfClass:[NSArray class]] ? self.pathSegments : nil;
+    
+    NSMutableArray *segmentPolylines = [NSMutableArray arrayWithCapacity:[pathSegments count]];
+    
+    for (NSInteger i = 0; i < pathSegments.count; i++) {
+        NSArray *pathSegment = [pathSegments[i] isKindOfClass:[NSArray class]] ? pathSegments[i] : nil;
+        
+        if (pathSegment) {
+            CLLocationCoordinate2D segmentPoints[pathSegment.count];
+            
+            for (NSInteger j = 0; j < pathSegment.count; j++) {
+                NSArray *pathCoordinateArray = [pathSegment[j] isKindOfClass:[NSArray class]] ? pathSegment[j] : nil;
+                
+                if (pathCoordinateArray && pathCoordinateArray.count > 1) {
+                    NSNumber *longitude = pathCoordinateArray[0];
+                    NSNumber *latitude = pathCoordinateArray[1];
+                    
+                    CLLocationCoordinate2D pathPointCoordinate = CLLocationCoordinate2DMake([latitude doubleValue], [longitude doubleValue]);
+                    segmentPoints[j] = pathPointCoordinate;
+                }
+            }
+            
+            MKPolyline *segmentPolyline = [MKPolyline polylineWithCoordinates:segmentPoints count:pathSegment.count];
+            [segmentPolylines addObject:segmentPolyline];
+        }
+    }
+    return [NSArray arrayWithArray:segmentPolylines];
+}
+
 @end
