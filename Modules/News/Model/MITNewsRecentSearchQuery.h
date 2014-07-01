7b16124178bf8c464a2f5968b1f147cee5037111
Mark Novak <mnovak@mit.edu>
2014-07-01 12:38:34 -0400

    Added core data skeleton for news search recents.  Modeled after Map search recents

---
+++ Modules/News/Model/MITNewsRecentSearchQuery.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "MITManagedObject.h"
+
+@interface MITNewsRecentSearchQuery : MITManagedObject
+
+@property (nonatomic, retain) NSString * text;
+
+@end
