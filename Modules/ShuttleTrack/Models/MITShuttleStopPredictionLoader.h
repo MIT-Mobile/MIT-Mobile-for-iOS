3bce83bebccd9ae959957673137d8e4a4009f948
Mark Daigneault <markd@intrepid.io>
2014-07-08 11:15:15 -0400

    Implemented background fetch for stop predictions to update local notifications

--- Modules/ShuttleTrack/Models/MITShuttleStopPredictionLoader.h
+++ Modules/ShuttleTrack/Models/MITShuttleStopPredictionLoader.h
@@ -14,6 +14,7 @@
 - (instancetype)initWithStop:(MITShuttleStop *)stop;
 - (NSDictionary *)predictionsByRoute;
 
+- (void)reloadPredictions;
 - (void)startRefreshingPredictions;
 - (void)stopRefreshingPredictions;
 
