805ad727ad11e86493d35482419b16e17eadbe42
Mark Novak <mnovak@mit.edu>
2014-07-17 12:35:57 -0400

    Subclassing MITNewsListViewController for use with Category Lists

---
+++ Modules/News/iPad/View Controllers/MITNewsCategoryListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MITNewsCategoryListViewController.h
+//  MIT Mobile
+//
+//  Created by Mark on 7/17/14.
+//
+//
+
+#import "MITNewsListViewController.h"
+
+@interface MITNewsCategoryListViewController : MITNewsListViewController
+
+@end
