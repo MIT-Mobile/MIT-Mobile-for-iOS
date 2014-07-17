12f6c0e9f5142105cc99d0c98498d1410a3b028b
Mark Novak <mnovak@mit.edu>
2014-07-17 13:56:29 -0400

    remove unneeded Created by:

--- Modules/News/iPad/View Controllers/MITNewsCategoryListViewController.h
+++ Modules/News/iPad/View Controllers/MITNewsCategoryListViewController.h
@@ -1,11 +1,3 @@
-//
-//  MITNewsCategoryListViewController.h
-//  MIT Mobile
-//
-//  Created by Mark on 7/17/14.
-//
-//
-
 #import "MITNewsListViewController.h"
 
 @interface MITNewsCategoryListViewController : MITNewsListViewController
