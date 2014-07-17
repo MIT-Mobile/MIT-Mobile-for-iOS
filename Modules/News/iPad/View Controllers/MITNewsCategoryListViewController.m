a652419f240df9bb5e063f9c4049a905e86eb6e9
Mark Novak <mnovak@mit.edu>
2014-07-17 12:35:57 -0400

    Subclassing MITNewsListViewController for use with Category Lists

---
+++ Modules/News/iPad/View Controllers/MITNewsCategoryListViewController.m
@@ -0,0 +1,49 @@
+//
+//  MITNewsCategoryListViewController.m
+//  MIT Mobile
+//
+//  Created by Mark on 7/17/14.
+//
+//
+
+#import "MITNewsCategoryListViewController.h"
+
+@interface MITNewsCategoryListViewController ()
+
+@end
+
+@implementation MITNewsCategoryListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
