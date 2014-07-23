d17b8b84cd3d54b881ac1a5abaff32ac24b45108
Mark Novak <mnovak@mit.edu>
2014-07-23 13:15:28 -0400

    Changed identifier and Fixed view hierarchy for load more cell.

--- Modules/News/View Controllers/MITNewsConstants.m
+++ Modules/News/View Controllers/MITNewsConstants.m
@@ -24,6 +24,6 @@ NSString* const MITNewsCellIdentifierStoryJumbo = @"NewsStoryJumboCollectionView
 NSString* const MITNewsCellIdentifierStoryDek = @"NewsStoryDekCollectionViewCell";
 NSString* const MITNewsCellIdentifierStoryClip = @"NewsStoryClipCollectionViewCell";
 NSString* const MITNewsCellIdentifierStoryWithImage = @"NewsStoryImageCollectionViewCell";
-NSString* const MITNewsCellIdentifierStoryLoadMore = @"NewsStoryLoadingMoreCollectionView";
+NSString* const MITNewsCellIdentifierStoryLoadMore = @"NewsStoryLoadingMoreCollectionViewCell";
 NSString* const MITNewsReusableViewIdentifierSectionHeader = @"NewsStoryHeaderReusableView";
 NSString* const MITNewsReusableViewIdentifierDivider = @"MITNewsCollectionDecorationDividerIdentifier";
