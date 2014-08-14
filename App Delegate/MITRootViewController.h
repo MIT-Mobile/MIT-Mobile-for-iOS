979d65ee2238b83f9a2927d53f78e21a8f5bf31f
Blake Skinner <bskinner@mit.edu>
2014-08-14 16:00:30 -0400

    Start ripping out all of the springboard-related code
    Build is broken in this commit

--- App Delegate/MITRootViewController.h
+++ App Delegate/MITRootViewController.h
@@ -1,19 +1,10 @@
 #import <UIKit/UIKit.h>
 
-typedef NS_ENUM(NSUInteger, MITInterfaceStyle) {
-    MITInterfaceStyleSpringboard = 0,
-    MITInterfaceStyleDrawer
-};
-
 @interface MITRootViewController : UIViewController
-@property (nonatomic) MITInterfaceStyle interfaceStyle;
 @property (nonatomic,copy) NSArray *modules;
 @property (nonatomic,getter=isLeftDrawerVisible) BOOL leftDrawerVisible;
 
 - (BOOL)showModuleWithTag:(NSString*)module;
 - (BOOL)showModuleWithTag:(NSString*)module animated:(BOOL)animated;
-
-#pragma mark Drawer-style interface specific
-// Has no effect when using the springboard interface style
 - (void)setLeftDrawerVisible:(BOOL)visible animated:(BOOL)animated;
 @end
--- App Delegate/MITRootViewController.m
+++ App Delegate/MITRootViewController.m
@@ -12,15 +12,8 @@
 @property (nonatomic,strong) NSArray *availableModules;
 @property (nonatomic,weak) MITModule *lastSelectedModule;
 
-// Used by the MITInterfaceStyleDrawer interface style.
-// The ECSlidingViewController is the root view controller and the launcher list view controller
-// is in the left drawer.
 @property (nonatomic,weak) ECSlidingViewController *rootDrawerViewController;
-@property (nonatomic,weak) MITLauncherListViewController *moduleListViewController;
-
-// Used by the MITInterfaceStyleSpringboard interface style
-@property (nonatomic,weak) UINavigationController *rootNavigationViewController;
-@property (nonatomic,weak) MITLauncherGridViewController *moduleSpringboardViewController;
+@property (nonatomic,weak) UITableViewController *moduleListViewController;
 @end
 
 @implementation MITRootViewController
@@ -49,10 +42,9 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
-    [self transitionToInterfaceStyle:self.interfaceStyle animated:NO];
-
-    if (!self.lastSelectedModule && (self.interfaceStyle == MITInterfaceStyleDrawer)) {
-
+    
+    if (!self.lastSelectedModule) {
+        [self _presentModule:[self.modules firstObject] animated:animated];
     }
 }
 
@@ -84,7 +76,7 @@
     }
 }
 
-- (void)_presentModule:(MITModule*)module forInterfaceStyle:(MITInterfaceStyle)style animated:(BOOL)animated
+- (void)_presentModule:(MITModule*)module animated:(BOOL)animated
 {
     if (style == MITInterfaceStyleDrawer) {
         UIViewController *moduleViewController = [module homeViewController];
@@ -144,96 +136,6 @@
     }
 }
 
-#pragma mark Changing the interface style
-- (void)transitionToInterfaceStyle:(MITInterfaceStyle)style animated:(BOOL)animated
-{
-    BOOL needsInitialTransition = !(self.rootDrawerViewController || self.rootNavigationViewController);
-
-    if (needsInitialTransition || (self.interfaceStyle != style)) {
-        MITInterfaceStyle oldStyle = self.interfaceStyle;
-        [self willTransitionToInterfaceStyle:style animated:animated];
-
-        _interfaceStyle = style;
-        if (style == MITInterfaceStyleSpringboard) {
-            [self _presentViewControllerForSpringboardInterface:animated completion:nil];
-        } else if (style == MITInterfaceStyleDrawer) {
-            [self _presentViewControllerForSpringboardInterface:animated completion:nil];
-        } else {
-            @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"unable to transition to new style, requested interface style is unknown" userInfo:nil];
-        }
-        
-        [self didTransitionFromInterfaceStyle:oldStyle animated:animated];
-    }
-}
-
-- (void)_transitionFromViewController:(UIViewController*)fromViewController toViewController:(UIViewController*)toViewController animated:(BOOL)animated completion:(void (^)(BOOL finished))block
-{
-    NSParameterAssert(toViewController);
-    
-    toViewController.view.translatesAutoresizingMaskIntoConstraints = YES;
-    toViewController.view.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    toViewController.view.frame = self.view.bounds;
-    [self addChildViewController:toViewController];
-    
-    NSTimeInterval duration = (animated ? 0.25 : 0.);
-    if (fromViewController) {
-        // An existing drawer controller already exists, we'll need to transition between the two
-        [fromViewController willMoveToParentViewController:nil];
-        [self transitionFromViewController:fromViewController
-                          toViewController:toViewController
-                                  duration:duration
-                                   options:(UIViewAnimationOptionBeginFromCurrentState |
-                                            UIViewAnimationOptionTransitionCrossDissolve)
-                                animations:^{
-                                    // Nothing here at the moment
-                                } completion:^(BOOL finished) {
-                                    [fromViewController removeFromParentViewController];
-                                    [toViewController didMoveToParentViewController:self];
-                                    
-                                    if (block) {
-                                        block(finished);
-                                    }
-                                }];
-    } else {
-        // Otherwise, this is the first view controller being added, so play nice
-        [self.view addSubview:toViewController.view];
-        toViewController.view.alpha = 0.;
-        [UIView animateWithDuration:duration
-                         animations:^{
-                             // Just face the view in at this point
-                             toViewController.view.alpha = 1.;
-                         } completion:^(BOOL finished) {
-                             [toViewController didMoveToParentViewController:self];
-                             if (block) {
-                                 block(finished);
-                             }
-                         }];
-    }
-}
-
-- (void)_presentViewControllerForSpringboardInterface:(BOOL)animated completion:(void (^)(BOOL finished))block
-{
-    MITLauncherGridViewController *springboardViewController = [[MITLauncherGridViewController alloc] init];
-    springboardViewController.dataSource = self;
-    springboardViewController.delegate = self;
-
-    UIImage *logoView = [UIImage imageNamed:@"global/navbar_mit_logo_dark"];
-    springboardViewController.navigationItem.titleView = [[UIImageView alloc] initWithImage:logoView];
-    springboardViewController.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Home" style:UIBarButtonItemStyleBordered target:nil action:nil];
-
-    UINavigationController *navigationController = [[MITNavigationController alloc] initWithRootViewController:springboardViewController];
-    navigationController.navigationBarHidden = NO;
-    navigationController.toolbarHidden = YES;
-
-    navigationController.navigationBar.barStyle = UIBarStyleDefault;
-    navigationController.navigationBar.translucent = YES;
-
-    self.rootNavigationViewController = navigationController;
-    self.moduleSpringboardViewController = springboardViewController;
-
-    [self _transitionFromViewController:self.rootDrawerViewController toViewController:self.rootNavigationViewController animated:YES completion:block];
-}
-
 - (void)_presentViewControllerForDrawerInterface:(BOOL)animated completion:(void (^)(BOOL finished))block
 {
     if (self.rootDrawerViewController) {
@@ -253,21 +155,7 @@
             @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:@"the springboard interface style is active but a drawer view controller already exists" userInfo:nil];
         }
     } else {
-        // Using a dummy view controller to keep the sliding view controller happy (it needs any view
-        //  view controller when allocated). This will be replaced once we start activating modules
-        UIViewController *dummyViewController = [[UIViewController alloc] init];
-        dummyViewController.view.backgroundColor = [UIColor mit_backgroundColor];
-        ECSlidingViewController *drawerViewController = [[ECSlidingViewController alloc] initWithTopViewController:dummyViewController];
 
-        MITLauncherListViewController *launcherViewController = [[MITLauncherListViewController alloc] init];
-        launcherViewController.dataSource = self;
-        launcherViewController.delegate = self;
-        launcherViewController.edgesForExtendedLayout = (UIRectEdgeLeft | UIRectEdgeRight | UIRectEdgeBottom);
-
-        self.moduleListViewController = launcherViewController;
-
-        drawerViewController.underLeftViewController = launcherViewController;
-        drawerViewController.anchorRightRevealAmount = 280.;
 
         [self _transitionFromViewController:self.rootNavigationViewController toViewController:self.rootDrawerViewController animated:YES completion:block];
     }
