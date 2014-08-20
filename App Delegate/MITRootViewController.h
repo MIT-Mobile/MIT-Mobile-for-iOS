d37992a0b655577fffdd5f4c1e968bc1b40c8ec5
Blake Skinner <bskinner@mit.edu>
2014-08-20 08:51:31 -0400

    Removed the leftDrawerVisible property for now

--- App Delegate/MITRootViewController.h
+++ App Delegate/MITRootViewController.h
@@ -2,9 +2,7 @@
 
 @interface MITRootViewController : UIViewController
 @property (nonatomic,copy) NSArray *modules;
-@property (nonatomic,getter=isLeftDrawerVisible) BOOL leftDrawerVisible;
 
 - (BOOL)showModuleWithTag:(NSString*)module;
 - (BOOL)showModuleWithTag:(NSString*)module animated:(BOOL)animated;
-- (void)setLeftDrawerVisible:(BOOL)visible animated:(BOOL)animated;
 @end
