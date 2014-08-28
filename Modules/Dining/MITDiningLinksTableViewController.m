8148e651c36c57b8acfe5ae6c0baff7a5ff9d1d0
Ross LeBeau <ross.lebeau@gmail.com>
2014-08-28 09:46:15 -0400

    Cleanup of auto-gen code and comments

--- Modules/Dining/MITDiningLinksTableViewController.m
+++ Modules/Dining/MITDiningLinksTableViewController.m
@@ -4,10 +4,6 @@
 
 static NSString *const kMITDiningLinksTableViewControllerCell = @"kMITDiningLinksTableViewControllerCell";
 
-@interface MITDiningLinksTableViewController ()
-
-@end
-
 @implementation MITDiningLinksTableViewController
 
 - (id)initWithStyle:(UITableViewStyle)style
@@ -22,12 +18,6 @@ static NSString *const kMITDiningLinksTableViewControllerCell = @"kMITDiningLink
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:kMITDiningLinksTableViewControllerCell];
 }
@@ -67,54 +57,5 @@ static NSString *const kMITDiningLinksTableViewControllerCell = @"kMITDiningLink
     MITDiningLinks *link = self.diningLinks[indexPath.row];
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:link.url]];
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
