e5b9e9f42e93a2d1cf7fd6446d62970d53287a57
Blake Skinner <bskinner@mit.edu>
2014-09-03 16:01:23 -0400

    Add a skeleton xib for the drawer view

---
+++ App Delegate/MITDrawerViewController.h
@@ -0,0 +1,5 @@
+#import <UIKit/UIKit.h>
+
+@interface MITDrawerViewController : UIViewController
+
+@end
---
+++ App Delegate/MITDrawerViewController.m
@@ -0,0 +1,19 @@
+#import "MITDrawerViewController.h"
+
+@interface MITDrawerViewController ()
+
+@end
+
+@implementation MITDrawerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
