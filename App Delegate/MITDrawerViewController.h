ee62f67bcf11ee00e8916b0bf63ba0d1d15cca90
Blake Skinner <bskinner@mit.edu>
2014-09-09 08:36:13 -0400

    UI and behavior changes
    * Add the top and bottom gradient views
    * Clean up the row selection code

--- App Delegate/MITDrawerViewController.h
+++ App Delegate/MITDrawerViewController.h
@@ -2,7 +2,7 @@
 
 @class MITGradientView;
 
-@interface MITDrawerViewController : UIViewController
+@interface MITDrawerViewController : UIViewController <UITableViewDelegate, UITableViewDataSource>
 @property (nonatomic,copy) NSArray *modules;
 @property (nonatomic,weak) MITModule *selectedModule;
 
--- App Delegate/MITDrawerViewController.m
+++ App Delegate/MITDrawerViewController.m
@@ -2,15 +2,17 @@
 #import "UIViewController+MITDrawerNavigation.h"
 #import "MITModule.h"
 #import "MITSlidingViewController.h"
+#import "MITGradientView.h"
 
 static NSString* const MITDrawerReuseIdentifierItemCell = @"DrawerItemCellReuseIdentifier";
 static NSUInteger const MITModuleSectionIndex = 0;
 
 @interface MITDrawerViewController ()
-@property(nonatomic,strong) IBOutlet UIView *topView;
-@property(nonatomic,strong) IBOutlet UIImageView *logoView;
-@property(nonatomic,strong) IBOutlet UITableView *tableView;
-@property(nonatomic,strong) IBOutlet MITGradientView *gradientView;
+@property(nonatomic,weak) IBOutlet UIView *logoContainer;
+@property(nonatomic,weak) IBOutlet UIImageView *logoView;
+@property(nonatomic,weak) IBOutlet UITableView *tableView;
+@property(nonatomic,weak) IBOutlet MITGradientView *topGradientView;
+@property(nonatomic,weak) IBOutlet MITGradientView *bottomGradientView;
 
 @property (nonatomic,strong) NSIndexPath *selectedIndexPath;
 @end
@@ -19,12 +21,18 @@ static NSUInteger const MITModuleSectionIndex = 0;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.topGradientView.direction = CGRectMinYEdge;
+    self.bottomGradientView.direction = CGRectMaxYEdge;
+    
     // Do any additional setup after loading the view from its nib.
 }
 
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    
+    self.tableView.contentInset = UIEdgeInsetsMake(CGRectGetHeight(self.topGradientView.frame), 0, 0, 0);
     [self.tableView reloadData];
 }
 
@@ -32,6 +40,39 @@ static NSUInteger const MITModuleSectionIndex = 0;
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark Properties
+- (void)setModules:(NSArray *)modules
+{
+    if (![_modules isEqualToArray:modules]) {
+        _modules = [modules copy];
+        [self.tableView reloadData];
+    }
+}
+
+- (void)setSelectedModule:(MITModule *)module
+{
+    NSIndexPath *selectedIndexPath = [self _indexPathForModule:module];
+    if (selectedIndexPath) {
+        _selectedModule = module;
+        self.selectedIndexPath = selectedIndexPath;
+    }
+}
+
+- (void)setSelectedIndexPath:(NSIndexPath *)selectedIndexPath
+{
+    [self setSelectedIndexPath:selectedIndexPath animated:NO];
+}
+
+- (void)setSelectedIndexPath:(NSIndexPath *)selectedIndexPath animated:(BOOL)animated
+{
+    if (![_selectedIndexPath isEqual:selectedIndexPath]) {
+        NSIndexPath *previousIndexPath = _selectedIndexPath;
+        _selectedIndexPath = selectedIndexPath;
+    }
+}
+
+#pragma mark Private
 
 - (MITModule*)_moduleForIndexPath:(NSIndexPath*)indexPath
 {
@@ -62,7 +103,7 @@ static NSUInteger const MITModuleSectionIndex = 0;
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return [[MIT_MobileAppDelegate applicationDelegate].modules count];
+    return [self.modules count];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -84,13 +125,16 @@ static NSUInteger const MITModuleSectionIndex = 0;
     if ([cell isKindOfClass:[UITableViewCell class]]) {
         MITModule *module = [self _moduleForIndexPath:indexPath];
 
+        cell.selectionStyle = UITableViewCellSelectionStyleNone;
         cell.imageView.image = module.springboardIcon;
         cell.textLabel.text = module.shortName;
 
         if ([self.selectedIndexPath isEqual:indexPath]) {
+            cell.selected = YES;
             cell.contentView.backgroundColor = self.view.tintColor;
         } else {
-            cell.contentView.backgroundColor = self.view.backgroundColor;
+            cell.selected = NO;
+            cell.contentView.backgroundColor = [UIColor clearColor];
         }
     }
 
@@ -108,14 +152,14 @@ static NSUInteger const MITModuleSectionIndex = 0;
 #warning replace with +[NSIndexPath indexPathForIndexPath:]
     indexPath = [NSIndexPath indexPathForRow:indexPath.row inSection:indexPath.section];
 
-    MITSlidingViewController *slidingViewController = [[MIT_MobileAppDelegate applicationDelegate] rootViewController];
-    MITModule *module = [self _moduleForIndexPath:indexPath];
-    if (![module isEqual:slidingViewController.visibleModule]) {
-        slidingViewController.visibleModule = module;
+    if (![self.selectedIndexPath isEqual:indexPath]) {
         NSIndexPath *previousIndexPath = self.selectedIndexPath;
         self.selectedIndexPath = indexPath;
-
-        [tableView reloadRowsAtIndexPaths:@[self.selectedIndexPath,previousIndexPath] withRowAnimation:UITableViewRowAnimationNone];
+        
+        MITModule *module = [self _moduleForIndexPath:indexPath];
+        [MIT_MobileAppDelegate applicationDelegate].rootViewController.visibleModule = module;
+        
+        [self.tableView reloadRowsAtIndexPaths:@[self.selectedIndexPath,previousIndexPath] withRowAnimation:UITableViewScrollPositionNone];
     }
 }
 
