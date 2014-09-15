c80d5ce1e8d3d9db37165f88246014946e031908
Ross LeBeau <ross.lebeau@gmail.com>
2014-09-14 20:46:44 -0400

    Used tap gesture recognizer instead of textview delegate method to improve responsiveness when tapping dietary flags

--- Modules/Dining/MITDiningHallMealCollectionCell.m
+++ Modules/Dining/MITDiningHallMealCollectionCell.m
@@ -36,6 +36,52 @@
     [super awakeFromNib];
     self.mealTitleTextView.contentInset = UIEdgeInsetsMake(0, -5, 0, -5);
     self.mealTitleTextView.textContainerInset = UIEdgeInsetsMake(0, 0, 0, 0);
+    [self.mealTitleTextView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mealTitleTapped:)]];
+}
+
+- (void)mealTitleTapped:(UITapGestureRecognizer *)tapGestureRecognizer
+{
+    if (![self.menuItem.dietaryFlags isKindOfClass:[NSArray class]]) {
+        return;
+    } else if (((NSArray *)self.menuItem.dietaryFlags).count < 1) {
+        return;
+    }
+    
+    // We need to find the dietary flag images and search to see if the tap is in the bounding box for each of them
+    // We search each one separately in case there is overflow due to lots of flags and/or a very long final word. Can't count on the bounding box for all of them together not to contain other areas
+    NSMutableArray *flagRangesArray = [NSMutableArray array];
+    [self.mealTitleTextView.attributedText enumerateAttributesInRange:NSMakeRange(0, self.mealTitleTextView.attributedText.length) options:0 usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
+        if ([attrs objectForKey:NSAttachmentAttributeName]) {
+            [flagRangesArray addObject:[NSValue valueWithRange:range]];
+        }
+    }];
+    
+    if (flagRangesArray.count > 0) {
+        BOOL tapIsOnFlag = NO;
+        CGPoint tapLocation = [tapGestureRecognizer locationInView:self.mealTitleTextView];
+        
+        for (NSValue *rangeValue in flagRangesArray) {
+            NSRange flagCharacterRange = [rangeValue rangeValue];
+            NSRange flagGlyphRange = [self.mealTitleTextView.layoutManager glyphRangeForCharacterRange:flagCharacterRange actualCharacterRange:NULL];
+            CGRect flagBoundingBox = [self.mealTitleTextView.layoutManager boundingRectForGlyphRange:flagGlyphRange inTextContainer:self.mealTitleTextView.textContainer];
+            if (CGRectContainsPoint(flagBoundingBox, tapLocation)) {
+                tapIsOnFlag = YES;
+                break;
+            }
+        }
+        
+        if (tapIsOnFlag) {
+            MITDiningDietaryFlagListViewController *flagsVC = [[MITDiningDietaryFlagListViewController alloc] init];
+            flagsVC.flags = self.menuItem.dietaryFlags;
+            self.dietaryFlagPopoverController = [[UIPopoverController alloc] initWithContentViewController:flagsVC];
+            self.dietaryFlagPopoverController.popoverContentSize = [flagsVC targetTableViewSize];
+            
+            NSRange lastFlagCharacterRange = [flagRangesArray.lastObject rangeValue];
+            NSRange flagsGlyphRange = [self.mealTitleTextView.layoutManager glyphRangeForCharacterRange:lastFlagCharacterRange actualCharacterRange:NULL];
+            CGRect dietaryFlagRect = [self.mealTitleTextView.layoutManager boundingRectForGlyphRange:flagsGlyphRange inTextContainer:self.mealTitleTextView.textContainer];
+            [self.dietaryFlagPopoverController presentPopoverFromRect:dietaryFlagRect inView:self.mealTitleTextView permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+        }
+    }
 }
 
 #pragma mark - Public Methods
@@ -59,37 +105,6 @@
     self.mealTitleTextView.attributedText = [[NSAttributedString alloc] initWithAttributedString:dietaryFlagString];
     
     _menuItem = menuItem;
-}
-
-#pragma mark - UITextViewDelegate Methods
-
-- (BOOL)textView:(UITextView *)textView shouldInteractWithTextAttachment:(NSTextAttachment *)textAttachment inRange:(NSRange)characterRange
-{
-    if (![self.menuItem.dietaryFlags isKindOfClass:[NSArray class]]) {
-        return NO;
-    }
-    
-    __block BOOL foundFlag = NO;
-    __block NSUInteger lastFlagLocation = 0;
-    [self.mealTitleTextView.attributedText enumerateAttributesInRange:NSMakeRange(0, self.mealTitleTextView.attributedText.length) options:0 usingBlock:^(NSDictionary *attrs, NSRange range, BOOL *stop) {
-        if ([attrs objectForKey:NSAttachmentAttributeName]) {
-            foundFlag = YES;
-            lastFlagLocation = range.location;
-        }
-    }];
-    
-    if (foundFlag) {
-        MITDiningDietaryFlagListViewController *flagsVC = [[MITDiningDietaryFlagListViewController alloc] init];
-        flagsVC.flags = self.menuItem.dietaryFlags;
-        self.dietaryFlagPopoverController = [[UIPopoverController alloc] initWithContentViewController:flagsVC];
-        self.dietaryFlagPopoverController.popoverContentSize = [flagsVC targetTableViewSize];
-        
-        NSRange flagsGlyphRange = [self.mealTitleTextView.layoutManager glyphRangeForCharacterRange:NSMakeRange(lastFlagLocation, 1) actualCharacterRange:NULL];
-        CGRect dietaryFlagRect = [self.mealTitleTextView.layoutManager boundingRectForGlyphRange:flagsGlyphRange inTextContainer:self.mealTitleTextView.textContainer];
-        [self.dietaryFlagPopoverController presentPopoverFromRect:dietaryFlagRect inView:self.mealTitleTextView permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
-    }
-    
-    return NO;
 }
 
 #pragma mark - Determining Dynamic Cell Height
