2a226d5124021067d4a0f32b8430dd2bd433e191
Mark Novak <mnovak@mit.edu>
2014-10-01 11:39:56 -0400

    Fix for rotation and back button on Image Gallery.  currentContext doesn't allow for a clean rotation.  View would still be in orientation it was before the rotate.

--- Modules/News/View Controllers/MITNewsMediaGalleryViewController.m
+++ Modules/News/View Controllers/MITNewsMediaGalleryViewController.m
@@ -36,6 +36,13 @@
     return self;
 }
 
+
+- (IBAction)goBackward:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:^() {
+        [self performSegueWithIdentifier:@"unwindFromImageGallery" sender:self];
+    }];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
