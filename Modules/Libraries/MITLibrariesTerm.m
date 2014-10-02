cedf93192c83bf2acc2f3f75e57b76ce2a60b76b
Sam Voigt <sam@intrepid.io>
2014-10-02 15:19:58 -0400

    Exceptions and closings are now ordered by date

--- Modules/Libraries/MITLibrariesTerm.m
+++ Modules/Libraries/MITLibrariesTerm.m
@@ -5,6 +5,7 @@
 #import "MITLibrariesClosingsTerm.h"
 #import "MITLibrariesExceptionsTerm.h"
 #import "Foundation+MITAdditions.h"
+#import "MITLibrariesTermProtocol.h"
 
 @implementation MITLibrariesTerm
 
@@ -44,14 +45,20 @@
         hoursDescription = [NSString stringWithFormat:@"%@%@\n", hoursDescription, [term termHoursDescription]];
     }
     
-    for (MITLibrariesClosingsTerm *term in self.closingsTerm) {
+    NSMutableArray *exceptionsAndClosings = [[NSMutableArray alloc] initWithCapacity:self.closingsTerm.count + self.exceptionsTerm.count];
+    [exceptionsAndClosings addObjectsFromArray:self.closingsTerm];
+    [exceptionsAndClosings addObjectsFromArray:self.exceptionsTerm];
+    
+    [exceptionsAndClosings sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        id<MITLibrariesTermProtocol> firstTerm = obj1;
+        id<MITLibrariesTermProtocol> secondTerm = obj2;
+        return [firstTerm.dates.startDate compare:secondTerm.dates.startDate];
+    }];
+    
+    for (id<MITLibrariesTermProtocol> term in exceptionsAndClosings) {
         hoursDescription = [NSString stringWithFormat:@"%@%@\n", hoursDescription, [term termHoursDescription]];
     }
-    
-    for (MITLibrariesClosingsTerm *term in self.exceptionsTerm) {
-        hoursDescription = [NSString stringWithFormat:@"%@%@\n", hoursDescription, [term termHoursDescription]];
-    }
-    
+        
     return [hoursDescription stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
 }
 
