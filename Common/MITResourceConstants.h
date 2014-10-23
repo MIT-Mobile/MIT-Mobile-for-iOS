096610b44384c2e3a1901a0aac168ae0c315545a
Blake Skinner <bskinner@mit.edu>
2014-10-23 12:32:14 -0400

    Initial commit of a dedicated header for the image asset names

---
+++ Common/MITResourceConstants.h
@@ -0,0 +1,116 @@
+#ifndef MIT_Mobile_MITResourceConstants_h
+#define MIT_Mobile_MITResourceConstants_h
+
+#pragma mark Module Icons
+extern NSString * const MITImageAboutModuleIcon;
+extern NSString * const MITImageBuildingServicesModuleIcon;
+extern NSString * const MITImageEventsModuleIcon;
+extern NSString * const MITImageMapModuleIcon;
+extern NSString * const MITImageDiningModuleIcon;
+extern NSString * const MITImageEmergencyModuleIcon;
+extern NSString * const MITImageLibrariesModuleIcon;
+extern NSString * const MITImageLinksModuleIcon;
+extern NSString * const MITImageNewsModuleIcon;
+extern NSString * const MITImagePeopleModuleIcon;
+extern NSString * const MITImageScannerModuleIcon;
+extern NSString * const MITImageSettingsModuleIcon;
+extern NSString * const MITImageShuttlesModuleIcon;
+extern NSString * const MITImageToursModuleIcon;
+
+#pragma - mark Dining
+extern NSString * const MITImageDiningBookmark;
+extern NSString * const MITImageDiningBookmarkSelected;
+extern NSString * const MITImageDiningInfo;
+extern NSString * const MITImageDiningInfoSelected;
+extern NSString * const MITImageDiningRotateDevice;
+
+#pragma mark Meal Types
+extern NSString * const MITImageDiningMealFarmToFork;
+extern NSString * const MITImageDiningMealGlutenFree;
+extern NSString * const MITImageDiningMealHalal;
+extern NSString * const MITImageDiningMealHumane;
+extern NSString * const MITImageDiningMealInBalance;
+extern NSString * const MITImageDiningMealKosher;
+extern NSString * const MITImageDiningMealOrganic;
+extern NSString * const MITImageDiningMealSeafoodWatch;
+extern NSString * const MITImageDiningMealVegan;
+extern NSString * const MITImageDiningMealVegetarian;
+extern NSString * const MITImageDiningMealWellBeing;
+
+#pragma mark - Events (Calendar)
+extern NSString * const MITImageEventsDayPicker;
+extern NSString * const MITImageEventsPadChevronUp;
+extern NSString * const MITImageEventsPadChevronDown;
+
+#pragma mark - Libraries
+extern NSString * const MITImageLibrariesRadioCheckbox;
+extern NSString * const MITImageLibrariesRadioCheckboxSelected;
+
+#pragma mark Status Types
+extern NSString * const MITImageLibrariesStatusAlert;
+extern NSString * const MITImageLibrariesStatusError;
+extern NSString * const MITImageLibrariesStatusOK;
+extern NSString * const MITImageLibrariesStatusReady;
+
+#pragma mark - Map
+extern NSString * const MITImageMapBrowseBuildings;
+extern NSString * const MITImageMapBrowseFoodServices;
+extern NSString * const MITImageMapBrowseResidences;
+extern NSString * const MITImageMapLocation;
+extern NSString * const MITImageMapLocationSelected;
+extern NSString * const MITImageMapAnnotationUserLocation;
+extern NSString * const MITImageMapAnnotationPin;
+
+#pragma mark - News
+extern NSString * const MITImageNewsImagePlaceholder;
+
+#pragma mark - People Directory
+extern NSString * const MITImagePeopleDirectoryDestructiveButton;
+extern NSString * const MITImagePeopleDirectoryDestructiveButtonHighlighted;
+
+#pragma mark - Scanner
+extern NSString * const MITImageScannerCameraUnsupported;
+extern NSString * const MITImageScannerSampleBarcode;
+extern NSString * const MITImageScannerSampleQRCode;
+extern NSString * const MITImageScannerMissingImage;
+extern NSString * const MITImageScannerScanBarButton;
+
+#pragma mark - Shuttles
+extern NSString * const MITImageShuttlesShuttleRouteActive;
+extern NSString * const MITImageShuttlesShuttleRouteInactive;
+extern NSString * const MITImageShuttlesShuttleRouteLocationUnavailable;
+extern NSString * const MITImageShuttlesBusBubble;
+extern NSString * const MITImageShuttlesAlertEnabled;
+extern NSString * const MITImageShuttlesAlertDisabled;
+extern NSString * const MITImageShuttlesAnnotationBus;
+extern NSString * const MITImageShuttlesAnnotationNextStop;
+extern NSString * const MITImageShuttlesAnnotationNextStopSelected;
+extern NSString * const MITImageShuttlesAnnotationCurrentStop;
+extern NSString * const MITImageShuttlesAnnotationCurrentStopSelected;
+
+extern NSString * const MITImageNameEmail;
+extern NSString * const MITImageNameEmailHighlight;
+extern NSString * const MITImageNameMap;
+extern NSString * const MITImageNameMapHighlight;
+extern NSString * const MITImageNamePeople;
+extern NSString * const MITImageNamePeopleHighlight;
+extern NSString * const MITImageNamePhone;
+extern NSString * const MITImageNamePhoneHighlight;
+extern NSString * const MITImageNameExternal;
+extern NSString * const MITImageNameExternalHighlight;
+extern NSString * const MITImageNameEmergency;
+extern NSString * const MITImageNameEmergencyHighlight;
+extern NSString * const MITImageNameSecure;
+extern NSString * const MITImageNameSecureHighlight;
+extern NSString * const MITImageNameCalendar;
+extern NSString * const MITImageNameCalendarHighlight;
+
+extern NSString * const MITImageNameLeftArrow;
+extern NSString * const MITImageNameRightArrow;
+extern NSString * const MITImageNameUpArrow;
+extern NSString * const MITImageNameDownArrow;
+
+extern NSString * const MITImageNameSearch;
+extern NSString * const MITImageNameBookmark;
+
+#endif
---
+++ Common/MITResourceConstants.m
@@ -0,0 +1,117 @@
+#import <Foundation/Foundation.h>
+
+#pragma mark Module Icons
+NSString * const MITImageAboutModuleIcon = @"home-about";
+NSString * const MITImageBuildingServicesModuleIcon = @"home-facilities";
+NSString * const MITImageEventsModuleIcon = @"home-calendar";
+NSString * const MITImageMapModuleIcon = @"home-map";
+NSString * const MITImageDiningModuleIcon = @"home-dining";
+NSString * const MITImageEmergencyModuleIcon = @"home-emergency";
+NSString * const MITImageLibrariesModuleIcon = @"home-libraries";
+NSString * const MITImageLinksModuleIcon = @"home-webmitedu";
+NSString * const MITImageNewsModuleIcon = @"home-news";
+NSString * const MITImagePeopleModuleIcon = @"home-people";
+NSString * const MITImageScannerModuleIcon = @"home-qrreader";
+NSString * const MITImageSettingsModuleIcon = @"home-settings";
+NSString * const MITImageShuttlesModuleIcon = @"home-shuttle";
+NSString * const MITImageToursModuleIcon = @"home-tours";
+
+#pragma - mark Dining
+NSString * const MITImageDiningBookmark = @"dining-bookmark";
+NSString * const MITImageDiningBookmarkSelected = @"dining-bookmark-selected";
+NSString * const MITImageDiningInfo = @"dining-info";
+NSString * const MITImageDiningInfoHighlighted = @"dining-info-pressed";
+NSString * const MITImageDiningRotateDevice = @"dining-rotate-device";
+
+#pragma mark Meal Types
+NSString * const MITImageDiningMealFarmToFork = @"dining-meal-farm-to-fork";
+NSString * const MITImageDiningMealGlutenFree = @"dining-meal-gluten-free";
+NSString * const MITImageDiningMealHalal = @"dining-meal-halal";
+NSString * const MITImageDiningMealHumane = @"dining-meal-humane";
+NSString * const MITImageDiningMealInBalance = @"dining-meal-in-balance";
+NSString * const MITImageDiningMealKosher = @"dining-meal-kosher";
+NSString * const MITImageDiningMealOrganic = @"dining-meal-organic";
+NSString * const MITImageDiningMealSeafoodWatch = @"dining-meal-seafood-watch";
+NSString * const MITImageDiningMealVegan = @"dining-meal-vegan";
+NSString * const MITImageDiningMealVegetarian = @"dining-meal-vegetarian";
+NSString * const MITImageDiningMealWellBeing = @"dining-meal-well-being";
+
+#pragma mark - Events (Calendar)
+NSString * const MITImageEventsDayPickerButton = @"events-day-picker-button";
+NSString * const MITImageEventsPadChevronUp = @"events-pad-chevron-up";
+NSString * const MITImageEventsPadChevronDown = @"events-pad-chevron-down";
+
+#pragma mark - Libraries
+NSString * const MITImageLibrariesCheckmark = @"libraries-checkmark-unselected";
+NSString * const MITImageLibrariesCheckmarkSelected = @"libraries-checkmark-selected";
+
+#pragma mark Status Types
+NSString * const MITImageLibrariesStatusAlert = @"libraries-status-alert";
+NSString * const MITImageLibrariesStatusError = @"libraries-status-error";
+NSString * const MITImageLibrariesStatusOK = @"libraries-status-ok";
+NSString * const MITImageLibrariesStatusReady = @"libraries-status-ready";
+
+#pragma mark - Map
+NSString * const MITImageMapBrowseBuildings = @"map-browse-buildings";
+NSString * const MITImageMapBrowseFoodServices = @"map-browse-food-services";
+NSString * const MITImageMapBrowseResidences = @"map-browse-residences";
+NSString * const MITImageMapLocation = @"map-location";
+NSString * const MITImageMapLocationHighlighted = @"map-location-highlighted";
+NSString * const MITImageMapAnnotationUserLocation = @"map-annotation-user-location";
+NSString * const MITImageMapAnnotationPin = @"map-annotation-pin";
+NSString * const MITImageMapAnnotationPlacePin = @"map-annotation-place-pin";
+
+#pragma mark - News
+NSString * const MITImageNewsImagePlaceholder = @"";
+
+#pragma mark - People Directory
+NSString * const MITImagePeopleDirectoryDestructiveButton = @"";
+NSString * const MITImagePeopleDirectoryDestructiveButtonHighlighted = @"";
+
+#pragma mark - Scanner
+NSString * const MITImageScannerCameraUnsupported = @"";
+NSString * const MITImageScannerSampleBarcode = @"";
+NSString * const MITImageScannerSampleQRCode = @"";
+NSString * const MITImageScannerMissingImage = @"";
+NSString * const MITImageScannerScanBarButton = @"";
+
+#pragma mark - Shuttles
+NSString * const MITImageShuttlesShuttleRouteActive = @"";
+NSString * const MITImageShuttlesShuttleRouteInactive = @"";
+NSString * const MITImageShuttlesShuttleRouteLocationUnavailable = @"";
+NSString * const MITImageShuttlesBusBubble = @"";
+NSString * const MITImageShuttlesAlertEnabled = @"";
+NSString * const MITImageShuttlesAlertDisabled = @"";
+NSString * const MITImageShuttlesAnnotationBus = @"";
+NSString * const MITImageShuttlesAnnotationNextStop = @"";
+NSString * const MITImageShuttlesAnnotationNextStopSelected = @"";
+NSString * const MITImageShuttlesAnnotationCurrentStop = @"";
+NSString * const MITImageShuttlesAnnotationCurrentStopSelected = @"";
+
+
+NSString * const MITImageNameBackground      = @"global/body-background.png";
+
+NSString * const MITImageNameEmail           = @"global/action-email.png";
+NSString * const MITImageNameEmailHighlight  = @"global/action-email-highlight.png";
+NSString * const MITImageNameMap             = @"global/action-map.png";
+NSString * const MITImageNameMapHighlight    = @"global/action-map-highlight.png";
+NSString * const MITImageNamePeople          = @"global/action-people.png";
+NSString * const MITImageNamePeopleHighlight = @"global/action-people-highlight.png";
+NSString * const MITImageNamePhone           = @"global/action-phone.png";
+NSString * const MITImageNamePhoneHighlight  = @"global/action-phone-highlight.png";
+NSString * const MITImageNameExternal           = @"global/action-external.png";
+NSString * const MITImageNameExternalHighlight  = @"global/action-external-highlight.png";
+NSString * const MITImageNameEmergency          = @"global/action-emergency.png";
+NSString * const MITImageNameEmergencyHighlight = @"global/action-emergency-highlight.png";
+NSString * const MITImageNameSecure           = @"global/action-secure.png";
+NSString * const MITImageNameSecureHighlight  = @"global/action-secure-highlight.png";
+NSString * const MITImageNameCalendar       = @"global/action-calendar";
+NSString * const MITImageNameCalendarHighlight  = @"global/action-calendar-highlight";
+
+NSString * const MITImageNameLeftArrow = @"global/arrow-white-left.png";
+NSString * const MITImageNameRightArrow = @"global/arrow-white-right.png";
+NSString * const MITImageNameUpArrow = @"global/arrow-white-up.png";
+NSString * const MITImageNameDownArrow = @"global/arrow-white-down.png";
+
+NSString * const MITImageNameSearch = @"global/search.png";
+NSString * const MITImageNameBookmark = @"global/bookmark.png";
