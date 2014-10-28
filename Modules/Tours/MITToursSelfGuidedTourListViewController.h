60a5addf1c77ee0cc845f0dc0808af3f1f758ceb
Sam Voigt <sam@intrepid.io>
2014-10-28 16:59:31 -0400

    Added tours map/list container controller

---
+++ Modules/Tours/MITToursSelfGuidedTourListViewController.h
@@ -0,0 +1,13 @@
+//
+//  MITToursSelfGuidedTourListViewController.h
+//  MIT Mobile
+//
+//  Created by Samuel Voigt on 10/28/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MITToursSelfGuidedTourListViewController : UITableViewController
+
+@end
