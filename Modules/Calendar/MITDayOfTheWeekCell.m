e2c2fbdb6635d48ac014b6c7b2cf6cca9da03491
Logan Wright <logan.william.wright@gmail.com>
2014-10-29 15:45:12 -0400

    Incorporated day of month, updated day selection UI

--- Modules/Calendar/MITDayOfTheWeekCell.m
+++ Modules/Calendar/MITDayOfTheWeekCell.m
@@ -4,30 +4,41 @@
 @interface MITDayOfTheWeekCell ()
 
 @property (weak, nonatomic) IBOutlet UILabel *dayOfTheWeekLabel;
+@property (weak, nonatomic) IBOutlet UILabel *dayOfTheMonthLabel;
 
 @end
 
 @implementation MITDayOfTheWeekCell
 
-- (id)initWithFrame:(CGRect)frame
+#pragma mark - Initialization
+
+- (void)awakeFromNib
 {
-    self = [super initWithFrame:frame];
-    if (self) {
-        // Initialization code
-    }
-    return self;
+    self.dayOfTheMonthLabel.layer.cornerRadius = CGRectGetHeight(self.dayOfTheMonthLabel.bounds) / 2.0;
+    self.dayOfTheMonthLabel.layer.masksToBounds = YES;
+    self.dayOfTheMonthLabel.layer.shouldRasterize = YES;
+    self.dayOfTheMonthLabel.layer.rasterizationScale = [[UIScreen mainScreen] scale];
 }
+
+#pragma mark - Drawing
 
 - (void)setDayOfTheWeek:(MITDayOfTheWeek)dayOfTheWeek
 {
     _dayOfTheWeek = dayOfTheWeek;
+    
+    if (_dayOfTheWeek == MITDayOfTheWeekSaturday || _dayOfTheWeek == MITDayOfTheWeekSunday) {
+        self.dayOfTheWeekLabel.textColor = [self lighterGrayColor];
+        self.dayOfTheMonthLabel.textColor = [self lighterGrayColor];
+    } else {
+        self.dayOfTheWeekLabel.textColor = [UIColor darkTextColor];
+        self.dayOfTheMonthLabel.textColor = [UIColor darkTextColor];
+    }
     
     switch (_dayOfTheWeek) {
         case MITDayOfTheWeekSaturday:
         case MITDayOfTheWeekSunday:
             self.dayOfTheWeekLabel.text = @"S";
             break;
-        
         case MITDayOfTheWeekMonday:
             self.dayOfTheWeekLabel.text = @"M";
             break;
@@ -50,21 +61,44 @@
 - (void)setState:(MITDayOfTheWeekState)state
 {
     _state = state;
-    
-    self.dayOfTheWeekLabel.textColor = [UIColor colorWithWhite:0.4 alpha:1.0];
-    
-    if ((_state & MITDayOfTheWeekStateUnselected) == MITDayOfTheWeekStateUnselected) {
-        self.dayOfTheWeekLabel.font = [UIFont systemFontOfSize:17.0];
-    }
-    
-    if ((_state & MITDayOfTheWeekStateSelected) == MITDayOfTheWeekStateSelected) {
-        self.dayOfTheWeekLabel.font = [UIFont boldSystemFontOfSize:17.0];
-        self.dayOfTheWeekLabel.textColor = [UIColor blackColor];
-    }
-    
-    if ((_state & MITDayOfTheWeekStateToday) == MITDayOfTheWeekStateToday) {
-        self.dayOfTheWeekLabel.textColor = [UIColor mit_tintColor];
+
+    if (state == (MITDayOfTheWeekStateSelected | MITDayOfTheWeekStateToday)) {
+        // Is Selected && Is Today
+        self.dayOfTheMonthLabel.textColor = [UIColor whiteColor];
+        self.dayOfTheMonthLabel.backgroundColor = [UIColor mit_tintColor];
+        self.dayOfTheMonthLabel.font = [UIFont boldSystemFontOfSize:17.0];
+    } else if (state == MITDayOfTheWeekStateSelected) {
+        // Is Selected && Not Today
+        self.dayOfTheMonthLabel.textColor = [UIColor whiteColor];
+        self.dayOfTheMonthLabel.backgroundColor = [UIColor darkTextColor];
+        self.dayOfTheMonthLabel.font = [UIFont boldSystemFontOfSize:17.0];
+    } else if (state == (MITDayOfTheWeekStateToday | MITDayOfTheWeekStateUnselected)) {
+        // Is Today && Not Selected
+        self.dayOfTheMonthLabel.textColor = [UIColor mit_tintColor];
+        self.dayOfTheMonthLabel.backgroundColor = [UIColor clearColor];
+        self.dayOfTheMonthLabel.font = [UIFont systemFontOfSize:17.0];
+    } else if (state == MITDayOfTheWeekStateUnselected) {
+        // Is UnSelected && Not Today
+        if (self.dayOfTheWeek == MITDayOfTheWeekSaturday || self.dayOfTheWeek == MITDayOfTheWeekSunday) {
+            self.dayOfTheMonthLabel.textColor = [self lighterGrayColor];
+        } else {
+            self.dayOfTheMonthLabel.textColor = [UIColor darkTextColor];
+        }
+        self.dayOfTheMonthLabel.backgroundColor = [UIColor clearColor];
+        self.dayOfTheMonthLabel.font = [UIFont systemFontOfSize:17.0];
     }
 }
 
+- (void)setDayOfTheMonth:(NSInteger)dayOfTheMonth
+{
+    _dayOfTheMonth = dayOfTheMonth;
+    self.dayOfTheMonthLabel.text = [NSString stringWithFormat:@"%d", dayOfTheMonth];
+}
+
+#pragma mark - Lighter Text Color
+
+- (UIColor *)lighterGrayColor
+{
+    return [UIColor colorWithWhite:0.4 alpha:1.0];
+}
 @end
