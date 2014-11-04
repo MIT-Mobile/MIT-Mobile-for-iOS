f388c8b740d4b473b1e796fcf10d5381fc40e852
Andrew Dolce <andrew@intrepid.io>
2014-11-03 19:57:53 -0500

    Add stop image getter for full-size URL and fix thumbnail URL

--- Modules/Tours/MITToursImage.m
+++ Modules/Tours/MITToursImage.m
@@ -18,10 +18,26 @@
 - (NSString *)thumbnailURL
 {
     NSString *imageURL = nil;
+    NSInteger minWidth = NSIntegerMax;
+    for (MITToursImageRepresentation *representation in self.representations) {
+        NSInteger width = [representation.width integerValue];
+        if (width < minWidth) {
+            imageURL = representation.url;
+            minWidth = width;
+        }
+    }
+    return imageURL;
+}
+
+- (NSString *)fullImageURL
+{
+    NSString *imageURL = nil;
     NSInteger maxWidth = 0;
     for (MITToursImageRepresentation *representation in self.representations) {
-        if ([representation.width integerValue] > maxWidth) {
+        NSInteger width = [representation.width integerValue];
+        if (width > maxWidth) {
             imageURL = representation.url;
+            maxWidth = width;
         }
     }
     return imageURL;
