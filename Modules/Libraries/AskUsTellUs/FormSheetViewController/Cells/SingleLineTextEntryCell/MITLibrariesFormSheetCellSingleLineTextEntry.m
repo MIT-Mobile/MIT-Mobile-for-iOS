85ee2f0fdca0d5b93aee4ba2ab5e12982706a29a
Logan Wright <logan.william.wright@gmail.com>
2014-11-06 11:56:44 -0500

    Setting up single line entry

--- Modules/Libraries/AskUsTellUs/FormSheetViewController/Cells/SingleLineTextEntryCell/MITLibrariesFormSheetCellSingleLineTextEntry.m
+++ Modules/Libraries/AskUsTellUs/FormSheetViewController/Cells/SingleLineTextEntryCell/MITLibrariesFormSheetCellSingleLineTextEntry.m
@@ -4,21 +4,44 @@
 
 NSString * const MITLibrariesFormSheetCellSingleLineTextEntryNibName = @"MITLibrariesFormSheetCellSingleLineTextEntry";
 
-@interface MITLibrariesFormSheetCellSingleLineTextEntry ()
+@interface MITLibrariesFormSheetCellSingleLineTextEntry () <UITextFieldDelegate>
 @property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) MITLibrariesFormSheetElement *element;
 @end
 
 @implementation MITLibrariesFormSheetCellSingleLineTextEntry
+
+- (void)awakeFromNib
+{
+    self.textField.delegate = self;
+    [self.textField addTarget:self action:@selector(textFieldTextDidChange:) forControlEvents:UIControlEventEditingChanged];
+}
 
 - (void)configureCellForFormSheetElement:(MITLibrariesFormSheetElement *)element
 {
     self.textField.placeholder = element.title;
     self.textField.text = element.value;
+    self.element = element;
 }
 
 + (CGFloat)heightForCell
 {
     return 44.0;
+}
+
+#pragma mark - TextFieldTextDidChange
+
+- (void)textFieldTextDidChange:(UITextField *)textField
+{
+    self.element.value = textField.text;
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return NO;
 }
 
 @end
