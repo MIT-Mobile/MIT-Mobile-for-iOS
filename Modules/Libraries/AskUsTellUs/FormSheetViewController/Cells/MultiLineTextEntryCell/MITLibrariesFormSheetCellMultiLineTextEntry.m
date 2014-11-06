8a365b05c84575168bb7f64788cdb7ac3872f911
Logan Wright <logan.william.wright@gmail.com>
2014-11-06 12:01:59 -0500

    Setting up multi line entry

--- Modules/Libraries/AskUsTellUs/FormSheetViewController/Cells/MultiLineTextEntryCell/MITLibrariesFormSheetCellMultiLineTextEntry.m
+++ Modules/Libraries/AskUsTellUs/FormSheetViewController/Cells/MultiLineTextEntryCell/MITLibrariesFormSheetCellMultiLineTextEntry.m
@@ -5,21 +5,45 @@
 
 NSString * const MITLibrariesFormSheetCellMultiLineTextEntryNibName = @"MITLibrariesFormSheetCellMultiLineTextEntry";
 
-@interface MITLibrariesFormSheetCellMultiLineTextEntry ()
+@interface MITLibrariesFormSheetCellMultiLineTextEntry () <UITextViewDelegate>
 @property (weak, nonatomic) IBOutlet PlaceholderTextView *textView;
+@property (weak, nonatomic) MITLibrariesFormSheetElement *element;
 @end
 
 @implementation MITLibrariesFormSheetCellMultiLineTextEntry
+
+- (void)awakeFromNib
+{
+    self.textView.delegate = self;
+}
 
 - (void)configureCellForFormSheetElement:(MITLibrariesFormSheetElement *)element
 {
     self.textView.placeholder = element.title;
     self.textView.text = element.value;
+    self.element = element;
 }
 
 + (CGFloat)heightForCell
 {
     return 160.0;
+}
+
+#pragma mark - UITextFiewDelegate
+
+- (BOOL)textView:(UITextView *)textView shouldChangeTextInRange:(NSRange)range replacementText:(NSString *)text
+{
+    if ([text isEqualToString:@"\n"]) {
+        [textView resignFirstResponder];
+        return NO;
+    } else {
+        return YES;
+    }
+}
+
+- (void)textViewDidChange:(UITextView *)textView
+{
+    self.element.value = textView.text;
 }
 
 @end
