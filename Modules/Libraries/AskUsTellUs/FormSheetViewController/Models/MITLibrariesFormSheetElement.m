556c3cec70b39e8a313083277ea20e9086947b9b
Logan Wright <logan.william.wright@gmail.com>
2014-11-06 11:03:32 -0500

    Added options selection vc for choosing options in a formsheet

--- Modules/Libraries/AskUsTellUs/FormSheetViewController/Models/MITLibrariesFormSheetElement.m
+++ Modules/Libraries/AskUsTellUs/FormSheetViewController/Models/MITLibrariesFormSheetElement.m
@@ -14,21 +14,28 @@
 - (id)value
 {
     if (!_value) {
-        id firstAvailableOption = [self.availableOptions firstObject];
-        if ([firstAvailableOption isKindOfClass:[MITLibrariesFormSheetElementAvailableOption class]]) {
-            firstAvailableOption = [(MITLibrariesFormSheetElementAvailableOption *)firstAvailableOption value];
-        }
-        _value = firstAvailableOption;
+        MITLibrariesFormSheetElementAvailableOption *firstAvailableOption = [self.availableOptions firstObject];
+        _value = firstAvailableOption.value;
     }
-    
-    id returnVal = _value;
-    if ([returnVal isKindOfClass:[MITLibrariesFormSheetElementAvailableOption class]]) {
-        returnVal = [(MITLibrariesFormSheetElementAvailableOption *)returnVal value];
-    }
-    return returnVal;
+    return _value;
 }
 - (NSString *)title
 {
     return !_optional ? _title : [NSString stringWithFormat:@"%@ (optional)", _title];
+}
+
+- (void)setAvailableOptions:(NSArray *)availableOptions
+{
+    if (![availableOptions.firstObject isKindOfClass:[MITLibrariesFormSheetElementAvailableOption class]]) {
+        NSMutableArray *updatedOptions = [NSMutableArray array];
+        for (id option in availableOptions) {
+            MITLibrariesFormSheetElementAvailableOption *opt = [MITLibrariesFormSheetElementAvailableOption new];
+            opt.value = option;
+            [updatedOptions addObject:opt];
+        }
+        _availableOptions = updatedOptions;
+    } else {
+        _availableOptions = availableOptions;
+    }
 }
 @end
