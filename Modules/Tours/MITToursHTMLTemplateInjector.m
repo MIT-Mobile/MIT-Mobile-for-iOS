5b2568301e068fe8fb9e9b8797b83f05fc435af5
Sam Voigt <sam@intrepid.io>
2014-11-10 16:30:55 -0500

    Added main loop stop name to side trip directions

--- Modules/Tours/MITToursHTMLTemplateInjector.m
+++ Modules/Tours/MITToursHTMLTemplateInjector.m
@@ -18,13 +18,15 @@ static NSString *const kMITToursHTMLTemplateFilePath = @"tours/tours_directions_
     return templatedHtml;
 }
 
-+ (NSString *)templatedHTMLForSideTripStop:(MITToursStop *)sideTripStop viewWidth:(CGFloat)viewWidth
++ (NSString *)templatedHTMLForSideTripStop:(MITToursStop *)sideTripStop fromMainLoopStop:(MITToursStop *)mainLoopStop viewWidth:(CGFloat)viewWidth
 {
+    NSString *titleString = [NSString stringWithFormat:@"Directions from %@ to %@", mainLoopStop.title, sideTripStop.title];
+    
     NSMutableString *templatedHtml = [[NSMutableString alloc] initWithString:[MITToursHTMLTemplateInjector templateHTMLString]];
    
     NSString *maxWidth = [NSString stringWithFormat:@"%.0f", viewWidth];
     [templatedHtml replaceOccurrencesOfString:@"__WIDTH__" withString:maxWidth options:NSLiteralSearch range:NSMakeRange(0, [templatedHtml length])];
-    [templatedHtml replaceOccurrencesOfString:@"__TITLE__" withString:sideTripStop.title options:NSLiteralSearch range:NSMakeRange(0, [templatedHtml length])];
+    [templatedHtml replaceOccurrencesOfString:@"__TITLE__" withString:titleString options:NSLiteralSearch range:NSMakeRange(0, [templatedHtml length])];
     [templatedHtml replaceOccurrencesOfString:@"__BODY__" withString:@"Directions are not availiable for Side Trips" options:NSLiteralSearch range:NSMakeRange(0, [templatedHtml length])];
     
     return templatedHtml;
