c3c9e7b0e06d6e6de8613b17c39e762c95b93651
Sam Voigt <sam@intrepid.io>
2014-11-10 14:06:37 -0500

    Added tours links popover to iPad

---
+++ Modules/Tours/MITToursLinksTableViewController.m
@@ -0,0 +1,44 @@
+#import "MITToursLinksTableViewController.h"
+#import "MITToursLinksDataSourceDelegate.h"
+#import "MITMailComposeController.h"
+
+@interface MITToursLinksTableViewController () <MITToursLinksDataSourceDelegateDelegate, MFMailComposeViewControllerDelegate>
+
+@property (nonatomic, strong) MITToursLinksDataSourceDelegate *tableViewDataSourceDelegate;
+
+@end
+
+@implementation MITToursLinksTableViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.title = @"Links";
+    
+    self.tableView.scrollEnabled = NO;
+    
+    [self setupDataSourceDelegate];
+}
+
+- (void)setupDataSourceDelegate
+{
+    self.tableViewDataSourceDelegate = [[MITToursLinksDataSourceDelegate alloc] init];
+    self.tableView.dataSource = self.tableViewDataSourceDelegate;
+    self.tableView.delegate = self.tableViewDataSourceDelegate;
+    
+    self.tableViewDataSourceDelegate.delegate = self;
+}
+
+- (void)presentMailViewController:(MFMailComposeViewController *)mailViewController
+{
+    mailViewController.mailComposeDelegate = self;
+   [self presentViewController:mailViewController animated:YES completion:NULL];
+}
+
+- (void)mailComposeController:(MFMailComposeViewController *)controller didFinishWithResult:(MFMailComposeResult)result error:(NSError *)error
+{
+    [controller dismissViewControllerAnimated:YES completion:NULL];
+}
+
+@end
