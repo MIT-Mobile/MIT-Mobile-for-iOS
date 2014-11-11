46eb7ea6a442f52239f8c16f9c8148c0f564f32a
Ross LeBeau <ross.lebeau@gmail.com>
2014-11-11 17:15:24 -0500

    Dining iphone meal detail view made swipable

---
+++ Modules/Dining/MITDiningHouseMealListViewController.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+@class MITDiningMeal;
+
+@interface MITDiningHouseMealListViewController : UITableViewController
+
+@property (nonatomic, strong) MITDiningMeal *meal;
+
+
+- (void)applyFilters:(NSSet *)filters;
+
+@end
