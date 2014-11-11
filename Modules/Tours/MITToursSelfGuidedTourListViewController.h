e01318d297330e17aeac7d738fceb187e6dd2546
Andrew Dolce <andrew@intrepid.io>
2014-11-11 13:06:02 -0500

    Present details for stop selected in list view on iPhone

--- Modules/Tours/MITToursSelfGuidedTourListViewController.h
+++ Modules/Tours/MITToursSelfGuidedTourListViewController.h
@@ -1,9 +1,20 @@
 #import <UIKit/UIKit.h>
 
 @class MITToursTour;
+@class MITToursStop;
+@protocol MITToursSelfGuidedTourListViewControllerDelegate;
 
 @interface MITToursSelfGuidedTourListViewController : UITableViewController
 
 @property (nonatomic, strong) MITToursTour *tour;
+
+@property (nonatomic, weak) id<MITToursSelfGuidedTourListViewControllerDelegate> delegate;
+
+@end
+
+@protocol MITToursSelfGuidedTourListViewControllerDelegate <NSObject>
+
+@optional
+- (void)selfGuidedTourListViewController:(MITToursSelfGuidedTourListViewController *)selfGuidedTourListViewController didSelectStop:(MITToursStop *)stop;
 
 @end
