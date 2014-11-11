84a2a9589fa5f165bfe868af75e72e8273fcc676
Andrew Dolce <andrew@intrepid.io>
2014-11-11 11:52:38 -0500

    Add stop description to map callouts for iPad

--- Modules/Tours/MITToursMapViewController.h
+++ Modules/Tours/MITToursMapViewController.h
@@ -4,6 +4,7 @@
 @interface MITToursMapViewController : UIViewController
 
 @property (nonatomic, strong, readonly) MITToursTour *tour;
+@property (nonatomic) BOOL shouldShowStopDescriptions;
 
 - (instancetype)initWithTour:(MITToursTour *)tour nibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil;
 
