fdfc5b2f3629942c8c46ae9d6a21fdeed9eae3f3
Logan Wright <logan.william.wright@gmail.com>
2014-11-13 09:55:46 -0500

    Updated grid/list to use new icons

--- Modules/Libraries/MITLibrariesHomeViewControllerPad.m
+++ Modules/Libraries/MITLibrariesHomeViewControllerPad.m
@@ -70,8 +70,8 @@ static CGSize const MITLibrariesHomeViewControllerPadFormSheetPresentationPrefer
 {
     [self setupSearchBar];
     
-    self.listLayoutButton = [[UIBarButtonItem alloc] initWithTitle:@"List" style:UIBarButtonItemStylePlain target:self action:@selector(listViewPressed)];
-    self.gridLayoutButton = [[UIBarButtonItem alloc] initWithTitle:@"Grid" style:UIBarButtonItemStylePlain target:self action:@selector(gridViewPressed)];
+    self.listLayoutButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"libraries/list-view"] style:UIBarButtonItemStylePlain target:self action:@selector(listViewPressed)];
+    self.gridLayoutButton = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"libraries/grid-view"] style:UIBarButtonItemStylePlain target:self action:@selector(gridViewPressed)];
     self.layoutMode = MITLibrariesLayoutModeList;
 }
 
