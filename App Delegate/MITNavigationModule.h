5d9455b0519a1317555d98521d7440f6185ac71f
Blake Skinner <bskinner@mit.edu>
2014-11-20 12:38:07 -0500

    The navigation module's rootViewController should be strong. Fixes #350

--- App Delegate/MITNavigationModule.h
+++ App Delegate/MITNavigationModule.h
@@ -1,7 +1,7 @@
 #import "MITModule.h"
 
 @interface MITNavigationModule : MITModule
-@property(nonatomic,weak) UINavigationController *navigationController;
+@property(nonatomic,strong) UINavigationController *navigationController;
 @property(nonatomic,strong) UIViewController *rootViewController;
 
 - (instancetype)initWithName:(NSString *)name title:(NSString *)title;
--- Modules/Libraries/LibrariesModule.h
+++ Modules/Libraries/LibrariesModule.h
@@ -3,7 +3,7 @@
 #import "LibrariesViewController.h"
 
 @interface LibrariesModule : MITNavigationModule
-@property(nonatomic,weak) LibrariesViewController *rootViewController;
+@property(nonatomic,strong) LibrariesViewController *rootViewController;
 @property(nonatomic,strong) NSOperationQueue *requestQueue;
 
 - (instancetype)init;
