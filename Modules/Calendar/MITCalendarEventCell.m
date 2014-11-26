e3b2f63f764ffa5eedd7ae7fed9dbb2020793708
Logan Wright <logan.william.wright@gmail.com>
2014-11-26 12:28:24 -0500

    Renamed cell selected method for clarity

--- Modules/Calendar/MITCalendarEventCell.m
+++ Modules/Calendar/MITCalendarEventCell.m
@@ -88,7 +88,7 @@ static CGFloat kMITCalendarEventCellEstimatedHeight = 80.0;
 #pragma mark - Selection
 
 // Manually controlling selected appearance because if you use highlight, on scroll, cells deselect.
-- (void)updateForSelected:(BOOL)selected
+- (void)setBackgroundSelected:(BOOL)selected
 {
     if (selected) {
         self.backgroundColor = [UIColor colorWithRed:200/255.0 green:199/255.0 blue:204/255.0 alpha:1.0];
