57612ca7d45f37ee3cd4481bce20aa336a3b5c70
Mark Novak <mnovak@mit.edu>
2014-12-04 16:46:14 -0500

    Created OlderCreditsTableViewCell to toggle older credits.

--- Modules/About/AboutCreditsVC.m
+++ Modules/About/AboutCreditsVC.m
@@ -1,10 +1,19 @@
 #import "AboutCreditsVC.h"
 #import "MITAdditions.h"
 #import "AboutCreditsTableViewCell.h"
+#import "AboutCreditsViewOlderTableViewCell.h"
+
+NSString* const AboutCreditsViewOlderTableViewNibName = @"AboutCreditsViewOlderTableViewCell";
+NSString* const AboutCreditsViewOlderTableViewIdentifier = @"AboutCreditsViewOlder";
+
+NSString* const AboutCreditsTableViewCellNibName = @"AboutCreditsTableViewCell";
+NSString* const AboutCreditsTableViewCellIdentifier = @"AboutCredits";
 
 @interface AboutCreditsVC () <UIWebViewDelegate>
 @property (nonatomic, strong) NSMutableDictionary *dict;
+@property (nonatomic) BOOL showOlderCredits;
 @end
+
 
 @implementation AboutCreditsVC
 
@@ -13,23 +22,19 @@
 
     self.view.backgroundColor = [UIColor mit_backgroundColor];
     self.navigationItem.title = @"Credits";
-    
-    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory:YES];
-    NSURL *fileURL = [NSURL URLWithString:@"credits.html" relativeToURL:baseURL];
-    
-    NSError *error = nil;
-    NSMutableString *htmlString = [NSMutableString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
-    if (!htmlString) {
-        return;
-    }
-    [self.tableView registerNib:[UINib nibWithNibName:@"AboutCreditsTableViewCell" bundle:nil] forCellReuseIdentifier:@"AboutCredits"];
-    self.dict = [[NSMutableDictionary alloc] init];
 
+    [self.tableView registerNib:[UINib nibWithNibName:AboutCreditsTableViewCellNibName bundle:nil] forCellReuseIdentifier:AboutCreditsTableViewCellIdentifier];
+    [self.tableView registerNib:[UINib nibWithNibName:AboutCreditsViewOlderTableViewNibName bundle:nil] forCellReuseIdentifier:AboutCreditsViewOlderTableViewIdentifier];
+    
+    self.dict = [[NSMutableDictionary alloc] init];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return [self.dict[[NSNumber numberWithInt:indexPath.row]] floatValue];
+    if (self.dict[[NSNumber numberWithInt:indexPath.row]]) {
+        return [self.dict[[NSNumber numberWithInt:indexPath.row]] floatValue];
+    }
+    return 44;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
@@ -37,36 +42,46 @@
     return 2;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    self.showOlderCredits = YES;
+    [self.tableView reloadData];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    AboutCreditsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"AboutCredits"];
-    NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory:YES];
-    NSURL *fileURL = nil;
-    if (indexPath.row == 0) {
-        fileURL = [NSURL URLWithString:@"credits_current.html" relativeToURL:baseURL];
+    if (indexPath.row == 0 || self.showOlderCredits) {
+        NSURL *baseURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] resourcePath] isDirectory:YES];
+        NSURL *fileURL = nil;
+        if (indexPath.row == 0) {
+            fileURL = [NSURL URLWithString:@"credits_current.html" relativeToURL:baseURL];
+        } else {
+            fileURL = [NSURL URLWithString:@"credits_past.html" relativeToURL:baseURL];
+        }
+        
+        NSError *error = nil;
+        NSMutableString *htmlString = [NSMutableString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
+
+        AboutCreditsTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:AboutCreditsTableViewCellIdentifier];
+        cell.webView.scrollView.scrollEnabled = NO;
+        cell.webView.tag = indexPath.row;
+        if (!self.dict[[NSNumber numberWithInt:indexPath.row]]) {
+            cell.webView.delegate = self;
+        } else {
+            cell.webView.delegate = nil;
+        }
+        [cell.webView loadHTMLString:htmlString baseURL:nil];
+        
+        return cell;
     } else {
-        fileURL = [NSURL URLWithString:@"credits_past.html" relativeToURL:baseURL];
+        AboutCreditsViewOlderTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:AboutCreditsViewOlderTableViewIdentifier];
+        return cell;
     }
-    
-    NSError *error = nil;
-    NSMutableString *htmlString = [NSMutableString stringWithContentsOfURL:fileURL encoding:NSUTF8StringEncoding error:&error];
-    cell.webView.scrollView.scrollEnabled = NO;
-    cell.webView.tag = indexPath.row;
-    if (!self.dict[[NSNumber numberWithInt:indexPath.row]]) {
-        cell.webView.delegate = self;
-    } else {
-        cell.webView.delegate = nil;
-    }
-    [cell.webView loadHTMLString:htmlString baseURL:nil];
-    
-    return cell;
 }
 
 #pragma mark UIWebViewDelegate
 - (void)webViewDidStartLoad:(UIWebView *)webView
 {
-    [self.view setNeedsUpdateConstraints];
-    [self.view updateConstraintsIfNeeded];
 }
 
 - (void)webViewDidFinishLoad:(UIWebView *)webView
@@ -84,9 +99,7 @@
 
     self.dict[[NSNumber numberWithInt:webView.tag]] = [NSNumber numberWithFloat:size.height];
 
-    [self.view setNeedsUpdateConstraints];
-    [self.view updateConstraintsIfNeeded];
-    [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:webView.tag inSection:0]] withRowAnimation:(UITableViewRowAnimationNone)];
+    [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:webView.tag inSection:0]] withRowAnimation:(UITableViewRowAnimationAutomatic)];
 }
 
 - (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
---
+++ Modules/About/AboutCreditsViewOlderTableViewCell.m
@@ -0,0 +1,15 @@
+#import "AboutCreditsViewOlderTableViewCell.h"
+
+@implementation AboutCreditsViewOlderTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
