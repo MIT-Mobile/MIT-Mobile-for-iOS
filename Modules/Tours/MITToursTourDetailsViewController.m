03f27b2fa9c71f407c8c369a86658d8ea106ae16
Sam Voigt <sam@intrepid.io>
2014-12-04 11:22:49 -0500

    Tour Details page opens links in safari

--- Modules/Tours/MITToursTourDetailsViewController.m
+++ Modules/Tours/MITToursTourDetailsViewController.m
@@ -2,6 +2,10 @@
 #import "MITToursTour.h"
 #import "MITToursHTMLTemplateInjector.h"
 
+@interface MITToursTourDetailsViewController () <UIWebViewDelegate>
+
+@end
+
 @implementation MITToursTourDetailsViewController
 
 - (void)viewDidLoad
@@ -16,12 +20,23 @@
 - (void)viewWillAppear:(BOOL)animated
 {
     UIWebView *webview = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    webview.delegate = self;
     webview.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     webview.backgroundColor = [UIColor clearColor];
     [self.view addSubview:webview];
     
     NSString *templatedHTML = [MITToursHTMLTemplateInjector templatedHTMLForTourDetailsHTML:self.tour.descriptionHTML viewWidth:self.view.frame.size.width];
     [webview loadHTMLString:templatedHTML baseURL:nil];
+}
+
+- (BOOL)webView:(UIWebView *)inWeb shouldStartLoadWithRequest:(NSURLRequest *)inRequest navigationType:(UIWebViewNavigationType)inType
+{
+    if (inType == UIWebViewNavigationTypeLinkClicked) {
+        [[UIApplication sharedApplication] openURL:[inRequest URL]];
+        return NO;
+    }
+    
+    return YES;
 }
 
 @end
