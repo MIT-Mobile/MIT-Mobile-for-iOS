4839cab737087305032025666ac3661147e0226b
Yevgeniy Motov <ymotov@gmail.com>
2014-12-11 01:28:55 -0500

    updating alerts UI

--- Modules/QR Reader/MITBatchScanningAlertView.h
+++ Modules/QR Reader/MITBatchScanningAlertView.h
@@ -7,11 +7,24 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@class MITBatchScanningAlertView;
+
+@protocol MITBatchScanningAlertViewDelegate <NSObject>
+
+- (void)didTouchAlertView:(MITBatchScanningAlertView *)alertView;
+
+@end
 
 @interface MITBatchScanningAlertView : UIView
 
 @property (weak, nonatomic) IBOutlet UILabel *scanCodeLabel;
 @property (weak, nonatomic) IBOutlet UIImageView *ScanThumbnailView;
+
+@property (strong, nonatomic) NSManagedObjectID *scanId;
+
+@property (weak, nonatomic) id<MITBatchScanningAlertViewDelegate> delegate;
 
 - (void)fadeOutWithDuration:(NSTimeInterval)duration andWait:(NSTimeInterval)wait;
 
