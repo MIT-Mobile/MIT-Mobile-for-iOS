d3f542ef2e85eeb22816c1288d1f4af310e2eba9
Blake Skinner <bskinner@mit.edu>
2014-12-12 15:26:45 -0500

    Enable pushNotificationSupported for the Emergency module. #576

--- Modules/Emergency/EmergencyModule.m
+++ Modules/Emergency/EmergencyModule.m
@@ -17,6 +17,7 @@
     if (self) {
         self.longTitle = @"Emergency Info";
         self.imageName = MITImageEmergencyModuleIcon;
+        self.pushNotificationSupported = YES;
     }
     
     return self;
