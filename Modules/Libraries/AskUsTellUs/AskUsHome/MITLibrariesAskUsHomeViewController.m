0b4ccd71c16b1ea8f5019341edb56cfc1c8f1b72
Logan Wright <logan.william.wright@gmail.com>
2014-12-18 15:38:31 -0500

    Increased padding on askUs cells and spacing between subtitle #517

--- Modules/Libraries/AskUsTellUs/AskUsHome/MITLibrariesAskUsHomeViewController.m
+++ Modules/Libraries/AskUsTellUs/AskUsHome/MITLibrariesAskUsHomeViewController.m
@@ -5,6 +5,8 @@
 #import "MITLibrariesTellUsFormSheetViewController.h"
 
 static NSString * const MITLibrariesAskUsHomeViewControllerCellIdentifier = @"MITLibrariesAskUsHomeViewControllerCellIdentifier";
+static CGFloat const MITLibrariesAskUsHomeDetailLabelPadding = 6.0;
+static CGFloat const MITLibrariesAskUsHomeCellPadding = 38.0;
 
 @interface MITLibrariesAskUsHomeTableViewCell : UITableViewCell
 @end
@@ -17,6 +19,14 @@ static NSString * const MITLibrariesAskUsHomeViewControllerCellIdentifier = @"MI
         self.textLabel.lineBreakMode = NSLineBreakByWordWrapping;
     }
     return self;
+}
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    CGPoint center = self.detailTextLabel.center;
+    center.y += MITLibrariesAskUsHomeDetailLabelPadding;
+    // Subtitle seems to be off by 2pts.  This lines up much better.
+    center.x += 2.0;
+    self.detailTextLabel.center = center;
 }
 @end
 
@@ -183,9 +193,15 @@ static NSString * const MITLibrariesAskUsHomeViewControllerCellIdentifier = @"MI
     cell.textLabel.text = [self titleTextForIndexPath:indexPath];
     CGSize maxSize = CGSizeMake(CGRectGetWidth(tableView.bounds), CGFLOAT_MAX);
     CGSize titleSize = [cell.textLabel sizeThatFits:maxSize];
-    cell.detailTextLabel.text = [self detailTextForIndexPath:indexPath];
-    CGSize detailSize = [cell.detailTextLabel sizeThatFits:maxSize];
-    return titleSize.height + detailSize.height + 30; // Padding
+    NSString *detailText = [self detailTextForIndexPath:indexPath];
+    CGFloat detailHeight = 0;
+    if (detailText) {
+        cell.detailTextLabel.text = [self detailTextForIndexPath:indexPath];
+        CGSize detailSize = [cell.detailTextLabel sizeThatFits:maxSize];
+        detailHeight += detailSize.height;
+        detailHeight += MITLibrariesAskUsHomeDetailLabelPadding;
+    }
+    return titleSize.height + detailHeight + MITLibrariesAskUsHomeCellPadding;
 }
 
 #pragma mark - Getters | Setters
