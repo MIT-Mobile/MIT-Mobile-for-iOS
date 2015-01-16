3bafbe69445c098e2f262c4e9d2869b3bfa5ba66
Blake Skinner <bskinner@mit.edu>
2015-01-16 12:29:22 -0500

    Refactor the completion handler. #752

--- Modules/Emergency/Webservices/MITEmergencyInfoWebservices.m
+++ Modules/Emergency/Webservices/MITEmergencyInfoWebservices.m
@@ -17,20 +17,27 @@
 
 + (void)getEmergencyAnnouncement:(void (^)(MITEmergencyInfoAnnouncement *announcement, NSError *error))completion
 {
+    NSParameterAssert(completion);
+    
     [[MITMobile defaultManager] getObjectsForResourceNamed:MITEmergencyInfoAnnouncementResourceName
                                                 parameters:nil
                                                 completion:^(RKMappingResult *result, NSHTTPURLResponse *response, NSError *error) {
-                                                    
-                                                    MITEmergencyInfoAnnouncement *announcement = nil;
-                                                    
-                                                    if ([result.array count]) {
-                                                        id obj = result.array[0];
-                                                    
-                                                        if ([obj isKindOfClass:[MITEmergencyInfoAnnouncement class]]) {
-                                                            announcement = (MITEmergencyInfoAnnouncement *)result.array[0];
+                                                    if (error) {
+                                                        completion(nil, error);
+                                                    } else {
+                                                        MITEmergencyInfoAnnouncement *announcement = [result.array firstObject];
+                                                        
+                                                        if ([announcement isKindOfClass:[MITEmergencyInfoAnnouncement class]]) {
+                                                            completion(announcement, nil);
+                                                        } else if (announcement) {
+                                                            NSString *message = [NSString stringWithFormat:@"emergency announcement is kind of %@, expected %@",NSStringFromClass([announcement class]), NSStringFromClass([MITEmergencyInfoAnnouncement class])];
+                                                            @throw [NSException exceptionWithName:NSInternalInconsistencyException reason:message userInfo:nil];
+                                                        } else {
+                                                            NSString *message = [NSString stringWithFormat:@"failed to load announcement from remote server"];
+                                                            NSError *error = [NSError errorWithDomain:MITErrorDomain code:MITMobileRequestUnknownError userInfo:@{NSLocalizedDescriptionKey : message}];
+                                                            completion(nil,error);
                                                         }
                                                     }
-                                                    completion(announcement, error);
                                                 }];
 }
 
