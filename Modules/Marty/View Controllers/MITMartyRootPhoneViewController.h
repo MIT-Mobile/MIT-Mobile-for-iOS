10a98789bad472e991a9bd6ad8af4dbcd047f015
Blake Skinner <bskinner@mit.edu>
2015-01-23 13:35:00 -0500

    Initial commit of root view controller. #784
    RES-79

---
+++ Modules/Marty/View Controllers/MITMartyRootPhoneViewController.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+@interface MITMartyRootPhoneViewController : UIViewController
+@property(nonatomic,weak) IBOutlet UIView *helpTextView;
+@property(nonatomic,weak) IBOutlet UIView *contentContainerView;
+@property(nonatomic,weak) IBOutlet UIView *mapViewContainer;
+@property(nonatomic,weak) IBOutlet UIView *tableViewContainer;
+@end
