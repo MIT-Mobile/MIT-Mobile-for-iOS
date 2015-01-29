0b9e258dac388af9a3b4a2460440a5ed109b0cc2
Blake Skinner <bskinner@mit.edu>
2015-01-29 16:17:03 -0500

    Rough usage of the resources data source. #784
    RES-79

--- Modules/Marty/View Controllers/MITMartyRootPhoneViewController.h
+++ Modules/Marty/View Controllers/MITMartyRootPhoneViewController.h
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 
 @interface MITMartyRootPhoneViewController : UIViewController
+@property(nonatomic,strong) NSManagedObjectContext *managedObjectContext;
+
 @property(nonatomic,weak) IBOutlet UIView *helpTextView;
 @property(nonatomic,weak) IBOutlet UIView *contentContainerView;
 @property(nonatomic,weak) IBOutlet UIView *mapViewContainer;
