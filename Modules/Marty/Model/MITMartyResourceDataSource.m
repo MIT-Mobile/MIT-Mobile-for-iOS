5d7c2b3e8616643cf332128e29806a268df33fdf
Blake Skinner <bskinner@mit.edu>
2015-03-10 17:28:19 -0400

    Clear the current search results if a nil or empty string is passed in.

--- Modules/Marty/Model/MITMartyResourceDataSource.m
+++ Modules/Marty/Model/MITMartyResourceDataSource.m
@@ -64,63 +64,76 @@ static NSString* const MITMartyResourcePathPattern = @"resource";
 
 - (void)resourcesWithQuery:(NSString*)queryString completion:(void(^)(MITMartyResourceDataSource* dataSource, NSError *error))block
 {
-    NSURL *resourceReservations = [[NSURL alloc] initWithString:MITMartyDefaultServer];
-    NSMutableString *urlPath = [NSMutableString stringWithFormat:@"/%@",MITMartyResourcePathPattern];
-
-    if (queryString) {
-        NSString *encodedString = [queryString urlEncodeUsingEncoding:NSUTF8StringEncoding useFormURLEncoded:YES];
-        [urlPath appendFormat:@"?%@&q=%@",@"format=json",encodedString];
-    }
-
-    NSURL *resourcesURL = [NSURL URLWithString:urlPath relativeToURL:resourceReservations];
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:resourcesURL];
-    request.HTTPShouldHandleCookies = NO;
-    request.HTTPMethod = @"GET";
-    [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-
-    RKMapping *mapping = [MITMartyResource objectMapping];
-    RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping method:RKRequestMethodAny pathPattern:nil keyPath:@"collection.items" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
-
-    RKManagedObjectRequestOperation *requestOperation = [[RKManagedObjectRequestOperation alloc] initWithRequest:request responseDescriptors:@[responseDescriptor]];
-    requestOperation.managedObjectContext = self.managedObjectContext;
-
-    RKFetchRequestManagedObjectCache *cache = [[RKFetchRequestManagedObjectCache alloc] init];
-    requestOperation.managedObjectCache = cache;
-
-    __weak MITMartyResourceDataSource *weakSelf = self;
-    [requestOperation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
-        MITMartyResourceDataSource *blockSelf = weakSelf;
-        if (!blockSelf) {
-            return;
-        }
-
-        NSManagedObjectContext *context = blockSelf.managedObjectContext;
-        [context performBlock:^{
-            blockSelf.queryString = queryString;
-            blockSelf.lastFetched = [NSDate date];
-            blockSelf.resourceObjectIdentifiers = [NSManagedObjectContext objectIDsForManagedObjects:[mappingResult array]];
-
-            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                if (block) {
-                    block(blockSelf,nil);
-                }
-            }];
+    if (![queryString length]) {
+        self.queryString = nil;
+        self.lastFetched = [NSDate date];
+        self.resourceObjectIdentifiers = nil;
+        [self.managedObjectContext reset];
+        
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            if (block) {
+                block(self,nil);
+            }
         }];
-    } failure:^(RKObjectRequestOperation *operation, NSError *error) {
-        MITMartyResourceDataSource *blockSelf = weakSelf;
-        if (!blockSelf) {
-            return;
-        } else {
-            DDLogError(@"failed to request Marty resources: %@",error);
-            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                if (block) {
-                    block(blockSelf,error);
-                }
-            }];
-        }
-    }];
+    } else {
+        NSURL *resourceReservations = [[NSURL alloc] initWithString:MITMartyDefaultServer];
+        NSMutableString *urlPath = [NSMutableString stringWithFormat:@"/%@",MITMartyResourcePathPattern];
 
-    [self.mappingOperationQueue addOperation:requestOperation];
+        if (queryString) {
+            NSString *encodedString = [queryString urlEncodeUsingEncoding:NSUTF8StringEncoding useFormURLEncoded:YES];
+            [urlPath appendFormat:@"?%@&q=%@",@"format=json",encodedString];
+        }
+
+        NSURL *resourcesURL = [NSURL URLWithString:urlPath relativeToURL:resourceReservations];
+        NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:resourcesURL];
+        request.HTTPShouldHandleCookies = NO;
+        request.HTTPMethod = @"GET";
+        [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+
+        RKMapping *mapping = [MITMartyResource objectMapping];
+        RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping method:RKRequestMethodAny pathPattern:nil keyPath:@"collection.items" statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
+
+        RKManagedObjectRequestOperation *requestOperation = [[RKManagedObjectRequestOperation alloc] initWithRequest:request responseDescriptors:@[responseDescriptor]];
+        requestOperation.managedObjectContext = self.managedObjectContext;
+
+        RKFetchRequestManagedObjectCache *cache = [[RKFetchRequestManagedObjectCache alloc] init];
+        requestOperation.managedObjectCache = cache;
+
+        __weak MITMartyResourceDataSource *weakSelf = self;
+        [requestOperation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
+            MITMartyResourceDataSource *blockSelf = weakSelf;
+            if (!blockSelf) {
+                return;
+            }
+
+            NSManagedObjectContext *context = blockSelf.managedObjectContext;
+            [context performBlock:^{
+                blockSelf.queryString = queryString;
+                blockSelf.lastFetched = [NSDate date];
+                blockSelf.resourceObjectIdentifiers = [NSManagedObjectContext objectIDsForManagedObjects:[mappingResult array]];
+
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    if (block) {
+                        block(blockSelf,nil);
+                    }
+                }];
+            }];
+        } failure:^(RKObjectRequestOperation *operation, NSError *error) {
+            MITMartyResourceDataSource *blockSelf = weakSelf;
+            if (!blockSelf) {
+                return;
+            } else {
+                DDLogError(@"failed to request Marty resources: %@",error);
+                [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                    if (block) {
+                        block(blockSelf,error);
+                    }
+                }];
+            }
+        }];
+
+        [self.mappingOperationQueue addOperation:requestOperation];
+    }
 }
 
 #pragma mark - Recent Search List
