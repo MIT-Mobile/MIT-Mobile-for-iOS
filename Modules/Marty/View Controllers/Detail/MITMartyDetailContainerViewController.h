4622799550b3fcfca50b14c356a0d175bff0d381
Mark Novak <smartcop@gmail.com>
2015-03-18 16:03:34 -0400

    Delete files removed from project

--- Modules/Marty/View Controllers/Detail/MITMartyDetailContainerViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "MITMartyResource.h"
-
-@interface MITMartyDetailContainerViewController : UIViewController
-
-- (instancetype)initWithResource:(MITMartyResource *)resource resources:(NSArray *)resources nibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil;
-
-@end
--- Modules/Marty/View Controllers/Detail/MITMartyDetailTableViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import "MITMartyResource.h"
-
-@interface MITMartyDetailTableViewController : UITableViewController
-
-@property (nonatomic, weak) MITMartyResource *resource;
-
-@end
