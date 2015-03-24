bac4597fc609547c39fa6462ee02c5ae6cb3fd9b
Mark Novak <smartcop@gmail.com>
2015-03-24 16:58:08 -0400

    Revert "Shouldn't be relying on the cell class for determining what to do."
    This reverts commit 0600b013952a692e7a086d276056be80b20de45c.

--- Modules/Mobius/View Controllers/MITMobiusDetailTableViewController.m
+++ Modules/Mobius/View Controllers/MITMobiusDetailTableViewController.m
@@ -169,7 +169,7 @@ typedef NS_ENUM(NSInteger, MITMobiusTableViewSection) {
         [detailCell setTitle: self.resource.name];
         [detailCell setStatus:self.resource.status];
 
-    } else if (indexPath.section == MITMobiusTableViewSectionLocation) {
+    } else if ([cell isKindOfClass:[MITActionCell class]] && indexPath.section == MITMobiusTableViewSectionLocation) {
         MITActionCell *actionCell = (MITActionCell*)cell;
         [actionCell setupCellOfType:MITActionRowTypeLocation withDetailText:self.resource.room];
 
