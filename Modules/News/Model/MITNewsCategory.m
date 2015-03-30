d06b62390815d6d8bdd256129383eebddc68bf60
Mark Novak <smartcop@gmail.com>
2015-03-30 10:55:34 -0400

    Fixes news categorying ordering issue #935

--- Modules/News/Model/MITNewsCategory.m
+++ Modules/News/Model/MITNewsCategory.m
@@ -17,8 +17,6 @@
 
 + (RKMapping*)objectMapping
 {
-    RKDynamicMapping *categoryMapping = [[RKDynamicMapping alloc] init];
-
     RKEntityMapping *categoryOrderedMapping = [[RKEntityMapping alloc] initWithEntity:[self entityDescription]];
     categoryOrderedMapping.identificationAttributes = @[@"identifier"];
     [categoryOrderedMapping addAttributeMappingsFromDictionary:@{@"id" : @"identifier",
@@ -26,28 +24,18 @@
                                                                  @"name" : @"name",
                                                                  @"@metadata.mapping.collectionIndex" : @"order"}];
 
+    return categoryOrderedMapping;
+}
+
++ (RKMapping*)storyObjectMapping
+{
     RKEntityMapping *categoryDefaultMapping = [[RKEntityMapping alloc] initWithEntity:[self entityDescription]];
     categoryDefaultMapping.identificationAttributes = @[@"identifier"];
     [categoryDefaultMapping addAttributeMappingsFromDictionary:@{@"id" : @"identifier",
                                                                  @"url" : @"url",
                                                                  @"name" : @"name"}];
-
-    [categoryMapping setObjectMappingForRepresentationBlock:^RKObjectMapping *(id representation) {
-        if ([representation isProxy]) {
-            // Looks like we are dealing with an RKMappingSourceObject proxy.
-            // Grab the metadata from it so we can figure out if we should use the
-            //  ordered mapping (if we are pulling from /categories) or the regular mapping
-            RKRoute *route = [representation valueForKeyPath:@"@metadata.routing.route"];
-            if ([route.pathPattern isEqualToString:MITNewsCategoriesPathPattern]) {
-                return categoryOrderedMapping;
-            }
-        }
-
-        return categoryDefaultMapping;
-    }];
-
-
-    return categoryMapping;
+    
+    return categoryDefaultMapping;
 }
 
 @end
