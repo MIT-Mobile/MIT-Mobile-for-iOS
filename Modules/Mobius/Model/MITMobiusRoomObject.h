1a421efd735f1a7fb0eacc83e7c3cfad8fa06451
Mark Novak <smartcop@gmail.com>
2015-04-01 10:03:53 -0400

    NSString and NSOrderedSet should use copy semantics.

--- Modules/Mobius/Model/MITMobiusRoomObject.h
+++ Modules/Mobius/Model/MITMobiusRoomObject.h
@@ -6,9 +6,9 @@
 
 @interface MITMobiusRoomObject : NSObject <MKAnnotation>
 
-@property (nonatomic, retain) NSString * roomName;
+@property (nonatomic, copy) NSString * roomName;
 @property (nonatomic, retain) NSNumber * longitude;
 @property (nonatomic, retain) NSNumber * latitude;
-@property (nonatomic, retain) NSOrderedSet *resources;
+@property (nonatomic, copy) NSOrderedSet *resources;
 @property (nonatomic) NSInteger index;
 @end
