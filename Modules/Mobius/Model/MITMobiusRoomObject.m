9b01d3c7e42f6ec0e6cdfd4aaa1336c987cb43e9
Blake Skinner <bskinner@mit.edu>
2015-04-07 16:46:52 -0400

    Set the annotation coordinate based on the average of the resource locations.

--- Modules/Mobius/Model/MITMobiusRoomObject.m
+++ Modules/Mobius/Model/MITMobiusRoomObject.m
@@ -5,17 +5,23 @@
 
 @implementation MITMobiusRoomObject
 @synthesize resources = _resources;
+@synthesize coordinate = _coordinate;
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _coordinate = kCLLocationCoordinate2DInvalid;
+    }
+
+    return self;
+}
 
 #pragma mark MKAnnotation
 
 - (NSString*)title
 {
     return self.roomName;
-}
-
-- (CLLocationCoordinate2D)coordinate
-{
-    return CLLocationCoordinate2DMake([self.latitude doubleValue], [self.longitude doubleValue]);
 }
 
 - (void)setResources:(NSOrderedSet *)resources
@@ -31,6 +37,8 @@
     } else {
         _resources = nil;
     }
+
+    [self _updateCoordinateCentroid];
 }
 
 - (NSOrderedSet*)resources
@@ -64,6 +72,42 @@
     } else {
         return nil;
     }
+}
+
+- (void)_updateCoordinateCentroid
+{
+    if (_resources.count > 0) {
+        NSManagedObjectContext *managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+        managedObjectContext.parentContext = [MITCoreDataController defaultController].mainQueueContext;
+
+        [managedObjectContext performBlockAndWait:^{
+            __block MKMapPoint centroidPoint = MKMapPointMake(0, 0);
+            __block NSUInteger pointCount = 0;
+
+            NSOrderedSet *resources = [self resourcesInManagedObjectContext:managedObjectContext];
+            [resources enumerateObjectsUsingBlock:^(MITMobiusResource *resource, NSUInteger idx, BOOL *stop) {
+                CLLocationCoordinate2D coordinate = resource.coordinate;
+                if (CLLocationCoordinate2DIsValid(coordinate)) {
+                    MKMapPoint mapCoordinate = MKMapPointForCoordinate(coordinate);
+                    centroidPoint.x += mapCoordinate.x;
+                    centroidPoint.y += mapCoordinate.y;
+                    ++pointCount;
+                }
+            }];
+
+            if (pointCount > 0) {
+                centroidPoint.x /= (double)(pointCount);
+                centroidPoint.y /= (double)(pointCount);
+                _coordinate = MKCoordinateForMapPoint(centroidPoint);
+            } else {
+                _coordinate = kCLLocationCoordinate2DInvalid;
+            }
+        }];
+    } else {
+        _coordinate = kCLLocationCoordinate2DInvalid;
+    }
+
+    DDLogVerbose(@"Coordinate for %@: %@",self.roomName,NSStringFromCLLocationCoordinate2D(_coordinate));
 }
 
 @end
