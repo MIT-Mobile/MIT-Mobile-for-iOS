f8bfa87a55ce38e1f10f263489bb062a94d97c7a
Blake Skinner <bskinner@mit.edu>
2015-04-09 01:10:02 -0400

    Significant cleanup to the attribute loading.

--- Modules/Mobius/Model/MITMobiusAttributesDataSource.h
+++ Modules/Mobius/Model/MITMobiusAttributesDataSource.h
@@ -1,8 +1,16 @@
 #import <Foundation/Foundation.h>
 
 @interface MITMobiusAttributesDataSource : NSObject
+@property (nonatomic,readonly,strong) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic,readonly,copy) NSArray *attributes;
 @property (nonatomic,strong) NSDate *lastUpdated;
 
-- (NSArray*)attributesInManagedObjectContext:(NSManagedObjectContext*)managedObjectContext error:(NSError**)error;
+- (instancetype)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext NS_DESIGNATED_INITIALIZER;
+
+/** Initializes the data source with a managed object context whose parent is the default
+ *  main queue context.
+ */
+- (instancetype)init;
+
 - (void)attributes:(void(^)(MITMobiusAttributesDataSource *dataSource, NSError* error))completion;
 @end
--- Modules/Mobius/Model/MITMobiusAttributesDataSource.m
+++ Modules/Mobius/Model/MITMobiusAttributesDataSource.m
@@ -1,6 +1,6 @@
 #import "MITMobiusAttributesDataSource.h"
 #import "MITCoreData.h"
-#import "MITMobiusResourceAttribute.h"
+#import "MITMobiusAttribute.h"
 #import "MITMobiusDataSource.h"
 #import "MITAdditions.h"
 
@@ -10,7 +10,6 @@ static NSString* const MITMobiusAttributesPathPattern = @"/attribute";
 @property (nonatomic,strong) NSManagedObjectContext *managedObjectContext;
 @property (nonatomic,strong) NSOperationQueue *operationQueue;
 @property (nonatomic,strong) NSOperationQueue *completionOperationQueue;
-@property (nonatomic,copy) NSArray *objectIdentifiers;
 @property (nonatomic,copy) NSString *queryString;
 @end
 
@@ -19,60 +18,33 @@ static NSString* const MITMobiusAttributesPathPattern = @"/attribute";
     NSError *_requestError;
 }
 
+@synthesize attributes = _attributes;
+
 - (instancetype)init
 {
-    NSManagedObjectContext *managedObjectContext = [[MITCoreDataController defaultController] newManagedObjectContextWithConcurrencyType:NSPrivateQueueConcurrencyType trackChanges:YES];
+    NSManagedObjectContext *managedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+    managedObjectContext.parentContext = [MITCoreDataController defaultController].mainQueueContext;
     return [self initWithManagedObjectContext:managedObjectContext];
 }
 
 - (instancetype)initWithManagedObjectContext:(NSManagedObjectContext*)managedObjectContext
 {
     NSParameterAssert(managedObjectContext);
-
+    
     self = [super init];
     if (self) {
         _managedObjectContext = managedObjectContext;
         _operationQueue = [[NSOperationQueue alloc] init];
         _operationQueue.maxConcurrentOperationCount = 1;
-
+        
         _completionOperationQueue = [[NSOperationQueue alloc] init];
         _completionOperationQueue.maxConcurrentOperationCount = NSOperationQueueDefaultMaxConcurrentOperationCount;
     }
-
+    
     return self;
 }
 
-
 #pragma mark Public Interface Methods
-- (NSArray*)attributesInManagedObjectContext:(NSManagedObjectContext*)managedObjectContext error:(NSError**)error
-{
-    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:[MITMobiusResourceAttribute entityName]];
-    fetchRequest.predicate = [NSPredicate predicateWithFormat:@"ANY self.identifier IN %@",self.objectIdentifiers];
-
-    NSError *fetchError = nil;
-    NSArray *fetchedObjects = [managedObjectContext executeFetchRequest:fetchRequest error:&fetchError];
-
-    if (!fetchedObjects) {
-        DDLogWarn(@"Fetch of %@ objects failed: %@", [MITMobiusResourceAttribute entityName], fetchError);
-
-        if (error) {
-            (*error) = fetchError;
-        }
-    } else {
-        NSArray *sortedObjects = [fetchedObjects sortedArrayUsingComparator:^NSComparisonResult(MITMobiusResourceAttribute *attribute1, MITMobiusResourceAttribute *attribute2) {
-
-            NSNumber *attributeIndex1 = @([self.objectIdentifiers indexOfObject:attribute1.identifier]);
-            NSNumber *attributeIndex2 = @([self.objectIdentifiers indexOfObject:attribute2.identifier]);
-
-            return [attributeIndex1 compare:attributeIndex2];
-        }];
-
-        fetchedObjects = sortedObjects;
-    }
-
-    return fetchedObjects;
-}
-
 - (void)attributes:(void(^)(MITMobiusAttributesDataSource *dataSource, NSError* error))completion
 {
     [[NSOperationQueue mainQueue] addOperationWithBlock:^{
@@ -82,62 +54,57 @@ static NSString* const MITMobiusAttributesPathPattern = @"/attribute";
 
 - (void)_attributes:(void(^)(MITMobiusAttributesDataSource *dataSource, NSError* error))completion
 {
-    if (_requestInProgress) {
-        __weak MITMobiusAttributesDataSource *weakSelf = self;
-        [self.operationQueue addOperationWithBlock:^{
-            MITMobiusAttributesDataSource *blockSelf = weakSelf;
-            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
-                if (completion) {
-                    completion(blockSelf,_requestError);
-                }
-            }];
-        }];
-    } else {
+    if (!_requestInProgress) {
+        _requestInProgress = YES;
         self.completionOperationQueue.suspended = YES;
-
+        
         NSURL *serverURL = [MITMobiusDataSource mobiusServerURL];
         NSURL *attributesURL = [NSURL URLWithString:MITMobiusAttributesPathPattern relativeToURL:serverURL];
-
+        
         NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:attributesURL];
-        request.HTTPShouldHandleCookies = NO;
-        request.HTTPMethod = @"GET";
         [request setValue:@"application/json" forHTTPHeaderField:@"Accept"];
-
-        RKMapping *mapping = [MITMobiusResourceAttribute objectMapping];
+        
+        // Directly using the RestKit mapping operations here because the app
+        // is currently not capable of mapping requests from arbirary hosts on a per-request basis.
+        // (the current architecture was implemented under the assumption that all communication
+        // would be filtered through the MIT Mobile APIs)
+        // (bskinner - 2015.04.08)
+        RKMapping *mapping = [MITMobiusAttribute objectMapping];
         RKResponseDescriptor *responseDescriptor = [RKResponseDescriptor responseDescriptorWithMapping:mapping method:RKRequestMethodAny pathPattern:nil keyPath:nil statusCodes:RKStatusCodeIndexSetForClass(RKStatusCodeClassSuccessful)];
-
+        
         RKManagedObjectRequestOperation *requestOperation = [[RKManagedObjectRequestOperation alloc] initWithRequest:request responseDescriptors:@[responseDescriptor]];
-        requestOperation.managedObjectContext = self.managedObjectContext;
-
+        
+        NSManagedObjectContext *mappingContext = [[MITCoreDataController defaultController] newManagedObjectContextWithConcurrencyType:NSPrivateQueueConcurrencyType trackChanges:NO];
+        requestOperation.managedObjectContext = mappingContext;
+        
         RKFetchRequestManagedObjectCache *cache = [[RKFetchRequestManagedObjectCache alloc] init];
         requestOperation.managedObjectCache = cache;
-
+        
         __weak MITMobiusAttributesDataSource *weakSelf = self;
         [requestOperation setCompletionBlockWithSuccess:^(RKObjectRequestOperation *operation, RKMappingResult *mappingResult) {
             MITMobiusAttributesDataSource *blockSelf = weakSelf;
+            
             if (!blockSelf) {
                 return;
             }
-
-            NSManagedObjectContext *context = blockSelf.managedObjectContext;
-            [context performBlock:^{
-                blockSelf.lastUpdated = [NSDate date];
-                blockSelf.objectIdentifiers = [[mappingResult array] mapObjectsUsingBlock:^NSString*(MITMobiusResourceAttribute *attribute, NSUInteger idx) {
-                    Class attributeClass = [MITMobiusResourceAttribute class];
-                    NSAssert([attribute isKindOfClass:attributeClass], @"attribute is kind of %@, expected %@", NSStringFromClass([attribute class]), NSStringFromClass(attributeClass));
-                    return attribute.identifier;
-                }];
-
+            
+            blockSelf->_requestInProgress = NO;
+            blockSelf.lastUpdated = [NSDate date];
+            [blockSelf.managedObjectContext performBlock:^{
+                blockSelf->_attributes = [blockSelf.managedObjectContext transferManagedObjects:[mappingResult array]];
                 blockSelf.completionOperationQueue.suspended = NO;
             }];
         } failure:^(RKObjectRequestOperation *operation, NSError *error) {
             MITMobiusAttributesDataSource *blockSelf = weakSelf;
-            blockSelf.completionOperationQueue.suspended = NO;
-
+            
             if (!blockSelf) {
                 return;
             } else {
+                blockSelf.completionOperationQueue.suspended = NO;
+                blockSelf->_requestInProgress = NO;
                 blockSelf->_requestError = error;
+                blockSelf->_attributes = nil;
+                
                 DDLogError(@"failed to request Mobius resources: %@",error);
                 [self.completionOperationQueue addOperationWithBlock:^{
                     [[NSOperationQueue mainQueue] addOperationWithBlock:^{
@@ -146,13 +113,23 @@ static NSString* const MITMobiusAttributesPathPattern = @"/attribute";
                         }
                     }];
                 }];
-
+                
                 blockSelf.completionOperationQueue.suspended = NO;
             }
         }];
-
+        
         [self.operationQueue addOperation:requestOperation];
     }
+    
+    __weak MITMobiusAttributesDataSource *weakSelf = self;
+    [self.completionOperationQueue addOperationWithBlock:^{
+        MITMobiusAttributesDataSource *blockSelf = weakSelf;
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            if (completion) {
+                completion(blockSelf,_requestError);
+            }
+        }];
+    }];
 }
 
 @end
